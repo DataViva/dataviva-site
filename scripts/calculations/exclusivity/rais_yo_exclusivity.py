247fd77d3f4ff90de88442a746d522dad0f8f2ed
Alex Simoes <alexandersimoes@gmail.com>
2013-08-15 13:05:16 -0400

    Cleaned up exclusivity script using matrix math, much faster

--- scripts/calculations/exclusivity/rais_yo_exclusivity.py
+++ scripts/calculations/exclusivity/rais_yo_exclusivity.py
@@ -8,7 +8,7 @@
 import MySQLdb, os, argparse
 import pandas as pd, pandas.io.sql as sql
 import numpy as np
-import sys
+import sys, time
 
 from os import environ
 
@@ -26,46 +26,87 @@ db = MySQLdb.connect(host="localhost", user=environ["VISUAL_DB_USER"],
 db.autocommit(1)
 cursor = db.cursor()
 
-def get_all_isic(y):
-    cursor.execute("select distinct(isic_id) from rais_yi " \
-                        "where length(isic_id) = 5 and year = %s", [y])
-    return [x[0] for x in cursor.fetchall()]
-
 def get_all_cbo(y):
     cursor.execute("select distinct(cbo_id) from rais_yo where "\
                         "length(cbo_id) = 4 and year = %s", [y])
     return [x[0] for x in cursor.fetchall()]
 
-def calculate_exclusivity(y):
+def add_to_db(y, cbo, importance):
     
-    all_isic = get_all_isic(y)
+    to_add = []
+    for isic in importance.index:
+        imp = importance[isic]
+        to_add.append([imp, y, cbo, isic])
+    
+    cursor.executemany("update rais_yio set importance=%s where " \
+                    "year=%s and cbo_id=%s and isic_id=%s", to_add)
+
+def calculate_exclusivity(y):
+    start = time.time()
     all_cbo = get_all_cbo(y)
     
+    '''Get values from database'''
+    q = "select isic_id, bra_id, rca " \
+        "from rais_ybi " \
+        "where year = {0} and length(isic_id) = 5 " \
+        "and length(bra_id) = 8".format(y)
+    rcas = sql.read_frame(q, db)
+    rcas = rcas.pivot(index="bra_id", columns="isic_id", values="rca")
+    rcas = rcas.fillna(0)
+    
+    '''convert nominal RCA values to 0s and 1s'''
+    rcas[rcas >= 1] = 1
+    rcas[rcas < 1] = 0
+    
+    denoms = rcas.sum()
+    
+    '''mcp'''
+    q = """
+          select 
+            bra_id, isic_id, cbo_id, num_emp 
+          from 
+            rais_ybio
+          where 
+            year = {0} and 
+            length(bra_id) = 8 and 
+            length(isic_id) = 5 and 
+            length(cbo_id) = 4
+        """.format(y)
+    # print q
+    table = sql.read_frame(q, db)
+    table = table.pivot_table(rows=["isic_id", "cbo_id"], cols="bra_id", values="num_emp")
+    table = table.fillna(0)
+  
+    panel = table.to_panel()
+    panel = panel.swapaxes("items", "minor")
+    panel = panel.swapaxes("major", "minor")
+  
+    # z       = occupations
+    # rows    = bras
+    # colums  = isics
+    
+    print y, time.time() - start
+    # sys.exit()
+    
     for cbo in all_cbo:
-        print cbo
-        for isic in all_isic:
-            print isic
-            cursor.execute("select bra_id from rais_ybi where isic_id = %s " \
-                            "and year = %s and rca >= 1 and length(bra_id) = 8", [isic, y])
-            bras = [x[0] for x in cursor.fetchall()]
-            denom = len(bras)
-            numerator = 0
-            
-            for b in bras:
-                cursor.execute("select num_emp from rais_ybio where "\
-                                "year = %s and bra_id = %s and " \
-                                "isic_id = %s and cbo_id = %s", \
-                                [y, b, isic, cbo])
-                exists = cursor.fetchone()
-                if exists:
-                    numerator += 1.0
-            
-            # print numerator, denom, 
-            avar = numerator/float(denom)
-            print cbo, isic, avar
-            cursor.execute("update rais_yio set importance = %s "\
-                                "where year = %s and isic_id = %s "\
-                                "and cbo_id = %s", [avar,y,isic,cbo])
+        start = time.time()
+        
+        num_emp = panel[cbo].fillna(0)
+        numerators = num_emp * rcas
+        numerators = numerators.fillna(0)
+        
+        '''convert nominal num_emp values to 0s and 1s'''
+        numerators[numerators >= 1] = 1
+        numerators[numerators < 1] = 0
+        
+        numerators = numerators.sum()
+        importance = numerators / denoms
+        # print importance
+        
+        add_to_db(y, cbo, importance)
+        
+        print y, cbo, time.time() - start
+    
 
 def get_all_years():
     '''Get all years in the database'''
