0157a07c4860f66e633069d80bc64c7b27bc073c
Alex Simoes <alexandersimoes@gmail.com>
2013-10-03 16:16:55 -0400

    utility added to print translation CSV to JSON for updating apps/info.html

---
+++ scripts/utilities/translations_to_json.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+''' Import statements '''
+import csv, sys, os, argparse, json
+from collections import defaultdict
+
+def output(file):
+    apps_info = defaultdict(list)
+    
+    with open(file, 'rb') as csvfile:
+        file = csv.reader(csvfile, delimiter=',', quotechar='"')
+        header = file.next()
+        for row in file:
+            row = dict(zip(header, row))
+            data = {"text_en": row['English'], "text_pt": row["Portuguese"]}
+            if row["Image"] != "":
+                data["image"] = row["Image"]
+            if row["Video"] != "":
+                data["video"] = row["Video"]
+            apps_info[row['App']].append(data)
+        print json.dumps(apps_info, indent=4)
+        sys.exit()
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", help="full path to CSV file")
+    args = parser.parse_args()
+
+    file = args.file
+    if not file:
+        file = raw_input("Full path to CSV file: ")
+    
+    output(file)
