1a2ee7f2602c4d14c75c9fd29568185fb572625d
Dave Landry <landry.dave@gmail.com>
2013-10-21 16:18:06 -0400

    firefox bug fixes

--- dataviva/static/js/libs/d3plus.js
+++ dataviva/static/js/libs/d3plus.js
@@ -239,18 +239,18 @@ d3plus.tooltip = {};
 d3plus.tooltip.create = function(params) {
   
   var default_width = params.fullscreen ? 250 : 200
-  params.width = params.width ? params.width : default_width
-  params.max_width = params.max_width ? params.max_width : 386
-  params.id = params.id ? params.id : "default"
+  params.width = params.width || default_width
+  params.max_width = params.max_width || 386
+  params.id = params.id || "default"
   params.size = params.fullscreen || params.html ? "large" : "small"
-  params.offset = params.offset ? params.offset : 0
+  params.offset = params.offset || 0
   params.arrow_offset = params.arrow ? 8 : 0
-  params.x = params.x ? params.x : 0
-  params.y = params.y ? params.y : 0
-  params.color = params.color ? params.color : "#333"
-  params.parent = params.parent ? params.parent : d3.select("body")
-  params.background = params.background ? params.background : "#ffffff"
-  params.style = params.style ? params.style : "default"
+  params.x = params.x || 0
+  params.y = params.y || 0
+  params.color = params.color || "#333"
+  params.parent = params.parent || d3.select("body")
+  params.background = params.background || "#ffffff"
+  params.style = params.style || "default"
   
   d3plus.tooltip.remove("d3plus_tooltip_id_"+params.id)
   
@@ -359,7 +359,7 @@ d3plus.tooltip.create = function(params) {
     
     var newout = function() {
       
-      var target = d3.event.toElement
+      var target = d3.event.toElement || d3.event.relatedTarget
       if (target) {
         var c = typeof target.className == "string" ? target.className : target.className.baseVal
         var istooltip = c.indexOf("d3plus_tooltip") == 0
@@ -367,7 +367,7 @@ d3plus.tooltip.create = function(params) {
       else {
         var istooltip = false
       }
-      // console.log(!ischild(tooltip.node(),target), !ischild(params.mouseevents,target), !istooltip)
+      console.log(!ischild(tooltip.node(),target), !ischild(params.mouseevents,target), !istooltip)
       if (!target || (!ischild(tooltip.node(),target) && !ischild(params.mouseevents,target) && !istooltip)) {
         oldout(d3.select(params.mouseevents).datum())
         d3plus.tooltip.close()
@@ -3654,10 +3654,10 @@ d3plus.network = function(vars) {
         
         // Returns false if the mouse has moved into a child element.
         // This is used to catch when the mouse moves onto label text.
-        var target = d3.event.toElement
+        var target = d3.event.toElement || d3.event.relatedTarget
         if (target) {
           var id_check = target.__data__[vars.id_var] == d[vars.id_var]
-          if (d3.event.toElement.parentNode != this && !id_check) {
+          if (target.parentNode != this && !id_check) {
             hover = null;
             vars.update();
           }
