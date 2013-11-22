af78586b2eb126598f7794ee2cc853b57933879a
Alex Simoes <alexandersimoes@gmail.com>
2013-11-22 00:16:09 -0500

    fixed bug with rankings pages expecting cols to be string but receiving a list

--- dataviva/utils.py
+++ dataviva/utils.py
@@ -375,7 +375,7 @@ def make_query(data_table, url_args, lang, **kwargs):
     offset = url_args.get("offset", None)
     limit = url_args.get("limit", None)
     cols = url_args.get("cols", None)
-    if cols:
+    if type(cols) == str:
         cols = cols.split(".")
     excluding = url_args.get("excluding", None)
     if offset:
