9f520437d506eb56041e60c2c3516ff79968454a
Mario Mol <mariohmol@gmail.com>
2014-05-19 15:51:01 -0300

    fixing trans script

--- translate.sh
+++ translate.sh
@@ -1,3 +1,3 @@
-pybabel extract -F babel.cfg -o messages.pot dataviva
+pybabel extract -F babel.cfg -o dataviva/translations/messages.pot dataviva
 pybabel update -i dataviva/translations/messages.pot -d dataviva/translations
 pybabel compile -f -d dataviva/translations
