cec27adb8043a6d52ed3febe60c722523db6732b
Dave Landry <landry.dave@gmail.com>
2014-05-19 14:47:09 -0400

    simplified translate script

--- translate.sh
+++ translate.sh
@@ -1,6 +1,3 @@
-pybabel -v extract -F babel.cfg -o messages.pot ./
-mv messages.pot ./dataviva/translations
+pybabel extract -F babel.cfg -o messages.pot dataviva
 pybabel update -i dataviva/translations/messages.pot -d dataviva/translations
-cd dataviva
-pybabel compile -f -d ./translations
-cd ..
+pybabel compile -f -d dataviva/translations
