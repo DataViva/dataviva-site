3ac8a319b3f3cc64a3db74e27d3386b8a5c7af1b
Dave Landry <landry.dave@gmail.com>
2015-01-23 15:29:15 -0500

    fixed data page

--- dataviva/static/js/utils/utils.infinite_scroll.js
+++ dataviva/static/js/utils/utils.infinite_scroll.js
@@ -7,17 +7,17 @@ function infinite_scroll(selection){
       offset = 0,
       order = null,
       format_items = function(d){ return d };
-  
+
   // Initialize variables
   var formatDate = d3.time.format("%B %-d, %Y"),
       parseDate = d3.time.format.iso.parse,
       offset = 0,
       fetching;
-  
+
   scroll = function(selection) {
     selection.each(function(data_passed) {
       var container_el = this;
-      
+
       // On first time add
       var inf_loading_div = d3.select(container_el).selectAll(".infinite_loading")
 
@@ -27,13 +27,13 @@ function infinite_scroll(selection){
         .each(function(){
           infinite_loading = dataviva.ui.loading(".infinite_loading")
         })
-      
+
       infinite_loading.text(dataviva.format.text("loading_items")).show()
-      
+
       // The item we're going to listen on for scrolling
       d3.select(window)
           .on("scroll", maybe_fetch)
-      
+
       // Check if we need to fetch
       function maybe_fetch() {
 
@@ -51,12 +51,12 @@ function infinite_scroll(selection){
         }
 
       }
-      
+
       // Call the server for more acitivities
       function fetch() {
         fetching = true;
         refresh = false;
-  
+
         // conver url to Location object
         a = url
         // decide whether to use '?' or '&'
@@ -66,27 +66,31 @@ function infinite_scroll(selection){
         else {
           a += "?limit="+limit+"&offset="+offset
         }
-        
+
         if (order) {
           a += "&limit="+limit+"&order="+order
         }
-        
-        // Here we set the header X-Requested-With to XMLHttpRequest so the 
+
+        // Here we set the header X-Requested-With to XMLHttpRequest so the
         // server knows it's an AJAX call
         d3.json(a)
           .header("X-Requested-With", "XMLHttpRequest")
           .get(display);
       }
-      
-      // The meat and potatoes, this function get called after we've made the 
+
+      // The meat and potatoes, this function get called after we've made the
       // call to the server
       function display(error, new_data) {
-        
+
+        if ("headers" in new_data) {
+          new_data.data = dataviva.cleanData(new_data, url.split("/")[1], "all")
+        }
+
         activities = new_data.activities || new_data.data;
-  
+
         // we're obviously no longer fetching
         fetching = false;
-  
+
         // if the server returned an empty list, return and get rid of loading
         // div
         if (!activities.length) {
@@ -107,30 +111,30 @@ function infinite_scroll(selection){
             .style("height", 0)
           return;
         }
-  
+
         // increment offset by number of items received from server
         offset += activities.length;
-  
+
         // using d3's helpful enter/update/exit paradigm add new items from
         // the server
         format_items(container_el, activities, offset)
-        
+
         // remove no data p
         d3.select(container_el).selectAll("p.no_data").remove()
-  
+
         // maybe the user has a super dooper tall screen (or high resolution)
         // so we need to check if we're already at the bottem, even though we
         // just added new items
         setTimeout(maybe_fetch, 50);
-        
+
       }
-      
+
       // call this on page load
       maybe_fetch();
-      
+
     })
   }
-  
+
   scroll.url = function(value) {
     if(!arguments.length) return url;
     if(value.split("?")[0] != url.split("?")[0]){
@@ -139,31 +143,31 @@ function infinite_scroll(selection){
     url = value;
     return scroll;
   }
-  
+
   scroll.format_items = function(value) {
     if(!arguments.length) return format_items;
     format_items = value;
     return scroll;
   }
-  
+
   scroll.remove = function(value) {
     if(!arguments.length) return remove;
     remove = value;
     return scroll;
   }
-  
+
   scroll.offset = function(value) {
     if(!arguments.length) return offset;
     offset = value;
     return scroll;
   }
-  
+
   scroll.order = function(value) {
     if(!arguments.length) return order;
     order = value;
     return scroll;
   }
-  
+
   return scroll
-  
+
 }
