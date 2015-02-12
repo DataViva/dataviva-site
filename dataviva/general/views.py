696d5d923bed7fd942f0c03e436001e04fc5ddbe
Dave Landry <landry.dave@gmail.com>
2015-02-12 17:42:15 -0500

    homepage tweaks

--- dataviva/general/views.py
+++ dataviva/general/views.py
@@ -104,7 +104,7 @@ def after_request(response):
     return response
 
 @mod.route('/', methods=['GET', 'POST'])
-@view_cache.cached(timeout=604800, key_prefix=make_cache_key)
+# @view_cache.cached(timeout=604800, key_prefix=make_cache_key)
 @gzipped
 def home():
     # return render_template("test.html")
