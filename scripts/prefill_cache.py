7ce00509177cabbd0e575aea7529a0e274558a68
Jon Speiser <jspeis@gmail.com>
2015-02-17 13:11:03 -0500

    script now loads all iframes on profiles

--- scripts/prefill_cache.py
+++ scripts/prefill_cache.py
@@ -9,8 +9,6 @@ from selenium.webdriver.common.by import By
 @click.command()
 @click.argument('base_url', type=str)
 def main(base_url):
-    base_url = "http://localhost:5000"
-
     driver = webdriver.Firefox()
     driver.get(base_url)
     links = driver.find_elements_by_tag_name("a")
@@ -30,11 +28,17 @@ def main(base_url):
         driver.get(url)
         if "profiles/b" in url or "profiles/c" in url:
             try:
-                driver.switch_to_frame(driver.find_element_by_tag_name("iframe"))
-                element = WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.ID, "key"))
-                )
-                driver.switch_to_default_content()
+                iframes = driver.find_elements_by_xpath("//div[@class='lightbox guide_app']/iframe")
+                links = driver.find_elements_by_class_name("app_links")
+                for idx, link in enumerate(links):
+                    frame = iframes[idx]
+                    link.click()
+                    driver.switch_to_frame(driver.find_element_by_id(frame.get_attribute("id")))
+                    element = WebDriverWait(driver, 10).until(
+                        EC.element_to_be_clickable((By.ID, "key"))
+                    )
+                    driver.switch_to_default_content()
+
             except Exception, exception:
                 print "Moving on after 10 second wait.", str(exception)
 
