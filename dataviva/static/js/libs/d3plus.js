1d73f06abe44de01fef1eb81c9773e40a0f599f0
Dave Landry <landry.dave@gmail.com>
2015-03-03 15:42:02 -0500

    fixed x axis label padding

--- dataviva/static/js/libs/d3plus.js
+++ dataviva/static/js/libs/d3plus.js
@@ -30359,7 +30359,7 @@ labelPadding = function(vars) {
   }
   xAxisHeight = Math.round(xAxisHeight);
   xAxisWidth = Math.round(xAxisWidth);
-  vars.axes.margin.bottom += xAxisHeight;
+  vars.axes.margin.bottom += xAxisHeight + vars.labels.padding;
   lastTick = vars.x.ticks.visible[vars.x.ticks.visible.length - 1];
   rightLabel = vars.x.scale.viz(lastTick);
   rightLabel += xAxisWidth / 2 + vars.axes.margin.left;
