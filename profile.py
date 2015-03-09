7713c94acd99cc73ef182f8e0eda18e198012fbd
Dave Landry <landry.dave@gmail.com>
2015-03-09 12:26:59 -0400

    reorganized some scripts

--- profile.py
+++
@@ -1,6 +0,0 @@
-from werkzeug.contrib.profiler import ProfilerMiddleware
-from dataviva import app
-
-app.config['PROFILE'] = True
-app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions = [10])
-app.run(debug = True)
