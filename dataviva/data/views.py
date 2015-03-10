9c7d245d8e9320fb6923b99dcac1817c3d71247f
Dave Landry <landry.dave@gmail.com>
2015-03-10 12:27:40 -0400

    fixed vocational course output nesting

--- dataviva/data/views.py
+++ dataviva/data/views.py
@@ -56,13 +56,13 @@ def table(data_type="rais", year="all", bra_id="4mg", filter_1="show.1", filter_
 @view_cache.cached(timeout=604800, key_prefix=make_cache_key)
 def index(data_type="rais", year="all", bra_id=None, filter_1=None, filter_2=None):
     # /hedu/all/show.3/01298.show.5/all/
-    
+
     def get_filter_by_id(f):
         match = [v for v in filters if v["id"] == f]
         if match:
             return match[0]
         return None
-    
+
     datasets = [["rais", _('Employment')],
                 ["secex", _('International Trade')],
                 ["hedu", _('Higher Education')],
@@ -76,7 +76,7 @@ def index(data_type="rais", year="all", bra_id=None, filter_1=None, filter_2=Non
             months = range(int(start_year.split("-")[1]), int(end_year.split("-")[1])+1)
             months = [(m, dt.strptime(str(m),"%m").strftime("%B")) for m in months]
             d.append(months)
-    
+
     trans_lookup = dictionary()
     filters = [
         {"name": _('Brazilian Location'), "id": "bra", "datasets": "rais secex hedu sc", "nestings": [1, 3, 5, 7, 8, 9]},
@@ -88,23 +88,23 @@ def index(data_type="rais", year="all", bra_id=None, filter_1=None, filter_2=Non
         {"name": _('Trade Partners'), "id": "wld", "datasets": "secex", "nestings":[2, 5]},
         {"name": _('Universities'), "id": "university", "datasets": "hedu", "nestings":[5]},
         {"name": _('Majors'), "id": "course_hedu", "datasets": "hedu", "nestings":[2, 6]},
-        {"name": _('Vocational Courses'), "id": "course_sc", "datasets": "sc"}
+        {"name": _('Vocational Courses'), "id": "course_sc", "datasets": "sc", "nestings":[2, 5]}
     ]
     for f in filters:
         if "nestings" in f:
             attr_type = f["id"].replace("_r", "").replace("_s", "")
             f["nestings"] = [(n, trans_lookup["{}_{}".format(attr_type, n)]) for n in f["nestings"]]
-    
+
     selected_filters = []
-    
+
     '''
         Determine the selected dataset/filters/output from URL
     '''
     output = {"filters":[]}
-    
+
     # get selected dataset
     output["dataset"] = [v for v in datasets if v[0] == data_type][0]
-    
+
     # get filters
     filter_type_lookup = {
         "rais": [("bra", Bra), ("cnae", Cnae), ("cbo", Cbo)],
@@ -112,7 +112,7 @@ def index(data_type="rais", year="all", bra_id=None, filter_1=None, filter_2=Non
         "hedu": [("bra", Bra), ("university", University), ("course_hedu", Course_hedu)],
         "sc": [("bra", Bra), None, ("course_sc", Course_sc)],
     }
-    
+
     # parse year
     if year == "all":
         output["year"] = year
@@ -121,7 +121,7 @@ def index(data_type="rais", year="all", bra_id=None, filter_1=None, filter_2=Non
         output["year"], output["month"] = int(y), int(m)
     else:
         output["year"] = int(year)
-    
+
     if bra_id and filter_1 and filter_2:
         for i, f in enumerate([bra_id, filter_1, filter_2]):
             filter = f
@@ -150,6 +150,6 @@ def index(data_type="rais", year="all", bra_id=None, filter_1=None, filter_2=Non
         output["nesting"] = 8
         filter = Bra.query.get("4mg")
         output["filters"].append({"id":"bra", "datasets":"rais secex hedu sc", "filter":filter})
-    
+
     # raise Exception(output["dataset"], datasets, output["dataset"] == datasets[0])
     return render_template("data/index.html", datasets=datasets, filters=filters, selected_filters=selected_filters, output=output)
