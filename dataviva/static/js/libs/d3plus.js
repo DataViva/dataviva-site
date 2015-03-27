ab979454f0bb82704d8074a077e14fe3e86eb91a
Dave Landry <landry.dave@gmail.com>
2015-03-27 17:20:46 -0400

    fixed rings click flashing large tooltip

--- dataviva/static/js/libs/d3plus.js
+++ dataviva/static/js/libs/d3plus.js
@@ -22671,7 +22671,7 @@ module.exports = function(vars) {
           }
 
         }
-        else {
+        else if (vars.types[vars.type.value].requirements.indexOf("focus") < 0) {
 
           edge_update()
 
