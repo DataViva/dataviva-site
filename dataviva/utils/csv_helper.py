218f0676a77642a32674ca6afa42ca8dec67bce3
Jon Speiser <jspeis@gmail.com>
2015-04-06 12:09:09 -0400

    Adding IBGE ids to CSV downloads, closes #292

--- dataviva/utils/csv_helper.py
+++ dataviva/utils/csv_helper.py
@@ -1,9 +1,25 @@
 from flask import Response, request
+from dataviva.attrs.models import Bra
+
+def add_ibge_id(header, data):
+    bras = Bra.query.all()
+    bra_lookup = {b.id : b.id_ibge for b in bras}
+
+    bra_idx = header.index("bra_id")
+    header.append("ibge_id")
+    for item in data:
+        ibge_id = bra_lookup[item[bra_idx]]
+        item.append(ibge_id)
+    return header, data
 
 def gen_csv(raw_data, dataset):
     header = raw_data["headers"]
     data = raw_data["data"]
     all_rows = [header] + data
+
+    if "bra_id" in header:
+        header, data = add_ibge_id(header, data)
+
     def generate():
         for row in all_rows:
             yield ','.join(map(str, row)) + '\n'
