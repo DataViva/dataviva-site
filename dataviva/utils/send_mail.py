1c27f2799c1603a81650d9e95e8c3a353c491f0c
Dave Landry <landry.dave@gmail.com>
2015-04-30 15:42:26 -0400

    disabled errors getting sent to admins

--- dataviva/utils/send_mail.py
+++ dataviva/utils/send_mail.py
@@ -1,12 +1,12 @@
-''' Send email to users 
+''' Send email to users
     recipients=["dataviva@googlegroups.com","datavivaweb@gmail.com"]
     title="Hello"
     message="Invite friends: {0}".format(name)
 '''
-def send_mail(title, recipients,message):   
+def send_mail(title, recipients, message):   
     from dataviva import mail
     from flask.ext.mail import Message
-    msg = Message(title,sender="datavivaweb@gmail.com",recipients=recipients)   
+    msg = Message(title,sender="datavivaweb@gmail.com",recipients=recipients)
     msg.body = message
-    msg.html = msg.body 
+    msg.html = msg.body
     mail.send(msg)
