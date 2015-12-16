ded05e818adebcf831fa1e0f83ff1048060bbe19
FredericoVieira <fredops1@hotmail.com>
2015-12-16 08:01:04 -0200

    teste crontab

--- dataviva/profiles/views.py
+++ dataviva/profiles/views.py
@@ -134,6 +134,8 @@ def index_selector(category = None, id = None):
     selector = category
     article = None
 
+    #teste crontab
+
     if category == "cbo":
         article = gettext(u"an occupation")
     elif category == "cnae":
