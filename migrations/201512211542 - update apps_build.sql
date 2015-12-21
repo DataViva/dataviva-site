7f428eeff87c3761ab0e9e4551dd3376d355f14e
FredericoVieira <fredops1@hotmail.com>
2015-12-21 16:39:34 -0200

    Issue #326 corrigida no banco de dados.

---
+++ migrations/201512211542 - update apps_build.sql
@@ -0,0 +1,14 @@
+UPDATE apps_build SET
+slug2_en='Enrollment by State',
+slug2_pt='Matrículas por Estado'
+WHERE
+dataset = 'hedu' AND
+id = 107;
+
+UPDATE apps_build SET
+slug2_en='Enrollment in a Major by State',
+slug2_pt='Matrículas em um Curso por Estado'
+WHERE
+dataset = 'hedu' AND
+id = 108;
+
