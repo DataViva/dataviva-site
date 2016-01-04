46a604c9061e08fca9b7f128f47cba301800ceb8
Diogo Lundberg <diogo@lundberg.com.br>
2016-01-04 09:35:07 -0200

    This should fix the enrollment issue with municipality name. Issue #326

--- migrations/201512211542__update_apps_build.sql
+++ migrations/201512211542__update_apps_build.sql
@@ -1,13 +1,13 @@
 UPDATE apps_build SET
-slug2_en='Enrollment by State',
-slug2_pt='Matrículas por Estado'
+slug2_en='Enrollment by Location',
+slug2_pt='Matrículas por Localidade'
 WHERE
 dataset = 'hedu' AND
 id = 107;
 
 UPDATE apps_build SET
-slug2_en='Enrollment in a Major by State',
-slug2_pt='Matrículas em um Curso por Estado'
+slug2_en='Enrollment in a Major by Location',
+slug2_pt='Matrículas em um Curso por Localidade'
 WHERE
 dataset = 'hedu' AND
 id = 108;
