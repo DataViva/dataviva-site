9205ff8335ecab4f1fa181fe659b9041261b81f5
Diogo Lundberg <diogo@lundberg.com.br>
2016-01-24 12:59:55 -0200

    Removing unused package

--- dataviva/__init__.py
+++ dataviva/__init__.py
@@ -99,7 +99,6 @@ from dataviva.apps.data.views import mod as data_module
 from dataviva.apps.general.views import mod as general_module
 from dataviva.apps.graphs.views import mod as graphs_module
 from dataviva.apps.location.views import mod as location_module
-from dataviva.apps.profiles.views import mod as profiles_module
 from dataviva.apps.rankings.views import mod as rankings_module
 from dataviva.apps.wizard.views import mod as wizard_module
 
@@ -121,6 +120,5 @@ app.register_blueprint(data_module)
 app.register_blueprint(ask_module)
 app.register_blueprint(rankings_module)
 app.register_blueprint(about_module)
-app.register_blueprint(profiles_module)
 app.register_blueprint(location_module)
 app.register_blueprint(wizard_module)
