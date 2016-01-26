6d0d38777fadc104d4c6fb8f69a691c43f91449f
lauradebella <lauradebella@gmail.com>
2016-01-26 18:31:42 -0200

    Conclusao da tabela responsiva - salario e emprego

--- dataviva/apps/occupation/views.py
+++ dataviva/apps/occupation/views.py
@@ -17,18 +17,24 @@ def pull_lang_code(endpoint, values):
 def add_language_code(endpoint, values):
     values.setdefault('lang_code', get_locale())
 
-dict = {
+dic = {
 	'occupation' : 'Engenheiros em Computacao', 
 	'texto_perfil':'Engenharia de Computacao e o ramo da engenharia que lida com a realizacao de projeto e construcaoo de computadores e de sistemas que integram hardware e software, viabilizando a producao de novas maquinas e de equipamentos computacionais para serem utilizados em diversos setores.',
+	'family' : True,
+
+
 	'municipio_por_empregos' : 'Sao Paulo',
 	'num_empregos_principal_municipio' : 1.62 ,
 	'unidade_empregos_principal_municipio' : 'milhares',
 	'atividade_por_empregos' : 'atividade x',
 	'valor_atividade_por_empregos': 1.0,
-	'unidade_atividade_por_empregos': 'bilhao'
-
+	'unidade_atividade_por_empregos': 'bilhao',
+	'valor_maior_renda_media_mensal': 12.3,
+	'unidade_maior_renda_media_mensal': 'bilhoes',
+	'atividade_maior_renda': 'Desenvolvimento Sob Encomenda ',
+	'valor_atividade_maior_renda' : 990
 } 
 
 @mod.route('/')
 def index():
-	return render_template('occupation/index.html', body_class='perfil-estado', dic = dict)#occupation = occupation, texto_perfil = text_perfil_geral, family = True)
+	return render_template('occupation/index.html', body_class='perfil-estado', dic = dic)#occupation = occupation, texto_perfil = text_perfil_geral, family = True)
