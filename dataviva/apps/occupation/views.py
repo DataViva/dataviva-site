8658666ea11333501e245098607ba0aaca11f2d3
lauradebella <lauradebella@gmail.com>
2016-01-26 12:41:43 -0200

    Criacao das tags occupation e text

--- dataviva/apps/occupation/views.py
+++ dataviva/apps/occupation/views.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, g
 from dataviva.apps.general.views import get_locale
 
 mod = Blueprint('occupation', __name__,
-                template_folder='templates/occupation',
+                template_folder='templates',
                 url_prefix='/<lang_code>/occupation',
                 static_folder='static')
 
@@ -17,8 +17,9 @@ def pull_lang_code(endpoint, values):
 def add_language_code(endpoint, values):
     values.setdefault('lang_code', get_locale())
 
+occupation = 'Engenheiros em Computacao'
+text_perfil_geral = 'Engenharia de Computacao e o ramo da engenharia que lida com a realizacao de projeto e construcaoo de computadores e de sistemas que integram hardware e software, viabilizando a producao de novas maquinas e de equipamentos computacionais para serem utilizados em diversos setores.'
 
 @mod.route('/')
 def index():
-	occupation = 'Engenheiros em Computaçāo'
-    return render_template('occupation/index.html', body_class='perfil-estado', occupation = occupation)
+	return render_template('occupation/index.html', body_class='perfil-estado', occupation = occupation, texto_perfil = text_perfil_geral)
