c269ff41b9659af7df2a4d7c0c5b24380d5db4a0
lauradebella <lauradebella@gmail.com>
2016-01-27 17:36:25 -0200

    Adicionando variavel de ano parametrizavel e atualizando alguns nomes.

--- dataviva/apps/occupation/views.py
+++ dataviva/apps/occupation/views.py
@@ -19,10 +19,11 @@ def add_language_code(endpoint, values):
 
 context = {
 	#index
-	'occupation' : unicode('Engenheiros em Computaçāo', 'utf8') ,
+	'name' : unicode('Engenheiros em Computaçāo', 'utf8') ,
 	'text_profile': unicode('Engenharia de Computacao e o ramo da engenharia que lida com a realizacao de projeto e construcaoo de computadores e de sistemas que integram hardware e software, viabilizando a producao de novas maquinas e de equipamentos computacionais para serem utilizados em diversos setores.'),
 	'background_image': unicode("'static/img/bg-profile-location.jpg'", 'utf8'),
 	'family' : True,
+	'year' : 2010,
 	#header 
 	'average_monthly_income' : 8, #'renda_media_mensal' 
 	'average_monthly_income_unity' : 'Milhares', #'unidade_renda_media_mensal'
@@ -32,17 +33,20 @@ context = {
 	'total_employment_unity' : 'milhares', 
 	'total_establishments' : 6.8, #'total_estabelecimentos'
 	'total_establishments_unity' : 'milhares', #'unidade_total_estabelecimentos' 
-	#tabela responsiva 
-	'county_for_jobs': 'Sao Paulo', #'municipio_por_empregos' 
+	#tab-geral
+	'county_for_jobs': unicode('Sāo Paulo', 'utf8'), #'municipio_por_empregos' 
 	'num_jobs_county' : 1.62 , #num_empregos_principal_municipio
 	'jobs_county_unity' : 'milhares', #'unidade_empregos_principal_municipio'
 	'activity_for_job' : 'atividade x', #atividade_por_empregos
 	'num_activity_for_job': 1.0, #valor_atividade_por_empregos
 	'activity_for_job_unit': unicode('bilhāo','utf8'), #unidade_atividade_por_empregos
+	'county_bigger_average_monsthly_income': unicode('Sāo Paulo', 'utf8'),
 	'bigger_average_monsthly_income': 12.3, #MUNICIPO Valor_maior_renda_media_mensal
 	'bigger_average_monsthly_income_unity': unicode('bilhões','utf-8'),
 	'activity_higher_income': 'Desenvolvimento Sob Encomenda ',  #atividade_maior_renda
-	'value_activity_higher_income' : 990 #
+	'value_activity_higher_income' : 990 
+	#tab-salario-emprego - utiliza as mesmas variaveis da tab geral salario e emprego
+
 } 
 
 @mod.route('/')
