c30c12e93d5e21a7f736a226e6b241b3109ce453
lauradebella <lauradebella@gmail.com>
2016-01-27 08:44:42 -0200

    Mudando as variaveis para ingles.

--- dataviva/apps/occupation/views.py
+++ dataviva/apps/occupation/views.py
@@ -17,36 +17,36 @@ def pull_lang_code(endpoint, values):
 def add_language_code(endpoint, values):
     values.setdefault('lang_code', get_locale())
 
-dic = {
+dicionario = {
 	#index
 	'occupation' : 'Engenheiros em Computacao', 
 	'text_profile':'Engenharia de Computacao e o ramo da engenharia que lida com a realizacao de projeto e construcaoo de computadores e de sistemas que integram hardware e software, viabilizando a producao de novas maquinas e de equipamentos computacionais para serem utilizados em diversos setores.',
 	'family' : True,
 	#header 
-	'renda_media_mensal' : 8, #Average monthly income
-	'unidade_renda_media_mensal': 'Milhares',
-	'massa_salarial': 17.9, #salary mass
-	'unidade_massa_salarial' : 'mil',
-	'total_empregos' : 1.6, #total employment
-	'unidade_total_empregos' : 'milhares', 
-	'total_estabelecimentos': 6.8, #total number of establishments
-	'unidade_total_estabelecimentos' : 'milhoes',
+	'average_monthly_income' : 8, #'renda_media_mensal' 
+	'average_monthly_income_unity' : 'Milhares', #'unidade_renda_media_mensal'
+	'salary_mass': 17.9, #'massa_salarial'
+	'salary_mass_unity' : 'mil',
+	'total_employment' : 1.6, #total de empregos
+	'total_employment_unity' : 'milhares', 
+	'total_establishments' : 6.8, #'total_estabelecimentos'
+	'total_establishments_unity' : 'milhares', #'unidade_total_estabelecimentos' 
 	#tabela responsiva 
-	'municipio_por_empregos' : 'Sao Paulo',
-	'num_empregos_principal_municipio' : 1.62 ,
-	'unidade_empregos_principal_municipio' : 'milhares',
-	'atividade_por_empregos' : 'atividade x', #activity for job
-	'valor_atividade_por_empregos': 1.0,
-	'unidade_atividade_por_empregos': 'bilhao',
-	'valor_maior_renda_media_mensal': 12.3, #Bigger Average monthly income
-	'unidade_maior_renda_media_mensal': 'bilhoes',
-	'atividade_maior_renda': 'Desenvolvimento Sob Encomenda ', 
-	'valor_atividade_maior_renda' : 990
+	'county_for_jobs': 'Sao Paulo', #'municipio_por_empregos' 
+	'num_jobs_county' : 1.62 , #num_empregos_principal_municipio
+	'jobs_county_unity' : 'milhares', #'unidade_empregos_principal_municipio'
+	'activity_for_job' : 'atividade x', #atividade_por_empregos
+	'num_activity_for_job': 1.0, #valor_atividade_por_empregos
+	'activity_for_job_unit': 'bilhao', #unidade_atividade_por_empregos
+	'bigger_average_monsthly_income': 12.3, #Bigger Average monthly income valor_maior_renda_media_mensal
+	'bigger_average_monsthly_income_unity': 'bilhoes',
+	'activity_higher_income': 'Desenvolvimento Sob Encomenda ',  #atividade_maior_renda
+	'value_activity_higher_income' : 990 #
 } 
 
 @mod.route('/')
 def index():
-	return render_template('occupation/index.html', body_class='perfil-estado', dic = dic)
+	return render_template('occupation/index.html', body_class='perfil-estado', dic = dicionario)
 
 
 	
