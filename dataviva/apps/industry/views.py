da1ccad0c677bc0f032c1a1d124ddaa283b952d3
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-01-27 11:34:53 -0200

    problema encode resolvido

--- dataviva/apps/industry/views.py
+++ dataviva/apps/industry/views.py
@@ -2,6 +2,8 @@
 from flask import Blueprint, render_template, g
 from dataviva.apps.general.views import get_locale
 
+
+
 mod = Blueprint('industry', __name__,
                 template_folder='templates',
                 url_prefix='/<lang_code>/industry',
@@ -30,16 +32,14 @@ Template css para alinhar headers
 ''' 
 
 
-
-
 @mod.route('/')
 def index():
-    dic = { 'industry_name': 'Supermercados', 
+    context = { 'industry_name': unicode('Supermercados', 'utf8'), 
             'location' : True ,
             'class' : True,
             'year' : 2010,
-            'background_image':  "'static/img/bg-profile-location.jpg'", 
-            'portrait' : 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7748245.803118934!2d-49.94643868147362!3d-18.514293729997753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa690a165324289%3A0x112170c9379de7b3!2sMinas+Gerais!5e0!3m2!1spt-BR!2sbr!4v1450524997110', 
+            'background_image':  unicode("'static/img/bg-profile-location.jpg'", 'utf8'),
+            'portrait' : unicode('https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7748245.803118934!2d-49.94643868147362!3d-18.514293729997753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa690a165324289%3A0x112170c9379de7b3!2sMinas+Gerais!5e0!3m2!1spt-BR!2sbr!4v1450524997110', 'utf8') ,
             'average_monthly_income' : 371,
             'salary_mass' : 17.9,
             'total_jobs' : 6.8, 
@@ -48,13 +48,15 @@ def index():
             'distance' : 75.3,
             'opportunity_gain' : 17.9,
 
-            'occupation_max_number_jobs_name' : 'Ministerio de Ferro',
+            'occupation_max_number_jobs_name' : unicode('Ministério de Ferro', 'utf8'),
             'occupation_max_number_jobs_value' : 12.2,
-            'county_max_number_jobs_name' : 'China',
+            'county_max_number_jobs_name' : unicode('China', 'utf8'),
             'county_max_number_jobs_value' : 8.82,            
             'occupation_max_monthly_income' : 29.3,
             'county_max_monthly_income' : 11, 
-            'text_profile' : 'Texto de perfil para Supermercados.'       
+            'text_profile' : unicode('Texto de perfil para Supermercados.', 'utf8')       
     }
-    return render_template('industry/index.html', body_class='perfil-estado', dic=dic)
+
+
+    return render_template('industry/index.html', body_class='perfil-estado', context=context)
 
