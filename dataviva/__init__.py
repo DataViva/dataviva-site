da13868cca6c7dcc5e263a256f8e0b31d1c7cee5
Victor Moraes <vdmoraes94@gmail.com>
2016-01-28 11:07:12 -0200

    Finishing Major

--- dataviva/__init__.py
+++ dataviva/__init__.py
@@ -99,6 +99,7 @@ from dataviva.apps.data.views import mod as data_module
 from dataviva.apps.general.views import mod as general_module
 from dataviva.apps.graphs.views import mod as graphs_module
 from dataviva.apps.location.views import mod as location_module
+from dataviva.apps.major.views import mod as major_module
 from dataviva.apps.occupation.views import mod as occupation_module
 from dataviva.apps.rankings.views import mod as rankings_module
 from dataviva.apps.university.views import mod as university_module
@@ -123,6 +124,7 @@ app.register_blueprint(ask_module)
 app.register_blueprint(rankings_module)
 app.register_blueprint(about_module)
 app.register_blueprint(location_module)
+app.register_blueprint(major_module)
 app.register_blueprint(occupation_module)
 app.register_blueprint(university_module)
 app.register_blueprint(wizard_module)
