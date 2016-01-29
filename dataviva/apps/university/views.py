e8235a740f439c277eebd066dd0c75da1d0dafa8
Victor Moraes <vdmoraes94@gmail.com>
2016-01-29 16:42:53 -0200

    Fixed enrolled variable names

--- dataviva/apps/university/views.py
+++ dataviva/apps/university/views.py
@@ -31,7 +31,7 @@ def index():
             Yu.university_id == university_id,
             Yu.year == yu_max_year_query)
 
-    yuc_enrollments_query = Yuc.query.join(Course_hedu).filter(
+    yuc_enrolled_query = Yuc.query.join(Course_hedu).filter(
             Yuc.university_id == university_id,
             Yuc.year == yuc_max_year_query,
             func.length(Yuc.course_hedu_id) == 6).order_by(desc(Yuc.enrolled)).limit(1)
@@ -55,7 +55,7 @@ def index():
         University.desc_pt
     )
 
-    yuc_enrollments_query_data = yuc_enrollments_query.values(
+    yuc_enrolled_query_data = yuc_enrolled_query.values(
         Course_hedu.name_pt,
         Yuc.enrolled,
         Course_hedu.desc_pt
@@ -75,7 +75,7 @@ def index():
 
     for name_pt, enrolled, entrants, graduates, year, profile in yu_query_data:
         university['name'] = name_pt
-        university['enrollments'] = enrolled
+        university['enrolled'] = enrolled
         university['entrants'] = entrants
         university['graduates'] =  graduates
         university['profile'] = profile
@@ -83,9 +83,9 @@ def index():
 
     course = {}
 
-    for name_pt, enrolled, profile in yuc_enrollments_query_data:
-        course['enrollments_name'] = name_pt
-        course['enrollments'] = enrolled
+    for name_pt, enrolled, profile in yuc_enrolled_query_data:
+        course['enrolled_name'] = name_pt
+        course['enrolled'] = enrolled
         course['profile'] = profile
 
     for name_pt, entrants in yuc_entrants_query_data:
