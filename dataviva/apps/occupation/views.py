ef86fc61ce6373bb51f5b7dda7ac3860b60f3be0
lauradebella <lauradebella@gmail.com>
2016-01-29 15:57:42 -0200

    Alterando a variavel location_id para bra_id

--- dataviva/apps/occupation/views.py
+++ dataviva/apps/occupation/views.py
@@ -27,114 +27,118 @@ def add_language_code(endpoint, values):
 @mod.route('/')
 def index():
 
-	occupation_id = '2122'
-	location_id = '1ac'
-	has_location = True
+    occupation_id = '2122'
+    bra_id = '1ac'
 
-	#se tiver sido selecionada uma localidade especific
-	if has_location:
+    #se tiver sido selecionada uma localidade especific
+    if bra_id:
 
-		#encontrando o ano mais recente 
-		ybo_max_year= db.session.query(func.max(Ybo.year)).filter(
-			Ybo.cbo_id == occupation_id, 
-			Ybo.bra_id == location_id)\
-			.one()
-			
-		year = 0
-		for year in ybo_max_year:
-			year = year
+        #encontrando o ano mais recente 
+        ybo_max_year= db.session.query(func.max(Ybo.year)).filter(
+            Ybo.cbo_id == occupation_id, 
+            Ybo.bra_id == bra_id)\
+            .one()
+            
+        year = 0
+        for year in ybo_max_year:
+            year = year
 
-		ybo_header_generator = Ybo.query.join(Cbo).filter(
-			Ybo.cbo_id == occupation_id,
-			Ybo.bra_id == location_id,
-			Ybo.year == year)\
-			.values(Cbo.name_pt,
-					Ybo.wage_avg,
-					Ybo.wage,
-					Ybo.num_jobs,
-					Ybo.num_est)
+        ybo_header_generator = Ybo.query.join(Cbo).filter(
+            Ybo.cbo_id == occupation_id,
+            Ybo.bra_id == bra_id,
+            Ybo.year == year)\
+            .values(Cbo.name_pt,
+                    Ybo.wage_avg,
+                    Ybo.wage,
+                    Ybo.num_jobs,
+                    Ybo.num_est)
 
-		ybo_county_num_jobs_generator = Ybo.query.join(Bra).filter(
-				Ybo.cbo_id == occupation_id,
-				Ybo.bra_id.like(location_id+'%'),
-				Ybo.year == year,
-				Ybo.bra_id_len == 9)\
-			.order_by(desc(Ybo.num_jobs)).limit(1)\
-			.values(Bra.name_pt,
-					Ybo.num_jobs)
+        ybo_county_num_jobs_generator = Ybo.query.join(Bra).filter(
+                Ybo.cbo_id == occupation_id,
+                Ybo.bra_id.like(bra_id+'%'),
+                Ybo.year == year,
+                Ybo.bra_id_len == 9)\
+            .order_by(desc(Ybo.num_jobs)).limit(1)\
+            .values(Bra.name_pt,
+                    Ybo.num_jobs)
 
-		ybo_county_wage_avg_generator = Ybo.query.join(Bra).filter(
-				Ybo.cbo_id == occupation_id,
-				Ybo.bra_id.like(location_id+'%'),
-				Ybo.year == year,
-				Ybo.bra_id_len == 9)\
-			.order_by(desc(Ybo.wage_avg)).limit(1)\
-			.values(Bra.name_pt,
-					Ybo.wage_avg)
-		
+        ybo_county_wage_avg_generator = Ybo.query.join(Bra).filter(
+                Ybo.cbo_id == occupation_id,
+                Ybo.bra_id.like(bra_id+'%'),
+                Ybo.year == year,
+                Ybo.bra_id_len == 9)\
+            .order_by(desc(Ybo.wage_avg)).limit(1)\
+            .values(Bra.name_pt,
+                    Ybo.wage_avg)
+        
 
-		ybio_activity_num_jobs_generator = Ybio.query.join(Cnae).filter(
-				Ybio.cbo_id == occupation_id,
-				Ybio.bra_id.like(location_id+'%'),
-				Ybio.year == year,
-				Ybio.cnae_id_len == 6)\
-			.order_by(desc(Ybio.num_jobs)).limit(1)\
-			.values(Cnae.name_pt,
-					Ybio.num_jobs)
-		
+        ybio_activity_num_jobs_generator = Ybio.query.join(Cnae).filter(
+                Ybio.cbo_id == occupation_id,
+                Ybio.bra_id.like(bra_id+'%'),
+                Ybio.year == year,
+                Ybio.cnae_id_len == 6)\
+            .order_by(desc(Ybio.num_jobs)).limit(1)\
+            .values(Cnae.name_pt,
+                    Ybio.num_jobs)
+        
 
-		ybio_activity_wage_avg_generator = Ybio.query.join(Cnae).filter(
-				Ybio.cbo_id == occupation_id,
-				Ybio.bra_id.like(location_id+'%'),
-				Ybio.year == year,
-				Ybio.cnae_id_len == 6)\
-			.order_by(desc(Ybio.wage_avg)).limit(1)\
-			.values(Cnae.name_pt,
-					Ybio.wage_avg)
+        ybio_activity_wage_avg_generator = Ybio.query.join(Cnae).filter(
+                Ybio.cbo_id == occupation_id,
+                Ybio.bra_id.like(bra_id+'%'),
+                Ybio.year == year,
+                Ybio.cnae_id_len == 6)\
+            .order_by(desc(Ybio.wage_avg)).limit(1)\
+            .values(Cnae.name_pt,
+                    Ybio.wage_avg)
 
-		
-		header = {}
-		body = {}
-		header['year'] = year
-		for name_pt, wage_avg, wage, num_jobs, num_est in ybo_header_generator:
-			header['name'] = name_pt
-			header['average_monthly_income'] = wage_avg
-			header['salary_mass'] = wage
-			header['total_employment'] = num_jobs
-			header['total_establishments'] = num_est
-		for name_pt, num_jobs in ybo_county_num_jobs_generator:
-			body['county_for_jobs'] = name_pt
-			body['num_jobs_county'] = num_jobs
-		for name_pt, wage_avg in ybio_activity_wage_avg_generator:
-			body['activity_higher_income'] = name_pt
-			body['value_activity_higher_income'] = wage_avg		
-		for name_pt, wage_avg in ybo_county_wage_avg_generator:
-			body['county_bigger_average_monsthly_income'] = name_pt
-			body['bigger_average_monsthly_income'] = wage_avg	
-		for name_pt, num_jobs in ybio_activity_num_jobs_generator:
-			body['activity_for_job'] = name_pt
-			body['num_activity_for_job'] = num_jobs	
+        
+        header = {}
+        body = {}
+        header['year'] = year
+
+        for name_pt, wage_avg, wage, num_jobs, num_est in ybo_header_generator:
+            header['name'] = name_pt
+            header['average_monthly_income'] = wage_avg
+            header['salary_mass'] = wage
+            header['total_employment'] = num_jobs
+            header['total_establishments'] = num_est
+
+        for name_pt, num_jobs in ybo_county_num_jobs_generator:
+            body['county_for_jobs'] = name_pt
+            body['num_jobs_county'] = num_jobs
+
+        for name_pt, wage_avg in ybio_activity_wage_avg_generator:
+            body['activity_higher_income'] = name_pt
+            body['value_activity_higher_income'] = wage_avg     
+
+        for name_pt, wage_avg in ybo_county_wage_avg_generator:
+            body['county_bigger_average_monsthly_income'] = name_pt
+            body['bigger_average_monsthly_income'] = wage_avg   
+
+        for name_pt, num_jobs in ybio_activity_num_jobs_generator:
+            body['activity_for_job'] = name_pt
+            body['num_activity_for_job'] = num_jobs 
 
 
-	context = {
-		'family' : True,
-		#unidades
-		'average_monthly_income_unity' : 'Milhares', #'unidade_renda_media_mensal'
-		'salary_mass_unity' : 'mil',
-		'total_employment_unity' : 'milhares', 
-		'total_establishments_unity' : 'milhares', #'unidade_total_estabelecimentos' 
-		#unidades
-		'jobs_county_unity' : 'milhares de', #'unidade_empregos_principal_municipio'
-		'activity_for_job_unity': unicode('bilhões','utf8'), #unidade_atividade_por_empregos
-		'bigger_average_monsthly_income_unity': unicode('bilhões','utf8'),
-		'activity_for_job_unity' : unicode('bilhoes','utf8'),
-		#tab-salario-emprego
-		'text_salario_e_emprego': unicode('Minas Gerais é uma das 27 unidades feder...','utf8'),
-		#tab-oportunidades-economicas
-		'text_oportunidades_economicas' : unicode('Minas Gerais é uma das 27 unidades federativas do Brasil, localizada na Região Sudeste ','utf8')
+    context = {
+        'family' : True,
+        #unidades
+        'average_monthly_income_unity' : 'Milhares', #'unidade_renda_media_mensal'
+        'salary_mass_unity' : 'mil',
+        'total_employment_unity' : 'milhares', 
+        'total_establishments_unity' : 'milhares', #'unidade_total_estabelecimentos' 
+        #unidades
+        'jobs_county_unity' : 'milhares de', #'unidade_empregos_principal_municipio'
+        'activity_for_job_unity': unicode('bilhões','utf8'), #unidade_atividade_por_empregos
+        'bigger_average_monsthly_income_unity': unicode('bilhões','utf8'),
+        'activity_for_job_unity' : unicode('bilhoes','utf8'),
+        #tab-salario-emprego
+        'text_salario_e_emprego': unicode('Minas Gerais é uma das 27 unidades feder...','utf8'),
+        #tab-oportunidades-economicas
+        'text_oportunidades_economicas' : unicode('Minas Gerais é uma das 27 unidades federativas do Brasil, localizada na Região Sudeste ','utf8')
 
-	} 
-	#acessar o contex do diogo com context.id.oqeuquero
+    } 
+    #acessar o contex do diogo com context.id.oqeuquero
 
-	
-	return render_template('occupation/index.html', body_class='perfil-estado', context=context, header = header, body = body)
+    
+    return render_template('occupation/index.html', body_class='perfil-estado', context=context, header = header, body = body)
