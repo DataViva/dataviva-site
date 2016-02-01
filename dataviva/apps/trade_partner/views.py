0bbe2358e06bf837081df1fa13d619ec26f2396c
Victor Moraes <vdmoraes94@gmail.com>
2016-02-01 13:21:14 -0200

    Done county exporting data retrieval

--- dataviva/apps/trade_partner/views.py
+++ dataviva/apps/trade_partner/views.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template, g
 from dataviva.apps.general.views import get_locale
-from dataviva.api.attrs.models import Wld
-from dataviva.api.secex.models import Ymw
+from dataviva.api.attrs.models import Wld, Bra, Hs
+from dataviva.api.secex.models import Ymw, Ymbw, Ympw
 from dataviva import db
 from sqlalchemy.sql.expression import func, desc
 
@@ -27,11 +27,26 @@ def index():
 
 	''' Queries que pegam o ano mais recente dos dados '''
 	ymw_max_year = db.session.query(func.max(Ymw.year)).filter_by(wld_id=wld_id)
+	ymbw_max_year = db.session.query(func.max(Ymbw.year)).filter_by(wld_id=wld_id)
 
 	ymw_query = Ymw.query.join(Wld).filter(
 		Ymw.wld_id == wld_id,
 		Ymw.month == 0,
 		Ymw.year == ymw_max_year)
+
+	ymbw_county_export_query = Ymbw.query.join(Bra).filter(
+		Ymbw.wld_id == wld_id,
+		Ymbw.month == 0,
+		Ymbw.year == ymbw_max_year,
+		func.length(Ymbw.bra_id) == 9).order_by(desc(Ymbw.export_val)).limit(1)
+
+	ymbw_county_import_query = Ymbw.query.join(Bra).filter(
+		Ymbw.wld_id == wld_id,
+		Ymbw.month == 0,
+		Ymbw.year == ymbw_max_year,
+		func.length(Ymbw.bra_id) == 9).order_by(desc(Ymbw.import_val)).limit(1)
+
+	ympw_county_export_query = Ympw.query.join(Hs)
 
 	ymw_data = ymw_query.values(
 		Wld.name_pt,
@@ -42,15 +57,35 @@ def index():
 		Ymw.import_val,
 		(Ymw.import_kg/Ymw.import_val))
 
-	country = {}
+	ymbw_county_export_data = ymbw_county_export_query.values(
+		Bra.name_pt,
+		Ymbw.export_val)
 
-	for name_pt, year, trade_balance, total_exported in ymw_data:
+	ymbw_county_import_data = ymbw_county_import_query.values(
+		Bra.name_pt,
+		Ymbw.import_val)
+
+	country = {}
+	trade = {}
+
+	for name_pt, year, trade_balance, total_exported, unity_weight_export_price, total_imported, unity_weight_import_price in ymw_data:
 		country['name'] = name_pt
 		country['year'] = year
 		country['trade_balance'] = trade_balance
 		country['total_exported'] = total_exported
+		country['unity_weight_export_price'] = unity_weight_export_price
+		country['total_imported'] = total_imported
+		country['unity_weight_import_price'] = unity_weight_import_price
 
-	return render_template('trade_partner/index.html', body_class='perfil-estado', country=country)
+	for name_pt, export_val in ymbw_county_export_data:
+		trade['leading_export_county'] = name_pt
+		trade['leading_export_county_value'] = export_val
+
+	for name_pt, import_val in ymbw_county_import_data:
+		trade['leading_import_county'] = name_pt
+		trade['leading_import_county_value'] = import_val
+
+	return render_template('trade_partner/index.html', body_class='perfil-estado', country=country, trade=trade)
 
 
 	
