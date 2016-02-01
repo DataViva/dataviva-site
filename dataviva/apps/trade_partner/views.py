69a9a037b6440ea0a64248641556a049b719bec0
Victor Moraes <vdmoraes94@gmail.com>
2016-02-01 12:31:18 -0200

    Started data retrieval for Trade Partner

--- dataviva/apps/trade_partner/views.py
+++ dataviva/apps/trade_partner/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template, g
 from dataviva.apps.general.views import get_locale
+from dataviva.api.attrs.models import Wld
+from dataviva.api.secex.models import Ymw
+from dataviva import db
+from sqlalchemy.sql.expression import func, desc
 
 mod = Blueprint('trade_partner', __name__,
                 template_folder='templates',
@@ -17,44 +21,36 @@ def pull_lang_code(endpoint, values):
 def add_language_code(endpoint, values):
     values.setdefault('lang_code', get_locale())
 
-context = {
-	#index
-	'name' : unicode('China', 'utf8') ,
-	'text_profile': unicode('Texto sobre a parceria economica entre Brasil e China.'),
-	'background_image': unicode("'static/img/bg-profile-location.jpg'", 'utf8'),
-	'year' : 2010,
-	#header 
-	'trade_balance' : 80, #balanca comercial 
-	'trade_balance_unity' : 'Bilhoes', 
-	'total_exported': 17.9, #'total exportado
-	'total_exported_unity' : 'milhoes',
-	'weight_exported_value' : 1.6, #pelo/valor exportado
-	'weight_exported_value_unity' : 'milhares', 
-	'total_imported': 17.9, #'total exportado
-	'total_imported_unity' : 'milhoes',
-	'weight_imported_value' : 1.6, #pelo/valor exportado
-	'weight_imported_value_unity' : 'milharess', 
-	#tab-geral e valores tab-comercio-internacional
-	'county_for_exported_value': unicode('Sāo Paulo', 'utf8'), #'municipio_por_valor exportado' 
-	'num_county_for_exported_value' : 1.62 , 
-	'county_for_imported_value': unicode('Municipio X', 'utf8'), #'municipio_por_valor importado' 
-	'num_county_for_imported_value' : 2.62 , 
-	'product_for_exported_value': unicode('Minerio', 'utf8'), #'produto_por_valor exportado' 
-	'num_product_for_exported_value' : 3.62 , 
-	'product_for_imported_value': unicode('Produto X', 'utf8'), #'produto_por_valor importado' 
-	'num_product_for_imported_value' : 4.62 , 
-	'product_bigger_trade_balance' : unicode('Produto Y', 'utf8'),
-	'value_product_bigger_trade_balance' : 10.88,
-	'product_smaller_trade_balance' : unicode('Produto Z', 'utf8'),
-	'value_product_smaller_trade_balance' : 1.88,
-	#tab-comercio-internacional
-	'text_comercio_internacional' : 'Texto sobre o comercio internacional. '
-	
-} 
-
 @mod.route('/')
 def index():
-	return render_template('trade_partner/index.html', body_class='perfil-estado', context=context)
+	wld_id = 'nausa'
+
+	''' Queries que pegam o ano mais recente dos dados '''
+	ymw_max_year = db.session.query(func.max(Ymw.year)).filter_by(wld_id=wld_id)
+
+	ymw_query = Ymw.query.join(Wld).filter(
+		Ymw.wld_id == wld_id,
+		Ymw.month == 0,
+		Ymw.year == ymw_max_year)
+
+	ymw_data = ymw_query.values(
+		Wld.name_pt,
+		Ymw.year,
+		(Ymw.export_val-Ymw.import_val),
+		Ymw.export_val,
+		(Ymw.export_kg/Ymw.export_val),
+		Ymw.import_val,
+		(Ymw.import_kg/Ymw.import_val))
+
+	country = {}
+
+	for name_pt, year, trade_balance, total_exported in ymw_data:
+		country['name'] = name_pt
+		country['year'] = year
+		country['trade_balance'] = trade_balance
+		country['total_exported'] = total_exported
+
+	return render_template('trade_partner/index.html', body_class='perfil-estado', country=country)
 
 
 	
