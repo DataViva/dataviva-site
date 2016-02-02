a9397080b58cbe92f11b77bb018ee07d8ce576ad
Diogo Lundberg <diogo@lundberg.com.br>
2016-02-02 12:12:49 -0200

    Views now uses the attrs services layer.

--- dataviva/apps/location/views.py
+++ dataviva/apps/location/views.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template, g
 from dataviva.apps.general.views import get_locale
-from dataviva.api.attrs.models import Ybs, Stat
+from dataviva.api.attrs.services import Location as AttrsLocationService
 from dataviva.api.secex.models import Ymb
-from dataviva import db
 from sqlalchemy import func, desc
 
 mod = Blueprint('location', __name__,
@@ -25,60 +24,11 @@ def add_language_code(endpoint, values):
 @mod.route('/')
 def index():
 
-    bra_id = '4mg'
-
-    # subquery do ano máximo YBS
-    ybs_max_year = db.session.query(
-        func.max(Ybs.year)).filter_by(bra_id=bra_id)
-
-    ''' Query básica para YBS'''
-    ybs_query = Ybs.query.filter_by(bra_id=bra_id).join(Stat)
-    # Exemplo de um resultado
-    gdp = ybs_query.filter_by(id='gdp') \
-        .first().stat_val
-    # Exemplo de um resultado usando a subquery
-    life_expectancy = ybs_query.filter(
-        Stat.id == 'life_exp',
-        Ybs.year == ybs_max_year) \
-        .first().stat_val
-    # Query com todos os filtros no mesmo lugar
-    population = Ybs.query.join(Stat).filter(
-        Ybs.bra_id == bra_id,
-        Ybs.year == ybs_max_year,
-        Stat.id == 'pop') \
-        .first().stat_val
-
-    ''' Método mais genérico que retorna todos os valores'''
-    ybs_list = Ybs.query.join(Stat).filter(
-        Ybs.bra_id == bra_id,
-        Ybs.year == ybs_max_year) \
-        .all()
-    # Retirando um valor da lista de objetos YBS
-    gdp_pc = filter(lambda x: x.stat_id == 'gdp_pc', ybs_list)[0].stat_val
-
-    ''' Método listando todas as estatisticas, mas definindo colunas'''
-    # O que é um generator?
-    # https://wiki.python.org/moin/Generators
-    # ou
-    # http://anandology.com/python-practice-book/iterators.html#generators
-    statistics_generator = Ybs.query.join(Stat).filter(
-        Ybs.bra_id == bra_id,
-        Ybs.year == ybs_max_year) \
-        .values(Stat.id,
-                Stat.name_pt,
-                Ybs.stat_val)
-
-    # Veja como utilizar este generator, iterando para pegar criar um dict
-    context = {}
-
-    for id, name_pt, stat_val in statistics_generator:
-        context[id] = {
-            'name': name_pt,
-            'value': stat_val,
-        }
+    location_service = AttrsLocationService(bra_id='4mg')
+    location_statistics = attrs_location_service.statistics()
 
     ''' Query básica para SECEX'''
     context['eci'] = Ymb.query.filter_by(bra_id=bra_id, month=0) \
         .order_by(desc(Ymb.year)).limit(1).first().eci
 
-    return render_template('location/index.html', context=context, body_class='perfil-estado')
+    return render_template('location/index.html', location_statistics=location_statistics, body_class='perfil-estado')
