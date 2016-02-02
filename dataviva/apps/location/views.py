65d57e68cd3e4a718a2e1245c6c6a7f5549a074d
Diogo Lundberg <diogo@lundberg.com.br>
2016-02-02 14:59:30 -0200

    Fixing eci statistics, point to the right object.

--- dataviva/apps/location/views.py
+++ dataviva/apps/location/views.py
@@ -2,7 +2,9 @@
 from flask import Blueprint, render_template, g
 from dataviva.apps.general.views import get_locale
 from dataviva.api.attrs.services import Location as AttrsLocationService
+from dataviva.api.attrs.models import Bra
 from dataviva.api.secex.models import Ymb
+from sqlalchemy import desc
 
 mod = Blueprint('location', __name__,
                 template_folder='templates',
@@ -20,14 +22,14 @@ def add_language_code(endpoint, values):
     values.setdefault('lang_code', get_locale())
 
 
-@mod.route('/')
-def index():
-    bra_id = '4mg'
-    attrs_location_service = AttrsLocationService(bra_id='4mg')
+@mod.route('/<bra_id>')
+def index(bra_id):
+
+    attrs_location_service = AttrsLocationService(bra_id=bra_id)
     location_statistics = attrs_location_service.statistics()
 
     ''' Query básica para SECEX'''
-    context['eci'] = Ymb.query.filter_by(bra_id=bra_id, month=0) \
+    location_statistics['eci'] = Ymb.query.filter_by(bra_id=bra_id, month=0) \
         .order_by(desc(Ymb.year)).limit(1).first().eci
 
     return render_template('location/index.html', location_statistics=location_statistics, body_class='perfil-estado')
