79cdd4268b0c5a1c60d3d8295b23935a0e77f83f
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-02-02 21:37:38 -0300

    Criado modulo de controle para controlar a aparição de alguns campos na tela

---
+++ dataviva/apps/industry/controler.py
@@ -0,0 +1,20 @@
+def templates_preview_controler(bra_id, cnae_id):
+    industry = {}
+    if bra_id == None :
+        industry['location'] = False
+        industry['county'] = True # view county where no country
+    else : 
+        industry['location'] = True    
+     
+        if len(bra_id) == 9 : 
+            industry['county'] = False
+        else :
+            industry['county'] = True    
+
+
+    if len(cnae_id) == 1 : 
+        industry['class'] = True
+    else : 
+        industry['class'] = False
+
+    return industry
