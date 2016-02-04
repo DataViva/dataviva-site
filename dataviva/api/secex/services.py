b0718144aed29adbedfe5cfb90d6797e950ee4d0
Diogo Lundberg <diogo@lundberg.com.br>
2016-02-04 17:28:35 -0200

    Now the products related classes on trade partner services are the same class: TradePartnerProducts

--- dataviva/api/secex/services.py
+++ dataviva/api/secex/services.py
@@ -22,7 +22,6 @@ class TradePartner:
         if not self._secex:
             secex_data = self.secex_query.one()
             self._secex = secex_data
-
         return self._secex
 
     def country_name(self):
@@ -35,7 +34,6 @@ class TradePartner:
     def trade_balance(self):
         export_val = self.__secex__().export_val
         import_val = self.__secex__().import_val
-
         return export_val - import_val
 
     def total_exported(self):
@@ -44,7 +42,6 @@ class TradePartner:
     def unity_weight_export_price(self):
         export_val = self.__secex__().export_val
         export_kg = self.__secex__().export_kg
-
         return export_val / export_kg
 
     def total_imported(self):
@@ -53,7 +50,6 @@ class TradePartner:
     def unity_weight_import_price(self):
         import_val = self.__secex__().import_val
         import_kg = self.__secex__().import_kg
-
         return import_val / import_kg
 
 class TradePartnerMunicipalityByExport(TradePartner):
@@ -94,6 +90,7 @@ class TradePartnerMunicipalityByImport(TradePartner):
 
 
 class TradePartnerProductByImport(TradePartner):
+
 
     def __init__(self, wld_id):
         TradePartner.__init__(self, wld_id)
@@ -165,5 +162,77 @@ class TradePartnerProductByLowestBalance(TradePartner):
     def lowest_balance_by_product(self):
         export_val = self.__secex__().export_val or 0
         import_val = self.__secex__().import_val or 0
+        return export_val - import_val
 
+class TradePartnerProducts(TradePartner):
+
+
+    def __init__(self, wld_id):
+        TradePartner.__init__(self, wld_id)
+        self.max_year_query = db.session.query(func.max(Ympw.year)).filter_by(wld_id=wld_id)
+        self.secex_query = Ympw.query.join(Hs).filter(
+            Ympw.wld_id == self.wld_id,
+            Ympw.month == 0,
+            Ympw.hs_id_len == 6,
+            Ympw.year == self.max_year_query)
+
+        self._secex_sorted_by_balance = None
+        self._secex_sorted_by_exports = None
+        self._secex_sorted_by_imports = None
+
+    def __secex__(self):
+        if not self._secex:
+            secex_data = self.secex_query.all()
+            self._secex = secex_data
+        return self._secex
+
+    def __secex_sorted_by_balance__(self):
+        self._secex_sorted_by_balance = self.__secex__()
+        self._secex_sorted_by_balance.sort(key=lambda secex: (secex.export_val or 0) - (secex.import_val or 0), reverse=True)
+        return self._secex_sorted_by_balance
+
+    def __secex_sorted_by_exports__(self):
+        self._secex_sorted_by_exports = self.__secex__()
+        self._secex_sorted_by_exports.sort(key=lambda secex: secex.export_val, reverse=True)
+        return self._secex_sorted_by_exports
+
+    def __secex_sorted_by_imports__(self):
+        self._secex_sorted_by_imports = self.__secex__()
+        self._secex_sorted_by_imports.sort(key=lambda secex: secex.import_val, reverse=True)
+        return self._secex_sorted_by_imports
+
+    def product_with_more_exports(self):
+        secex = self.__secex_sorted_by_exports__()[0]
+        return secex.hs.name()
+
+    def highest_export_value(self):
+        secex = self.__secex_sorted_by_exports__()[0]
+        return secex.export_val
+
+    def product_with_more_imports(self):
+        secex = self.__secex_sorted_by_imports__()[0]
+        return secex.hs.name()
+
+    def highest_import_value(self):
+        secex = self.__secex_sorted_by_imports__()[0]
+        return secex.import_val
+
+    def product_with_highest_balance(self):
+        secex = self.__secex_sorted_by_balance__()[0]
+        return secex.hs.name()
+
+    def highest_balance(self):
+        secex = self.__secex_sorted_by_balance__()[0]
+        export_val = secex.export_val or 0
+        import_val = secex.import_val or 0
+        return export_val - import_val
+
+    def product_with_lowest_balance(self):
+        secex = self.__secex_sorted_by_balance__()[-1]
+        return secex.hs.name()
+
+    def lowest_balance(self):
+        secex = self.__secex_sorted_by_balance__()[-1]
+        export_val = secex.export_val or 0
+        import_val = secex.import_val or 0
         return export_val - import_val
