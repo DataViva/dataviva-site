c3184cfe27b003b2e67534f9da0bd21b8668bfe8
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-02-04 15:00:08 -0200

    add flag for header preview when industry class

--- dataviva/apps/industry/controler.py
+++ dataviva/apps/industry/controler.py
@@ -2,10 +2,10 @@
 def templates_preview_controler(bra_id, cnae_id):
     industry = {}
     if bra_id == None :
-        industry['location'] = False
+        industry['flag_preview_headers'] = False
         industry['county'] = True # view county where no country
     else : 
-        industry['location'] = True    
+        industry['flag_preview_headers'] = True    
      
         if len(bra_id) == 9 : 
             industry['county'] = False
@@ -17,5 +17,8 @@ def templates_preview_controler(bra_id, cnae_id):
         industry['class'] = True
     else : 
         industry['class'] = False
+
+    if len(cnae_id) == 1 :
+        industry['flag_preview_headers'] = False 
 
     return industry
--- dataviva/apps/industry/views.py
+++ dataviva/apps/industry/views.py
@@ -31,6 +31,7 @@ def index(cnae_id):
 
     industry = {}
 
+
     industry = { 
         'background_image':  unicode("'static/img/bg-profile-location.jpg'", 'utf8'),
         'portrait' : unicode('https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7748245.803118934!2d-49.94643868147362!3d-18.514293729997753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa690a165324289%3A0x112170c9379de7b3!2sMinas+Gerais!5e0!3m2!1spt-BR!2sbr!4v1450524997110', 'utf8') ,        
@@ -49,7 +50,6 @@ def index(cnae_id):
         industry['rca'] = rais_industry_service.rca()
         industry['distance'] = rais_industry_service.distance()
         industry['opportunity_gain'] = rais_industry_service.opportunity_gain()
-
     else:
         rais_industry_service = RaisIndustryService(cnae_id=cnae_id)
    
