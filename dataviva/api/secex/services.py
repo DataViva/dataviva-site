7d3f95802e05e0108b0bfe0f99b9b7804916b379
Diogo Lundberg <diogo@lundberg.com.br>
2016-02-04 14:59:26 -0200

    Splitting the services into more specific classes, each one a single query.

--- dataviva/api/secex/services.py
+++ dataviva/api/secex/services.py
@@ -5,6 +5,7 @@ from flask import g
 from sqlalchemy.sql.expression import func, desc, asc
 
 class TradePartner:
+
     def __init__(self, wld_id):
         self._secex = None
         self.wld_id = wld_id
@@ -54,129 +55,105 @@ class TradePartner:
 
         return import_val / import_kg
 
-class TradePartnerByLocation(TradePartner):
+class TradePartnerMunicipalityByExport(TradePartner):
 
-   def __init__(self, wld_id, bra_id):
+   def __init__(self, wld_id):
         super(TradePartnerByLocation, self).__init__(wld_id)
-        self.bra_id = bra_id
         self.max_year_query = db.session.query(func.max(Ymbw.year)).filter_by(wld_id=wld_id)
         self.secex_query = Ymbw.query.join(Bra).filter(
             Ymbw.wld_id == self.wld_id,
             Ymbw.month == 0,
             Ymbw.year == self.max_year_query,
-            func.length(Ymbw.bra_id) == 9).order_by(desc(Ymbw.export_val))
+            func.length(Ymbw.bra_id) == 9).order_by(desc(Ymbw.export_val)).limit(1)
 
     def municipality_with_more_exports(self):
-        ymbw_municipality_export_query = Ymbw.query.join(Bra).filter(
+        return self.__secex__().name()
+
+    def highest_export_value_by_municipality(self):
+        return self.__secex__().export_val
+
+class TradePartnerMunicipalityByImport(TradePartner):
+   def __init__(self, wld_id):
+        super(TradePartnerByLocation, self).__init__(wld_id)
+        self.max_year_query = db.session.query(func.max(Ymbw.year)).filter_by(wld_id=wld_id)
+        self.secex_query = Ymbw.query.join(Bra).filter(
             Ymbw.wld_id == self.wld_id,
             Ymbw.month == 0,
             Ymbw.year == self.max_year_query,
-            func.length(Ymbw.bra_id) == 9).order_by(desc(Ymbw.export_val)).limit(1)
-
-        ymbw_municipality_export_data = ymbw_municipality_export_query.values(
-            Bra.name_pt,
-            Ymbw.export_val)
-
-        municipality = {}
-
-        for name_pt, export_val in ymbw_municipality_export_data:
-            municipality['name'] = name_pt
-            municipality['value'] = export_val
-
-        return municipality
-
-    def municipality_with_more_imports(self):
-        ymbw_municipality_import_query = Ymbw.query.join(Bra).filter(
-            Ymbw.wld_id == self.wld_id,
-            Ymbw.month == 0,
-            Ymbw.year == self.ymbw_max_year,
             func.length(Ymbw.bra_id) == 9).order_by(desc(Ymbw.import_val)).limit(1)
 
-        ymbw_municipality_import_data = ymbw_municipality_import_query.values(
-            Bra.name_pt,
-            Ymbw.import_val)
+    def municipality_with_more_imports(self):
+        return self.__secex__().name()
 
-        municipality = {}
+    def highest_import_value_by_municipality(self):
+        return self.__secex__().import_val
 
-        for name_pt, import_val in ymbw_municipality_import_data:
-            municipality['name'] = name_pt
-            municipality['value'] = import_val
+class TradePartnerProductByImport(TradePartner):
 
-        return municipality
-
-    def product_with_more_exports(self):
-        ympw_product_export_query = Ympw.query.join(Hs).filter(
+   def __init__(self, wld_id):
+        super(TradePartnerByLocation, self).__init__(wld_id)
+        self.max_year_query = db.session.query(func.max(Ympw.year)).filter_by(wld_id=wld_id)
+        self.secex_query = Ympw.query.join(Hs).filter(
             Ympw.wld_id == self.wld_id,
             Ympw.month == 0,
             Ympw.hs_id_len == 6,
-            Ympw.year == self.ympw_max_year).order_by(desc(Ympw.export_val)).limit(1)
-
-        ympw_product_export_data = ympw_product_export_query.values(
-            Hs.name_pt,
-            Ympw.export_val)
-
-        product = {}
-
-        for name_pt, export_val in ympw_product_export_data:
-            product['name'] = name_pt
-            product['value'] = export_val
-
-        return product
+            Ympw.year == self.max_year_query).order_by(desc(Ympw.import_val)).limit(1)
 
     def product_with_more_imports(self):
+        return self.__secex__().name()
 
-        ympw_product_import_query = Ympw.query.join(Hs).filter(
+    def highest_import_value_by_product(self):
+        return self.__secex__().import_val
+
+class TradePartnerProductByExport(TradePartner):
+
+   def __init__(self, wld_id):
+        super(TradePartnerByLocation, self).__init__(wld_id)
+        self.max_year_query = db.session.query(func.max(Ympw.year)).filter_by(wld_id=wld_id)
+        self.secex_query = Ympw.query.join(Hs).filter(
             Ympw.wld_id == self.wld_id,
             Ympw.month == 0,
             Ympw.hs_id_len == 6,
-            Ympw.year == self.ympw_max_year).order_by(desc(Ympw.import_val)).limit(1)
+            Ympw.year == self.max_year_query).order_by(desc(Ympw.export_val)).limit(1)
 
-        ympw_product_import_data = ympw_product_import_query.values(
-            Hs.name_pt,
-            Ympw.import_val)
+    def product_with_more_exports(self):
+        return self.__secex__().name()
 
-        product = {}
+    def highest_export_value_by_product(self):
+        return self.__secex__().export_val
 
-        for name_pt, import_val in ympw_product_import_data:
-            product['name'] = name_pt
-            product['value'] = import_val
 
-        return product
+class TradePartnerProductByHighestBalance(TradePartner):
 
-    def product_with_highest_balance(self):
+   def __init__(self, wld_id):
+        super(TradePartnerByLocation, self).__init__(wld_id)
+        self.max_year_query = db.session.query(func.max(Ympw.year)).filter_by(wld_id=wld_id)
         ympw_highest_balance_query = Ympw.query.join(Hs).filter(
             Ympw.wld_id == self.wld_id,
             Ympw.month == 0,
             Ympw.hs_id_len == 6,
-            Ympw.year == self.ympw_max_year).order_by(desc(Ympw.export_val-Ympw.import_val)).limit(1)
+            Ympw.year == self.max_year_query).order_by(desc(Ympw.export_val-Ympw.import_val)).limit(1)
 
-        ympw_highest_balance_data = ympw_highest_balance_query.values(
-            Hs.name_pt,
-            (Ympw.export_val - Ympw.import_val))
+    def product_with_highest_balance(self):
+        return self.__secex__().name()
 
-        product = {}
+    def highest_balance_by_product(self):
+        return self.__secex__().export_val - self.__secex__().import_val
 
-        for name_pt, trade_balance in ympw_highest_balance_data:
-            product['name'] = name_pt
-            product['value'] = trade_balance
 
-        return product
+class TradePartnerProductByLowestBalance(TradePartner):
 
-    def product_with_lowest_balance(self):
-        ympw_lowest_balance_query = Ympw.query.join(Hs).filter(
+   def __init__(self, wld_id):
+        super(TradePartnerByLocation, self).__init__(wld_id)
+        self.max_year_query = db.session.query(func.max(Ympw.year)).filter_by(wld_id=wld_id)
+        ympw_highest_balance_query = Ympw.query.join(Hs).filter(
             Ympw.wld_id == self.wld_id,
             Ympw.month == 0,
             Ympw.hs_id_len == 6,
-            Ympw.year == self.ympw_max_year).order_by(asc(Ympw.export_val-Ympw.import_val)).limit(1)
+            Ympw.year == self.max_year_query).order_by(asc(Ympw.export_val-Ympw.import_val)).limit(1)
 
-        ympw_lowest_balance_data = ympw_lowest_balance_query.values(
-            Hs.name_pt,
-            (Ympw.export_val - Ympw.import_val))
+    def product_with_lowest_balance(self):
+        return self.__secex__().name()
 
-        product = {}
-
-        for name_pt, trade_balance in ympw_lowest_balance_data:
-            product['name'] = name_pt
-            product['value'] = trade_balance
-
-        return product
+    def lowest_balance_by_product(self):
+        return self.__secex__().export_val - self.__secex__().import_val
