59d77b2fc344bbc2520b658ef0b890a39da4eec2
Diogo Lundberg <diogo@lundberg.com.br>
2016-02-04 14:03:51 -0200

    Now the secex query is reused for inheritance.

--- dataviva/api/secex/services.py
+++ dataviva/api/secex/services.py
@@ -6,61 +6,71 @@ from sqlalchemy.sql.expression import func, desc, asc
 
 class TradePartner:
     def __init__(self, wld_id):
-        self._country_info = None
-
+        self._secex = None
         self.wld_id = wld_id
-        self.ymw_max_year = db.session.query(func.max(Ymw.year)).filter_by(wld_id=wld_id)
-        self.ymbw_max_year = db.session.query(func.max(Ymbw.year)).filter_by(wld_id=wld_id)
-        self.ympw_max_year = db.session.query(func.max(Ympw.year)).filter_by(wld_id=wld_id)
+
+        self.max_year_query = db.session.query(func.max(Ymw.year)).filter_by(wld_id=wld_id)
 
         self.secex_query = Ymw.query.join(Wld).filter(
             Ymw.wld_id == self.wld_id,
             Ymw.month == 0,
-            Ymw.year == self.ymw_max_year)
+            Ymw.year == self.max_year_query)
 
-    def __country_info__(self):
-        if not self._country_info:
+    def __secex__(self):
+        if not self._secex:
             secex_data = self.secex_query.one()
-            self._country_info = secex_data
+            self._secex = secex_data
 
-        return self._country_info
+        return self._secex
 
     def country_name(self):
-        base_trade_partner = self.__country_info__().wld
+        base_trade_partner = self.__secex__().wld
         return base_trade_partner.name()
 
     def year(self):
-        return self.__country_info__().year
+        return self.__secex__().year
 
     def trade_balance(self):
-        export_val = self.__country_info__().export_val
-        import_val = self.__country_info__().import_val
+        export_val = self.__secex__().export_val
+        import_val = self.__secex__().import_val
 
         return export_val - import_val
 
     def total_exported(self):
-        return self.__country_info__().export_val
+        return self.__secex__().export_val
 
     def unity_weight_export_price(self):
-        export_val = self.__country_info__().export_val
-        export_kg = self.__country_info__().export_kg
+        export_val = self.__secex__().export_val
+        export_kg = self.__secex__().export_kg
 
         return export_val / export_kg
 
     def total_imported(self):
-        return self.__country_info__().import_val
+        return self.__secex__().import_val
 
     def unity_weight_import_price(self):
-        import_val = self.__country_info__().import_val
-        import_kg = self.__country_info__().import_kg
+        import_val = self.__secex__().import_val
+        import_kg = self.__secex__().import_kg
 
         return import_val / import_kg
+
+class TradePartnerByLocation(TradePartner):
+
+   def __init__(self, wld_id, bra_id):
+        super(TradePartnerByLocation, self).__init__(wld_id)
+        self.bra_id = bra_id
+        self.max_year_query = db.session.query(func.max(Ymbw.year)).filter_by(wld_id=wld_id)
+        self.secex_query = Ymbw.query.join(Bra).filter(
+            Ymbw.wld_id == self.wld_id,
+            Ymbw.month == 0,
+            Ymbw.year == self.max_year_query,
+            func.length(Ymbw.bra_id) == 9).order_by(desc(Ymbw.export_val))
 
     def municipality_with_more_exports(self):
         ymbw_municipality_export_query = Ymbw.query.join(Bra).filter(
             Ymbw.wld_id == self.wld_id,
             Ymbw.month == 0,
-            Ymbw.year == self.ymbw_max_year,
+            Ymbw.year == self.max_year_query,
             func.length(Ymbw.bra_id) == 9).order_by(desc(Ymbw.export_val)).limit(1)
 
         ymbw_municipality_export_data = ymbw_municipality_export_query.values(
