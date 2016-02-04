e4c05a85dfc4864ee50ba17110921113088e6b96
Diogo Lundberg <diogo@lundberg.com.br>
2016-02-04 15:00:04 -0200

    Using new services layer classes in the trade partner module controller.

--- dataviva/apps/trade_partner/views.py
+++ dataviva/apps/trade_partner/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template, g
 from dataviva.apps.general.views import get_locale
-from dataviva.api.secex.services import TradePartner
+from dataviva.api.secex.services import TradePartner, TradePartnerMunicipalityByExport, \
+TradePartnerMunicipalityByImport, TradePartnerProductByImport, TradePartnerProductByExport, \
+TradePartnerProductByHighestBalance, TradePartnerProductByLowestBalance
 from sqlalchemy.sql.expression import func, desc, asc
 
 mod = Blueprint('trade_partner', __name__,
@@ -21,8 +23,16 @@ def add_language_code(endpoint, values):
 
 @mod.route('/')
 def index():
-    trade_partner_service = TradePartner(wld_id='nausa')
-    
+    wld_id = 'nausa'
+
+    trade_partner_service = TradePartner(wld_id=wld_id)
+    municipality_by_export_service = TradePartnerMunicipalityByExport(wld_id=wld_id)
+    municipality_by_import_service = TradePartnerMunicipalityByImport(wld_id=wld_id)
+    product_by_import_service = TradePartnerProductByImport(wld_id=wld_id)
+    product_by_export_service = TradePartnerProductByExport(wld_id=wld_id)
+    product_by_highest_balance_service = TradePartnerProductByHighestBalance(wld_id=wld_id)
+    product_by_lowest_balance_service = TradePartnerProductByLowestBalance(wld_id=wld_id)
+
     header = {
         'name': trade_partner_service.country_name(),
         'year': trade_partner_service.year(),
@@ -33,13 +43,19 @@ def index():
         'unity_weight_import_price': trade_partner_service.unity_weight_import_price()
     }
 
-    trade = {
-        'municipality_with_more_exports': trade_partner_service.municipality_with_more_exports(),
-        'municipality_with_more_imports': trade_partner_service.municipality_with_more_imports(),
-        'product_with_more_exports': trade_partner_service.product_with_more_exports(),
-        'product_with_more_imports': trade_partner_service.product_with_more_imports(),
-        'product_with_highest_balance': trade_partner_service.product_with_highest_balance(),
-        'product_with_lowest_balance': trade_partner_service.product_with_lowest_balance()
+    body {
+        'municipality_with_more_exports' : municipality_by_export_service.municipality_with_more_exports()
+        'highest_export_value_by_municipality' : municipality_by_export_service.highest_export_value_by_municipality()
+        'municipality_with_more_imports' : product_by_import_service.municipality_with_more_imports()
+        'highest_import_value_by_municipality' : product_by_import_service.highest_import_value_by_municipality()
+        'product_with_more_imports' : product_by_import_service.product_with_more_imports()
+        'highest_import_value_by_product' : product_by_import_service.highest_import_value_by_product()
+        'product_with_more_exports' : product_by_export_service.product_with_more_exports()
+        'highest_export_value_by_product' : product_by_export_service.highest_export_value_by_product()
+        'product_with_highest_balance' : product_by_highest_balance_service.product_with_highest_balance()
+        'highest_balance_by_product' : product_by_highest_balance_service.highest_balance_by_product()
+        'product_with_lowest_balance' : product_by_lowest_balance_service.product_with_lowest_balance()
+        'lowest_balance_by_product' : product_by_lowest_balance_service.lowest_balance_by_product()
     }
 
     return render_template('trade_partner/index.html', body_class='perfil-estado', header=header, trade=trade)
