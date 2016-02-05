59ae3bcb72e56417324009c63c5d8ab41707487e
Diogo Lundberg <diogo@lundberg.com.br>
2016-02-05 15:09:36 -0200

    Fixing max year queries on rais occupation services.

--- dataviva/api/rais/services.py
+++ dataviva/api/rais/services.py
@@ -6,40 +6,27 @@ from sqlalchemy.sql.expression import func, desc, asc
 class Occupation:
     def __init__(self, occupation_id):
         self.occupation_id = occupation_id
-        self._data = None
-        self._municipality_with_more_jobs = None
-        self._municipality_with_biggest_wage_average = None
-        self._activity_with_more_jobs = None
-        self._activity_with_biggest_wage_average = None
-        self._rais_sorted_by_num_jobs = None
-        self._rais_sorted_by_wage_average = None
-        year=0
-        yo_max_year= db.session.query(func.max(Yo.year)).filter(
-            Ybo.cbo_id == occupation_id)\
-            .one()
-        for years in yo_max_year:
-            year = years
+        self._rais = None
 
-        self.year = year
+        self.max_year_query= db.session.query(func.max(Yo.year)).filter(
+            Yo.cbo_id == occupation_id)
         self.rais_query = Yo.query.filter(
-                                Yo.cbo_id == self.occupation_id,
-                                Yo.year == self.year)
+            Yo.cbo_id == self.occupation_id,
+            Yo.year == self.max_year_query)
 
     def __rais_data__(self):
-        if not self._data:
+        if not self._rais:
             rais_data = self.rais_query.first_or_404()
-            self._data = rais_data
-        return self._data
+            self._rais = rais_data
+        return self._rais
 
-    def __rais_list__(self):
-        if not self._data:
-            rais_data = self.rais_query.all()
-            self._data = rais_data
-        return self._data
+    def year(self):
+        rais = self.__rais_data__()
+        return rais.year
 
     def occupation_name(self):
-        occupation = self.__rais_data__().cbo
-        return occupation.name() #self.__rais_data__()['name']
+        rais = self.__rais_data__()
+        return rais.cbo.name()
 
     def average_monthly_income(self):
         average_monthly_income = self.__rais_data__().wage_avg
@@ -57,24 +44,31 @@ class Occupation:
         total_establishments = self.__rais_data__().num_est
         return total_establishments
 
-    def __rais_sorted_by_num_jobs__(self):
-        self._rais_sorted_by_num_jobs = self.__rais_list__()
-        self._rais_sorted_by_num_jobs.sort(key=lambda rais: rais.num_jobs, reverse=True)
-        return self._rais_sorted_by_num_jobs
+class OccupationByLocation(Occupation):
+    def __init__(self, occupation_id, bra_id):
+        Occupation.__init__(self, occupation_id)
+        self.bra_id = bra_id
+        self.max_year_query= db.session.query(func.max(Ybo.year)).filter(
+            Ybo.cbo_id == occupation_id,
+            Ybo.bra_id == self.bra_id)
+        self.rais_query = Ybo.query.filter(
+            Ybo.cbo_id == self.occupation_id,
+            Ybo.bra_id == self.bra_id,
+            Ybo.year == self.max_year_query)
 
-    def __rais_sorted_by_wage_average__(self):
-        self._rais_sorted_by_wage_average = self.__rais_list__()
-        self._rais_sorted_by_wage_average.sort(key=lambda rais: rais.wage_avg , reverse=True)
-        return self._rais_sorted_by_wage_average
+    def location_name(self):
+        rais = self.__rais_data__()
+        return rais.bra.name()
 
 class OccupationMunicipalities(Occupation):
     def __init__ (self, occupation_id):
         Occupation.__init__(self, occupation_id)
+        self.max_year_query= db.session.query(func.max(Ybo.year)).filter(
+            Ybo.cbo_id == occupation_id)
         self.rais_query = Ybo.query.join(Bra).filter(
                             Ybo.cbo_id == self.occupation_id,
-                            Ybo.year == self.year,
+                            Ybo.year == self.max_year_query,
                             Ybo.bra_id_len == 9)
-        #self.municipality_data = None
 
     def __municipality_with_more_jobs__(self):
         if not self._municipality_with_more_jobs:
@@ -105,9 +99,11 @@ class OccupationMunicipalities(Occupation):
 class OccupationActivities(Occupation):
     def __init__(self, occupation_id):
         Occupation.__init__(self, occupation_id)
+        self.max_year_query= db.session.query(func.max(Yio.year)).filter(
+            Yio.cbo_id == occupation_id)
         self.rais_query = Yio.query.join(Cnae).filter(
                             Yio.cbo_id == self.occupation_id,
-                            Yio.year == self.year,
+                            Yio.year == self.max_year_query,
                             Yio.cnae_id_len == 6)
 
     def __activity_with_more_jobs__(self):
@@ -137,26 +133,6 @@ class OccupationActivities(Occupation):
         return wage_avg.wage_avg
 
 
-class OccupationByLocation(Occupation):
-
-    def __init__(self, occupation_id, bra_id):
-
-        Occupation.__init__(self, occupation_id)
-        self.bra_id = bra_id
-
-        year=0
-        ybo_max_year= db.session.query(func.max(Ybo.year)).filter(
-            Ybo.cbo_id == occupation_id,
-            Ybo.bra_id == bra_id)\
-            .one()
-        for years in ybo_max_year:
-            year = years
-        self.year = year
-
-        self.rais_query = Ybo.query.filter(
-                                Ybo.cbo_id == self.occupation_id,
-                                Ybo.bra_id == self.bra_id,
-                                Ybo.year == self.year)
 
 
 '''
