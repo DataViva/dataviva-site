3661e4f23c5b41c768553fda8364859717bcf9d6
Pedro Goecking <pedrogoecking93@gmail.com>
2016-02-05 10:57:28 -0200

    Updating dictionary to get values from the different classes.

--- dataviva/apps/basic_course/views.py
+++ dataviva/apps/basic_course/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template, g
 from dataviva.apps.general.views import get_locale
-from dataviva.api.sc.services import Basic_course
+from dataviva.api.sc.services import Basic_course, Basic_course_school, Basic_course_city
 from dataviva.api.attrs.models import School, Bra, Course_sc
 from dataviva.api.sc.models import Yc_sc, Ysc, Ybc_sc, Ybsc
 from dataviva import db
@@ -27,9 +27,10 @@ def add_language_code(endpoint, values):
 def index(course_sc_id, bra_id):
 
     sc_service = Basic_course(course_sc_id= course_sc_id,bra_id=bra_id)
+    school_service = Basic_course_school(course_sc_id= course_sc_id,bra_id=bra_id)
+    city_service = Basic_course_city(course_sc_id= course_sc_id,bra_id=bra_id)
 
     header = {
-
         'course_name' : sc_service.course_name(),
         'course_description' : sc_service.course_description(),
         'course_classes' : sc_service.course_classes(),
@@ -37,16 +38,15 @@ def index(course_sc_id, bra_id):
         'course_enrolled' : sc_service.course_enrolled(),
         'course_average_class_size' : sc_service.course_average_class_size(),
         'course_year' : sc_service.course_year(),
-
     }
 
     body = {
-        'schools_count' : sc_service.schools_count(),
+        'school_count' : school_service.school_count(),
         'enrollment_statistics_description' : sc_service.enrollment_statistics_description(),
-        'school_name' : sc_service.school_name(),
-        'school_enrolled' : sc_service.school_enrolled(),
-        'city_name' : sc_service.city_name(),
-        'city_enrolled' : sc_service.city_enrolled(),
+        'school_name' : school_service.school_name(),
+        'school_enrolled' : school_service.school_enrolled(),
+        'city_name' : city_service.city_name(),
+        'city_enrolled' : city_service.city_enrolled(),
     }
 
     return render_template('basic_course/index.html', header=header, body=body, body_class='perfil-estado')
