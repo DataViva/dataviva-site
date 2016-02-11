d176e0953e22e225a685f29152ad7b8eaed378bf
Diogo Lundberg <diogo@lundberg.com.br>
2016-02-11 16:18:41 -0200

    Altering file to get data from new class Basic_course_by_location.

--- dataviva/apps/basic_course/views.py
+++ dataviva/apps/basic_course/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template, g
 from dataviva.apps.general.views import get_locale
-from dataviva.api.sc.services import Basic_course, Basic_course_school, Basic_course_city
+from dataviva.api.sc.services import Basic_course, Basic_course_by_location#, Basic_course_school, Basic_course_city
 from dataviva.api.attrs.models import School, Bra, Course_sc
 from dataviva.api.sc.models import Yc_sc, Ysc, Ybc_sc, Ybsc
 from dataviva import db
@@ -26,27 +26,31 @@ def add_language_code(endpoint, values):
 @mod.route('/<course_sc_id>/<bra_id>')
 def index(course_sc_id, bra_id):
 
-    sc_service = Basic_course(course_sc_id= course_sc_id,bra_id=bra_id)
-    school_service = Basic_course_school(course_sc_id= course_sc_id,bra_id=bra_id)
-    city_service = Basic_course_city(course_sc_id= course_sc_id,bra_id=bra_id)
+    if bra_id:
+        sc_service = Basic_course_by_location(course_sc_id= course_sc_id,bra_id=bra_id)
+    else:
+        sc_service = Basic_course(course_sc_id= course_sc_id)
+    #school_service = Basic_course_school(course_sc_id= course_sc_id,bra_id=bra_id)
+    #city_service = Basic_course_city(course_sc_id= course_sc_id,bra_id=bra_id)
 
     header = {
         'course_name' : sc_service.course_name(),
-        'course_description' : sc_service.course_description(),
+        #'course_description' : sc_service.course_description(),
         'course_classes' : sc_service.course_classes(),
         'course_age' : sc_service.course_age(),
         'course_enrolled' : sc_service.course_enrolled(),
         'course_average_class_size' : sc_service.course_average_class_size(),
         'course_year' : sc_service.course_year(),
-        'school_count' : school_service.school_count(),
+        #'school_count' : school_service.school_count(),
     }
 
+    
     body = {
-        'enrollment_statistics_description' : sc_service.enrollment_statistics_description(),
-        'school_name' : school_service.school_name(),
-        'school_enrolled' : school_service.school_enrolled(),
-        'city_name' : city_service.city_name(),
-        'city_enrolled' : city_service.city_enrolled(),
+    #    'enrollment_statistics_description' : sc_service.enrollment_statistics_description(),
+   #     'school_name' : school_service.school_name(),
+    #    'school_enrolled' : school_service.school_enrolled(),
+   #     'city_name' : city_service.city_name(),
+    #    'city_enrolled' : city_service.city_enrolled(),
     }
-
+ 
     return render_template('basic_course/index.html', header=header, body=body, body_class='perfil-estado')
