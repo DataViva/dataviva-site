d0113b30a326bb2f1e878ca62d37ab825360c92e
Pedro Goecking <pedrogoecking93@gmail.com>
2016-02-12 19:28:07 -0200

    Removing commented code and order by from query.

--- dataviva/api/sc/services.py
+++ dataviva/api/sc/services.py
@@ -52,101 +52,19 @@ class Basic_course:
         course_year = self.__sc__()
         return course_year.year
 
-    '''def __statistics__(self):
-
-        basic_course = {}
-
-        if self.bra_id:
-
-            most_enrolled_school_query = Ybsc.query.join(School).filter(
-                Ybsc.course_sc_id == self.course_sc_id,
-                Ybsc.year == self.ybsc_max_year_subquery,
-                Ybsc.bra_id == self.bra_id) \
-                .order_by(Ybsc.enrolled.desc()).limit(1)
-
-            most_enrolled_city_query = Ybc_sc.query.join(Bra).filter(
-                Ybc_sc.course_sc_id == self.course_sc_id,
-                Ybc_sc.year == self.max_year_subquery,
-                Ybc_sc.bra_id.like(str(self.bra_id)+'%'),
-                Ybc_sc.bra_id_len == 9) \
-                .order_by(Ybc_sc.enrolled.desc()).limit(1)
-
-            course_data = course_query.values(Course_sc.name_pt,
-                                        Course_sc.desc_pt,
-                                        Ybc_sc.classes,
-                                        Ybc_sc.age,
-                                        Ybc_sc.enrolled,
-                                        Ybc_sc.year)
-
-            school_data = most_enrolled_school_query.values(
-                School.name_pt,
-                Ybsc.enrolled)
-
-            if len(self.bra_id) < 9:
-                city_data = most_enrolled_city_query.values(
-                    Bra.name_pt,
-                    Ybc_sc.enrolled)
-            else:
-                city_data = None
-
-        else:
-
-            most_enrolled_school_query = Ysc.query.join(School).filter(
-                Ysc.course_sc_id == self.course_sc_id,
-                Ysc.year == self.max_year_subquery) \
-                .order_by(Ysc.enrolled.asc())
-
-            most_enrolled_city_query = Ybc_sc.query.join(Bra).filter(
-                Ybc_sc.course_sc_id == self.course_sc_id,
-                Ybc_sc.year == self.max_year_subquery,
-                Ybc_sc.bra_id_len == 9) \
-                .order_by(Ybc_sc.enrolled.asc())
-
-            course_data = course_query.values(
-                Course_sc.name_pt,
-                Course_sc.desc_pt,
-                Yc_sc.classes,
-                Yc_sc.age,
-                Yc_sc.enrolled,
-                Yc_sc.year)
-
-            school_data = most_enrolled_school_query.values(
-                School.name_pt,
-                Ysc.enrolled)
-
-            city_data = most_enrolled_city_query.values(
-                Bra.name_pt,
-                Ybc_sc.enrolled)
-
-        for name_pt, desc_pt, classes, age, enrolled, year in course_data:
-            basic_course['course_name'] = name_pt
-            basic_course['course_description'] = desc_pt or unicode('Não há descrição para este curso.', 'utf8')
-            basic_course['course_classes'] = classes
-            basic_course['course_age'] = age
-            basic_course['course_enrolled'] = enrolled
-            basic_course['course_year'] = year
-
-        basic_course['school_count'] = total_schools_query.count()
-        basic_course['enrollment_statistics_description'] = 'Enrollment Statistics Description'
-
-        for name_pt, enrolled in school_data:
-            basic_course['school_name'] = name_pt
-            basic_course['school_enrolled'] = enrolled
-
-        if city_data:
-            for name_pt, enrolled in city_data:
-                basic_course['city_name'] = name_pt
-                basic_course['city_enrolled'] = enrolled
-        else:
-            basic_course['city_name'] = None
-            basic_course['city_enrolled'] = None
-
-        self._statistics = basic_course
-
-        return self._statistics
-
     def enrollment_statistics_description(self):
-        return self.__statistics__()['enrollment_statistics_description']'''
+        return self.__statistics__()['enrollment_statistics_description']
+
+class Basic_course_by_location(Basic_course):
+    def __init__(self, course_sc_id, bra_id):
+        Basic_course.__init__(self, course_sc_id)
+        self.bra_id = bra_id
+        self.max_year_subquery = db.session.query(
+            func.max(Ybc_sc.year)).filter_by(course_sc_id=course_sc_id,bra_id=bra_id)
+        self.course_query = Ybc_sc.query.join(Course_sc).filter(
+                Ybc_sc.course_sc_id == self.course_sc_id,
+                Ybc_sc.year == self.max_year_subquery,
+                Ybc_sc.bra_id == self.bra_id)
 
 class Basic_course_school(Basic_course):
     def __init__(self, course_sc_id):
@@ -240,17 +158,4 @@ class Basic_course_city_by_location(Basic_course_city):
                 Ybc_sc.course_sc_id == self.course_sc_id,
                 Ybc_sc.year == self.max_year_subquery,
                 Ybc_sc.bra_id.like(str(self.bra_id)+'%'),
-                Ybc_sc.bra_id_len == 9) \
-                .order_by(Ybc_sc.enrolled.desc()).limit(1)
-
-
-class Basic_course_by_location(Basic_course):
-    def __init__(self, course_sc_id, bra_id):
-        Basic_course.__init__(self, course_sc_id)
-        self.bra_id = bra_id
-        self.max_year_subquery = db.session.query(
-            func.max(Ybc_sc.year)).filter_by(course_sc_id=course_sc_id,bra_id=bra_id)
-        self.course_query = Ybc_sc.query.join(Course_sc).filter(
-                Ybc_sc.course_sc_id == self.course_sc_id,
-                Ybc_sc.year == self.max_year_subquery,
-                Ybc_sc.bra_id == self.bra_id)
+                Ybc_sc.bra_id_len == 9)
