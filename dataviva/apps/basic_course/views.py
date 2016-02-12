017b3937798d4286208737ff0fdba0aac8779498
Pedro Goecking <pedrogoecking93@gmail.com>
2016-02-12 17:07:08 -0200

    Adding condition to get values from schools based on selected location.

--- dataviva/apps/basic_course/views.py
+++ dataviva/apps/basic_course/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template, g
 from dataviva.apps.general.views import get_locale
-from dataviva.api.sc.services import Basic_course, Basic_course_by_location, Basic_course_school#, Basic_course_city
+from dataviva.api.sc.services import Basic_course, Basic_course_by_location, Basic_course_school, Basic_course_school_by_location#, Basic_course_city
 from dataviva.api.attrs.models import School, Bra, Course_sc
 from dataviva.api.sc.models import Yc_sc, Ysc, Ybc_sc, Ybsc
 from dataviva import db
@@ -23,14 +23,17 @@ def add_language_code(endpoint, values):
     values.setdefault('lang_code', get_locale())
 
 
-@mod.route('/<course_sc_id>/<bra_id>')
-def index(course_sc_id, bra_id):
+@mod.route('/<course_sc_id>')
+def index(course_sc_id):
+
+    bra_id = '4mg'
 
     if bra_id:
         sc_service = Basic_course_by_location(course_sc_id= course_sc_id,bra_id=bra_id)
+        school_service = Basic_course_school_by_location(course_sc_id=course_sc_id, bra_id=bra_id)
     else:
         sc_service = Basic_course(course_sc_id= course_sc_id)
-    school_service = Basic_course_school(course_sc_id= course_sc_id)
+        school_service = Basic_course_school(course_sc_id= course_sc_id)
     #city_service = Basic_course_city(course_sc_id= course_sc_id,bra_id=bra_id)
 
     header = {
@@ -41,10 +44,10 @@ def index(course_sc_id, bra_id):
         'course_enrolled' : sc_service.course_enrolled(),
         'course_average_class_size' : sc_service.course_average_class_size(),
         'course_year' : sc_service.course_year(),
-        #'school_count' : school_service.school_count(),
+        'school_count' : school_service.school_count(),
     }
 
-    
+
     body = {
     #    'enrollment_statistics_description' : sc_service.enrollment_statistics_description(),
          'school_name' : school_service.school_name(),
@@ -52,5 +55,5 @@ def index(course_sc_id, bra_id):
    #     'city_name' : city_service.city_name(),
     #    'city_enrolled' : city_service.city_enrolled(),
     }
- 
+
     return render_template('basic_course/index.html', header=header, body=body, body_class='perfil-estado')
