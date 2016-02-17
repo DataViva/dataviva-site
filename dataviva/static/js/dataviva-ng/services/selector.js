8a990f27b10f61e7a216d313d0f7bb4c4c226f88
Diogo Lundberg <diogo@lundberg.com.br>
2016-02-16 23:21:22 -0200

    Adds basic course Js service.

--- dataviva/static/js/dataviva-ng/services/selector.js
+++ dataviva/static/js/dataviva-ng/services/selector.js
@@ -139,9 +139,69 @@
                     self.load_depth_entries(self.sections);
                 };
 
+                var BasicCourse = function(selection_callback) {
+                    var self = this;
+                    self.templateUrl = "/en/wizard/basic_course_selector/";
+                    self.selected_entry = null;
+                    self.selection_callback = selection_callback;
+
+                    /*
+                        By default this selector redirects to the location page
+                        But can be injected with a specfic callback
+                    */
+                    if(selection_callback) {
+                        self.selection_callback = selection_callback;
+                    } else {
+                        self.selection_callback = function(hs_id, event) {
+                            window.location = (
+                                window.location.protocol + 
+                                "//" + 
+                                window.location.host + 
+                                "/en/basic_course/" + 
+                                hs_id
+                            );
+                        }
+                    }
+
+                    self.fields = {
+                        depth_factor: 2,
+                        filter_string: '',
+                        entries: []
+                    };
+
+                    self.courses = {
+                        depth_factor: 5,
+                        filter_string: '',
+                        entries: []
+                    };
+
+                    self.load_depth_entries = function(group){
+                        if(!group || group.entries.length > 0) return;
+                        self.loading = true;
+
+                        $http({
+                            method: "GET",
+                            url: "/attrs/basic_course?depth=" + group.depth_factor,
+                        })
+                        .success(function(resp){
+                             group.entries = resp;
+                             self.loading = false;
+                        });
+                    };
+
+                    self.select = function(id, event) {
+                        self.selected_entry = id;
+                        self.selection_callback(id, event);
+                    };
+
+                    // NOTE: NECESSARY TO LOAD THE FIRST TAB CONTENT UPFRONT
+                    self.load_depth_entries(self.fields);
+                };
+
                 return {
                     Location: Location,
                     Product: Product,
+                    BasicCourse: BasicCourse,
                 };
     }]);
 }());
