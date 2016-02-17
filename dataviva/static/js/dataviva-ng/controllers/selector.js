4c894023a0eb3c580fdec314ac578d96e8f5fd51
Diogo Lundberg <diogo@lundberg.com.br>
2016-02-16 23:20:09 -0200

    Refactor on selector controller, now it should be generic.

--- dataviva/static/js/dataviva-ng/controllers/selector.js
+++ dataviva/static/js/dataviva-ng/controllers/selector.js
@@ -7,20 +7,8 @@
         '$scope', '$http', '$templateRequest', '$compile', 'Selectors',
         function ($scope, $http, $templateRequest, $compile, Selectors) {
 
-            $scope.selectors = {
-                location: {
-                    model: Selectors.Location,
-                },
-                product: {
-                    model: Selectors.Product,
-                },
-            };
-
             $scope.initialize = function(selector_name, selection_callback) {
-
-                var selector = $scope.selectors[selector_name];
-                $scope.selector_model = new selector.model(selection_callback);
-
+                $scope.selector_model = new  Selectors[selector_name];
                 $templateRequest($scope.selector_model.templateUrl).then(function(html){
                     var template = angular.element(html);
                     $(".selector-area").append(template);
