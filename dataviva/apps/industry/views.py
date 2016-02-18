9f54733650b5c955c30fb0164cda7c9ae9a181b4
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-02-18 14:42:05 -0200

    Removing static folder and images moke.

--- dataviva/apps/industry/views.py
+++ dataviva/apps/industry/views.py
@@ -10,8 +10,7 @@ from dataviva.api.rais.models import Yi
 
 mod = Blueprint('industry', __name__,
                 template_folder='templates',
-                url_prefix='/<lang_code>/industry',
-                static_folder='static')
+                url_prefix='/<lang_code>/industry')
 
 
 @mod.url_value_preprocessor
@@ -33,10 +32,6 @@ def index(cnae_id):
     header = {}
     body = {}
 
-    industry = { 
-        'background_image':  unicode("'static/img/bg-profile-location.jpg'", 'utf8'),
-        'portrait' : unicode('https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7748245.803118934!2d-49.94643868147362!3d-18.514293729997753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa690a165324289%3A0x112170c9379de7b3!2sMinas+Gerais!5e0!3m2!1spt-BR!2sbr!4v1450524997110', 'utf8') ,        
-    }
 
     if bra_id == None :
         industry['flag_preview_headers'] = False
