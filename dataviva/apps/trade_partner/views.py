536c116bd474c949a6f34ee20b109128bced6e70
Victor Moraes <vdmoraes94@gmail.com>
2016-02-18 11:40:56 -0200

    Adding image portraits to Trade Partner category

--- dataviva/apps/trade_partner/views.py
+++ dataviva/apps/trade_partner/views.py
@@ -4,10 +4,12 @@ from dataviva.apps.general.views import get_locale
 from dataviva.api.secex.services import TradePartner, \
     TradePartnerMunicipalities, TradePartnerProducts
 
+static_folder = '../../static/img/icons/wld'
+
 mod = Blueprint('trade_partner', __name__,
                 template_folder='templates',
                 url_prefix='/<lang_code>/trade_partner',
-                static_folder='static')
+                static_folder=static_folder)
 
 
 @mod.url_value_preprocessor
@@ -34,7 +36,8 @@ def index():
         'total_exported': trade_partner_service.total_exported(),
         'unity_weight_export_price': trade_partner_service.unity_weight_export_price(),
         'total_imported': trade_partner_service.total_imported(),
-        'unity_weight_import_price': trade_partner_service.unity_weight_import_price()
+        'unity_weight_import_price': trade_partner_service.unity_weight_import_price(),
+        'wld_id': wld_id
     }
 
     body = {
@@ -54,4 +57,4 @@ def index():
         'world_trade_description' : 'World trade description.',
     }
 
-    return render_template('trade_partner/index.html', body_class='perfil-estado', header=header, body=body)
+    return render_template('trade_partner/index.html', body_class='perfil-estado', header=header, body=body, static_folder=static_folder)
