4826a13cf17e1d6951ef9b5db867265690328822
billyninja <joaoeduardocm@gmail.com>
2016-02-18 12:07:51 -0200

    fixing the selectors-services refactor

--- dataviva/static/js/dataviva-ng/services/selector.js
+++ dataviva/static/js/dataviva-ng/services/selector.js
@@ -1,11 +1,11 @@
 (function () {
     "use strict";
 
-    var app = angular.module("dataviva.services");
+    var app = angular.module("dataviva.services.selectors");
 
     app.service('Selectors',[
             '$http', 'Location', 'Product', 'Industry', 'Major', 'BasicCourse',
-                function ($http Location, Product, Industry, Major, BasicCourse) {
+                function ($http, Location, Product, Industry, Major, BasicCourse) {
                 return {
                     BasicCourse: BasicCourse,
                     Industry: Industry,
