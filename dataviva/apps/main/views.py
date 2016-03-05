911fd7e50787c8ef1fe83929f498f87827cb053a
Diogo Lundberg <diogo@lundberg.com.br>
2016-03-05 12:46:27 -0300

    Adds new main module files structure

---
+++ dataviva/apps/main/views.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from flask import Blueprint, render_template, g
+from dataviva.apps.general.views import get_locale
+
+mod = Blueprint('main', __name__,
+                template_folder='templates',
+                url_prefix='/<lang_code>/main')
+
+
+@mod.url_value_preprocessor
+def pull_lang_code(endpoint, values):
+    g.locale = values.pop('lang_code')
+
+
+@mod.url_defaults
+def add_language_code(endpoint, values):
+    values.setdefault('lang_code', get_locale())
+
+
+@mod.route('/')
+def index():
+    return render_template('main/index.html')
