8496580ebc42bb703c8630ff34a15d962ff7a5a2
FredericoVieira <fredops1@hotmail.com>
2016-03-08 15:45:44 -0300

    Adds script to create scholar tables.

---
+++ migrations/201603081525__create_scholar.sql
@@ -0,0 +1,46 @@
+CREATE TABLE scholar_article(
+    id int UNSIGNED NOT NULL AUTO_INCREMENT,
+    title varchar (400) NULL,
+    abstract varchar (250) NULL,
+    file varchar (100) NULL,
+    postage_date datetime NULL,
+    PRIMARY KEY (id)
+);
+​
+CREATE TABLE scholar_theme(
+    id int UNSIGNED NOT NULL AUTO_INCREMENT,
+    theme varchar (50) NULL,
+    article_id int UNSIGNED NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (article_id) REFERENCES scholar_article(id)
+);
+​
+CREATE TABLE scholar_author(
+    id int UNSIGNED NOT NULL AUTO_INCREMENT,
+    first_name varchar (50) NULL,
+    last_name varchar (50) NULL,
+    PRIMARY KEY (id)
+);
+​
+CREATE TABLE scholar_article_author(
+    article_id int UNSIGNED NOT NULL,
+    author_id int UNSIGNED NOT NULL,
+    FOREIGN KEY (article_id) REFERENCES scholar_article(id),
+    FOREIGN KEY (author_id) REFERENCES scholar_author(id)
+);
+​
+CREATE TABLE scholar_key_word(
+    id int UNSIGNED NOT NULL AUTO_INCREMENT,
+    key_word varchar (50) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE scholar_article_key_word(
+    article_id int UNSIGNED NOT NULL,
+    key_word_id int UNSIGNED NOT NULL,
+    FOREIGN KEY (article_id) REFERENCES scholar_article(id),
+    FOREIGN KEY (key_word_id) REFERENCES scholar_key_word(id)
+);
+
+
+​
