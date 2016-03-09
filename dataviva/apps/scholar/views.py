b5b159a3b40e8cc637f5e518a821c56a88250e4e
FredericoVieira <fredops1@hotmail.com>
2016-03-09 15:50:10 -0300

    Updates scholar views to implements crud methods with models.

--- dataviva/apps/scholar/views.py
+++ dataviva/apps/scholar/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template, g, make_response, url_for, flash
+from flask import Blueprint, render_template, g, make_response, redirect, url_for, flash
 from dataviva.apps.general.views import get_locale
 
+from models import Article, Author, KeyWord
+from dataviva import db
 from forms import RegistrationForm
-from mock import Article, articles, ids
-import time
 from datetime import datetime
 
 
@@ -25,16 +25,14 @@ def add_language_code(endpoint, values):
 
 @mod.route('/', methods=['GET'])
 def index():
-    from models import Article
     articles = Article.query.all()
     return render_template('scholar/index.html', articles=articles)
 
 
 @mod.route('/article/<id>', methods=['GET'])
 def show(id):
-    id = int(id.encode())
-    article = articles[id]
-    return render_template('scholar/show.html', article=article, id=id)
+    article = Article.query.filter_by(id=id).first()
+    return render_template('scholar/show.html', article=article)
 
 
 @mod.route('/article/new', methods=['GET'])
@@ -46,8 +44,7 @@ def new():
 @mod.route('/article/<id>/edit', methods=['GET'])
 def edit(id):
     form = RegistrationForm()
-    id = int(id.encode())
-    article = articles[id]
+    article = Article.query.filter_by(id=id).first()
     form.title.data = article.title
     form.theme.data = article.theme
     form.author.data = article.author
@@ -62,9 +59,6 @@ def create():
     if form.validate() is False:
         return render_template('scholar/new.html', form=form)
     else:
-        from models import Article, Author, KeyWord
-        from dataviva import db
-
         article = Article()
         article.title = form.title.data
         article.theme = form.theme.data
@@ -72,12 +66,13 @@ def create():
         article.file_path = 'test'
         article.postage_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-        author = form.author.data
-        author = [Author(author)]
+        author_input_list = form.author.data.split(',')
+        for author_input in author_input_list:
+            article.author.append(Author(author_input))
 
-        key_word_input_list = form.key_words.data
+        key_word_input_list = form.key_words.data.split(',')
         for key_word_input in key_word_input_list:
-            key_word = KeyWord.query.filter_by(key_word=key_word_input).first()
+            key_word = KeyWord.query.filter_by(name=key_word_input).first()
 
             if not key_word:
                 article.key_words.append(KeyWord(key_word_input))
@@ -90,34 +85,46 @@ def create():
         message = u'Muito obrigado! Seu estudo foi submetido com sucesso e será analisado pela equipe do DataViva. \
                   Em até 15 dias você receberá um retorno sobre sua publicação no site!'
         flash(message, 'success')
-        return render_template('scholar/index.html', articles=articles)
+        return redirect(url_for('scholar.index'))
 
 
 @mod.route('/article/<id>/edit', methods=['POST'])
 def update(id):
     form = RegistrationForm()
-    id = int(id.encode())
     if form.validate() is False:
         return render_template('scholar/edit.html', form=form)
     else:
-        title = form.title.data
-        theme = form.theme.data
-        author = form.author.data
-        key_words = form.key_words.data
-        abstract = form.abstract.data
-        postage_date = time.strftime("%d/%m/%Y")
+        article = Article.query.filter_by(id=id).first()
+        article.title = form.title.data
+        article.theme = form.theme.data
+        article.abstract = form.abstract.data
+        article.file_path = 'test'
+        article.postage_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-        articles[id] = Article(title, theme, author, key_words, abstract, postage_date)
+        key_word_input_list = form.key_words.data.split(',')
+        for key_word_input in key_word_input_list:
+            key_word = KeyWord.query.filter_by(name=key_word_input).first()
+
+            if not key_word:
+                article.key_words.append(KeyWord(key_word_input))
+            else:
+                article.key_words.append(key_word)
+
+        db.session.commit()
+
         message = u'Estudo editado com sucesso!'
         flash(message, 'success')
-        return render_template('scholar/index.html', articles=articles)
+        return redirect(url_for('scholar.index'))
 
 
 @mod.route('/article/<id>/delete', methods=['GET'])
 def delete(id):
-    if articles.pop(int(id.encode())):
+    article = Article.query.filter_by(id=id).first()
+    if article:
+        db.session.delete(article)
+        db.session.commit()
         message = u"Estudo excluído com sucesso!"
         flash(message, 'success')
-        return render_template('scholar/index.html', articles=articles)
+        return redirect(url_for('scholar.index'))
     else:
         return make_response(render_template('not_found.html'), 404)
