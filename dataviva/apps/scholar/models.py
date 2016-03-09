877b3d7c3b23f10e54cbc807a3cf6c358319dc42
FredericoVieira <fredops1@hotmail.com>
2016-03-09 14:03:01 -0300

    Updates scholar models.

--- dataviva/apps/scholar/models.py
+++ dataviva/apps/scholar/models.py
@@ -1,13 +1,8 @@
 from dataviva import db
 from sqlalchemy import ForeignKey
 
-
-article_author_table = db.Table('article_author',
-    db.Column('article_id', db.Integer(), db.ForeignKey('scholar_article.id')),
-    db.Column('author_id', db.Integer(), db.ForeignKey('scholar_author.id'))
-)
-
-article_key_word_table = db.Table('article_key_word',
+article_key_word_table = db.Table(
+    'scholar_article_key_word',
     db.Column('article_id', db.Integer(), db.ForeignKey('scholar_article.id')),
     db.Column('key_word_id', db.Integer(), db.ForeignKey('scholar_key_word.id'))
 )
@@ -18,57 +13,37 @@ class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(400))
     abstract = db.Column(db.String(250))
-    file_path = db.Column(db.String(100))
+    theme = db.Column(db.String(250))
+    file_path = db.Column(db.String(255))
     postage_date = db.Column(db.DateTime)
-    theme = db.relationship('Theme', backref='scholar_article', lazy='dynamic')
-    author = db.relationship('Author', secondary=article_author_table)
-    key_word = db.relationship('KeyWord', secondary=article_key_word_table)
-
-    def __init__(self, title=None, abstract=None, file_path=None, postage_date=None):
-        self.title = title
-        self.abstract = abstract
-        self.file_path = file_path
-        self.postage_date = postage_date
+    author = db.relationship('Author', backref='scholar_article', lazy='dynamic')
+    key_words = db.relationship('KeyWord', secondary=article_key_word_table)
 
     def __repr__(self):
-        return '<Title %r>' % (self.title)
-
-
-class Theme(db.Model):
-    __tablename__ = 'scholar_theme'
-    id = db.Column(db.Integer, primary_key=True)
-    theme = db.Column(db.String(50))
-    article_id = db.Column(db.Integer, ForeignKey('scholar_article.id'))
-
-    def __init__(self, theme=None, article_id=None):
-        self.theme = theme
-        self.article_id = article_id
-
-    def __repr__(self):
-        return '<Theme %r>' % (self.theme)
+        return '<Article %r>' % (self.title)
 
 
 class Author(db.Model):
     __tablename__ = 'scholar_author'
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(50))
-    last_name = db.Column(db.String(50))
+    name = db.Column(db.String(50))
+    article_id = db.Column(db.Integer, ForeignKey('scholar_article.id'))
 
-    def __init__(self, first_name=None, last_name=None):
-        self.first_name = first_name
-        self.last_name = last_name
+    def __init__(self, name=None, article_id=None):
+        self.name = name
+        self.article_id = article_id
 
     def __repr__(self):
-        return '<Author %r>' % (self.first_name)
+        return '<Author %r>' % (self.name)
 
 
 class KeyWord(db.Model):
     __tablename__ = 'scholar_key_word'
     id = db.Column(db.Integer, primary_key=True)
-    key_word = db.Column(db.String(50))
+    name = db.Column(db.String(50))
 
-    def __init__(self, key_word=None):
-        self.key_word = key_word
+    def __init__(self, name=None):
+        self.name = name
 
     def __repr__(self):
-        return '<Key-Word %r>' % (self.key_word)
+        return '<Key-Word %r>' % (self.name)
