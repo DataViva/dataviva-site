5fe606c6ad2059f88a1a6f1a4217a64db49f6d77
FredericoVieira <fredops1@hotmail.com>
2016-03-10 18:01:07 -0300

    Updates create script for blog.

--- migrations/201603081525__create_blog.sql
+++ migrations/201603081525__create_blog.sql
@@ -1,30 +1,18 @@
 CREATE TABLE blog_post(
     id int UNSIGNED NOT NULL AUTO_INCREMENT,
     title varchar (400) NULL,
-    text_desc text NULL,
+    subject varchar (100) NULL,
+    text_content text NULL,
     image_path varchar (250) NULL,
     thumb_path varchar (250) NULL,
     postage_date datetime NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE blog_category(
-    id int UNSIGNED NOT NULL AUTO_INCREMENT,
-    category varchar (50) NULL,
-    post_id int UNSIGNED NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (post_id) REFERENCES blog_post(id)
-);
-
 CREATE TABLE blog_author(
     id int UNSIGNED NOT NULL AUTO_INCREMENT,
     name varchar (50) NULL,
-    PRIMARY KEY (id)
-);
-​
-CREATE TABLE blog_post_author(
-    post_id int UNSIGNED NOT NULL,
-    author_id int UNSIGNED NOT NULL,
-    FOREIGN KEY (post_id) REFERENCES blog_post(id),
-    FOREIGN KEY (author_id) REFERENCES blog_author(id)
+    post_id int UNSIGNED NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (post_id) REFERENCES blog_post(id)
 );
