1648a8386cd4e295fdf0566cc97f2c13ce76d6aa
Pedro Goecking <pedrogoecking93@gmail.com>
2016-03-14 15:15:01 -0300

    Passing name of location and organizing code according PEP 8.

--- dataviva/apps/industry/views.py
+++ dataviva/apps/industry/views.py
@@ -2,11 +2,8 @@
 from flask import Blueprint, render_template, g, request
 from dataviva.apps.general.views import get_locale
 from dataviva.api.rais.services import Industry, IndustryOccupation, IndustryMunicipality, IndustryByLocation
-from dataviva.api.attrs.models import Bra
-
 from dataviva import db
 from sqlalchemy import func, desc
-from dataviva.api.attrs.models import Cnae
 from dataviva.api.rais.models import Yi
 
 mod = Blueprint('industry', __name__,
@@ -36,77 +33,85 @@ def index(cnae_id):
     industry['cnae_id'] = cnae_id
     industry['section_id'] = cnae_id[0]
 
-    if bra_id == None :
+    if bra_id is None:
         industry['flag_preview_headers'] = False
-        industry['county'] = True # view county where no country
-    else :
+        industry['county'] = True  # view county where no country
+    else:
         industry['flag_preview_headers'] = True
         industry['bra_id'] = bra_id
-        industry['name_bra'] = IndustryByLocation.name()
 
-        if len(bra_id) == 9 : # municipatity
+        if len(bra_id) == 9:  # municipatity
             industry['county'] = False
-        else :
+        else:
             industry['county'] = True
 
-    if len(cnae_id) == 6 : # class
+    if len(cnae_id) == 6:  # class
         industry['class'] = True
-    else :
+    else:
         industry['class'] = False
 
-
-    if bra_id :
+    if bra_id:
         industry_service = IndustryByLocation(bra_id=bra_id, cnae_id=cnae_id)
-
+        header['name_bra'] = industry_service.name()
         header['rca'] = industry_service.rca()
         header['distance'] = industry_service.distance()
         header['opportunity_gain'] = industry_service.opportunity_gain()
     else:
         industry_service = Industry(cnae_id=cnae_id)
 
-    industry_occupation_service = IndustryOccupation(bra_id=bra_id, cnae_id=cnae_id)
-    industry_municipality_service = IndustryMunicipality(bra_id=bra_id, cnae_id=cnae_id)
+    industry_occupation_service = IndustryOccupation(
+        bra_id=bra_id, cnae_id=cnae_id)
+    industry_municipality_service = IndustryMunicipality(
+        bra_id=bra_id, cnae_id=cnae_id)
 
     header['name'] = industry_service.get_name()
     header['year'] = industry_service.get_year()
 
-    header['average_monthly_income'] = industry_service.average_monthly_income()
+    header[
+        'average_monthly_income'] = industry_service.average_monthly_income()
     header['salary_mass'] = industry_service.salary_mass()
     header['num_jobs'] = industry_service.num_jobs()
     header['num_establishments'] = industry_service.num_establishments()
 
+    body[
+        'occ_with_more_number_jobs_name'] = industry_occupation_service.occupation_with_more_jobs()
+    body[
+        'occ_with_more_number_jobs_value'] = industry_occupation_service.highest_number_of_jobs()
 
+    body[
+        'occ_with_more_wage_avg_name'] = industry_occupation_service.occupation_with_biggest_wage_average()
+    body[
+        'occ_with_more_wage_avg_value'] = industry_occupation_service.biggest_wage_average()
 
-    body['occ_with_more_number_jobs_name'] = industry_occupation_service.occupation_with_more_jobs()
-    body['occ_with_more_number_jobs_value'] = industry_occupation_service.highest_number_of_jobs()
+    if bra_id is None or len(bra_id) != 9:
+        body[
+            'municipality_with_more_num_jobs_value'] = industry_municipality_service.highest_number_of_jobs()
+        body[
+            'municipality_with_more_num_jobs_name'] = industry_municipality_service.municipality_with_more_num_jobs()
 
+        body['municipality_with_more_wage_avg_name'] = industry_municipality_service.\
+            municipality_with_more_wage_average()
+        body[
+            'municipality_with_more_wage_avg_value'] = industry_municipality_service.biggest_wage_average()
 
-    body['occ_with_more_wage_avg_name'] = industry_occupation_service.occupation_with_biggest_wage_average()
-    body['occ_with_more_wage_avg_value'] = industry_occupation_service.biggest_wage_average()
-
-    if bra_id == None  or len(bra_id) != 9 :
-        body['municipality_with_more_num_jobs_value'] = industry_municipality_service.highest_number_of_jobs()
-        body['municipality_with_more_num_jobs_name'] = industry_municipality_service.municipality_with_more_num_jobs()
-
-        body['municipality_with_more_wage_avg_name'] = industry_municipality_service.municipality_with_more_wage_average()
-        body['municipality_with_more_wage_avg_value'] = industry_municipality_service.biggest_wage_average()
-
-    yi_max_year = db.session.query(func.max(Yi.year)).filter_by(cnae_id=cnae_id)
+    yi_max_year = db.session.query(
+        func.max(Yi.year)).filter_by(cnae_id=cnae_id)
     list_rais = Yi.query.filter(
         Yi.year == yi_max_year
-        ).order_by(desc(Yi.num_jobs)).all()
+    ).order_by(desc(Yi.num_jobs)).all()
 
-    for index, rais in enumerate(list_rais) :
-        if rais.cnae_id == cnae_id :
+    for index, rais in enumerate(list_rais):
+        if rais.cnae_id == cnae_id:
             header['ranking'] = index+1
             break
 
     industry_service_num_establishments = Industry(cnae_id=cnae_id)
-    header['num_establishments_brazil'] = industry_service_num_establishments.num_establishments()
+    header[
+        'num_establishments_brazil'] = industry_service_num_establishments.num_establishments()
 
-    return render_template('industry/index.html', body_class='perfil-estado', header=header, body=body, industry=industry)
-
-
-
-
-
+    return render_template(
+        'industry/index.html',
+        body_class='perfil-estado',
+        header=header,
+        body=body,
+        industry=industry)
