1352e27e0bd37359822f499eec6d1affc4947f44
lauradebella <lauradebella@gmail.com>
2016-03-14 15:04:19 -0300

    Running PEP8

--- dataviva/apps/occupation/views.py
+++ dataviva/apps/occupation/views.py
@@ -6,9 +6,9 @@ from dataviva.api.rais.services import OccupationByLocation
 from dataviva.api.rais.services import OccupationMunicipalities
 from dataviva.api.rais.services import OccupationActivities
 
-from dataviva.api.rais.models import Yi, Yo, Ybo, Ybi, Yio, Ybio
+from dataviva.api.rais.models import Yo
 from dataviva import db
-from sqlalchemy import func, desc
+from sqlalchemy import func
 
 mod = Blueprint('occupation', __name__,
                 template_folder='templates',
@@ -28,7 +28,7 @@ def add_language_code(endpoint, values):
 @mod.route('/<occupation_id>')
 def index(occupation_id):
 
-    #Use Example /occupation/2122 OR /occupation/2122?bra_id=4mg
+    # Use Example /occupation/2122 OR /occupation/2122?bra_id=4mg
     bra_id = request.args.get('bra_id')
     header = {}
     body = {}
@@ -41,23 +41,27 @@ def index(occupation_id):
     if len(occupation_id) == 4:
         body['is_family'] = True
     else:
-         body['is_family'] = False
+        body['is_family'] = False
 
     body['is_not_municipality'] = True
 
     if bra_id:
-        occupation_service = OccupationByLocation(occupation_id = occupation_id, bra_id = bra_id)
+        occupation_service = OccupationByLocation(
+            occupation_id=occupation_id, bra_id=bra_id)
         header['location_name'] = occupation_service.location_name()
         if len(bra_id) == 9:
             body['is_not_municipality'] = False
     else:
-        occupation_service = Occupation(occupation_id = occupation_id)
+        occupation_service = Occupation(occupation_id=occupation_id)
 
-    occupation_municipalities_service = OccupationMunicipalities(occupation_id = occupation_id, bra_id=bra_id)
-    occupation_activities_service = OccupationActivities(occupation_id = occupation_id, bra_id = bra_id)
+    occupation_municipalities_service = OccupationMunicipalities(
+        occupation_id=occupation_id, bra_id=bra_id)
+    occupation_activities_service = OccupationActivities(
+        occupation_id=occupation_id, bra_id=bra_id)
 
     header['name'] = occupation_service.occupation_name()
-    header['average_monthly_income'] = occupation_service.average_monthly_income()
+    header[
+        'average_monthly_income'] = occupation_service.average_monthly_income()
     header['salary_mass'] = occupation_service.salary_mass()
     header['total_employment'] = occupation_service.total_employment()
     header['total_establishments'] = occupation_service.total_establishments()
@@ -66,21 +70,29 @@ def index(occupation_id):
 
     if body['is_not_municipality']:
 
-        body['municipality_with_more_jobs'] = occupation_municipalities_service.municipality_with_more_jobs()
-        body['municipality_with_more_jobs_value'] = occupation_municipalities_service.highest_number_of_jobs()
+        body[
+            'municipality_with_more_jobs'] = occupation_municipalities_service.municipality_with_more_jobs()
+        body[
+            'municipality_with_more_jobs_value'] = occupation_municipalities_service.highest_number_of_jobs()
 
-        body['municipality_with_biggest_wage_avg'] = occupation_municipalities_service.municipality_with_biggest_wage_average()
-        body['municipality_with_biggest_wage_avg_value'] = occupation_municipalities_service.biggest_wage_average()
+        body[
+            'municipality_with_biggest_wage_avg'] = occupation_municipalities_service.municipality_with_biggest_wage_average()
+        body[
+            'municipality_with_biggest_wage_avg_value'] = occupation_municipalities_service.biggest_wage_average()
 
-    body['activity_with_more_jobs'] = occupation_activities_service.activity_with_more_jobs()
-    body['activity_with_more_jobs_value'] = occupation_activities_service.highest_number_of_jobs()
+    body[
+        'activity_with_more_jobs'] = occupation_activities_service.activity_with_more_jobs()
+    body[
+        'activity_with_more_jobs_value'] = occupation_activities_service.highest_number_of_jobs()
 
-    body['activity_with_biggest_wage_avg'] = occupation_activities_service.activity_with_biggest_wage_average()
-    body['activity_with_biggest_wage_avg_value'] = occupation_activities_service.biggest_wage_average()
+    body[
+        'activity_with_biggest_wage_avg'] = occupation_activities_service.activity_with_biggest_wage_average()
+    body[
+        'activity_with_biggest_wage_avg_value'] = occupation_activities_service.biggest_wage_average()
 
-    #query relativa a posicao do ranking
-    max_year_query= db.session.query(func.max(Yo.year)).filter(
-            Yo.cbo_id == occupation_id)
+    # query relativa a posicao do ranking
+    max_year_query = db.session.query(func.max(Yo.year)).filter(
+        Yo.cbo_id == occupation_id)
     rais_query = Yo.query.filter(
         Yo.year == max_year_query)\
         .order_by(Yo.num_jobs.desc())
@@ -90,4 +102,4 @@ def index(occupation_id):
             header['ranking'] = index + 1
             break
 
-    return render_template('occupation/index.html', header = header, body = body)
+    return render_template('occupation/index.html', header=header, body=body)
