4ce9b91d2ddbab47af9439849d951921c21f43a1
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-03-16 16:42:14 -0300

    Add id in moke of edicts.

--- dataviva/apps/partners/views.py
+++ dataviva/apps/partners/views.py
@@ -27,35 +27,32 @@ def index():
 @mod.route('/be-a-partner')
 def be_a_partner():
     edicts = [ 
-    {"link" : ("http://www.fapemig.br/admin/editais/upload/20151215143334-Horizon%202020%20FAPEMIG%20guidelines.pdf").decode("utf-8"), "title" : ("Bolsa de Incentivo ao Pesquisador Público Estadual -  BIPDT.").decode("utf-8")},
-    {"link": ("http://www.fapemig.br/admin/editais/upload/20160212152041-Versao%20retificada%20PPM%202016.pdf").decode("utf-8"), "title" : ("Apoio a Núcleo de Inovação Tecnológica").decode("utf-8")}]
+    {"id":1, "link" : ("http://www.fapemig.br/admin/editais/upload/20151215143334-Horizon%202020%20FAPEMIG%20guidelines.pdf").decode("utf-8"), "title" : ("Bolsa de Incentivo ao Pesquisador Público Estadual -  BIPDT.").decode("utf-8")},
+    {"id":2, "link": ("http://www.fapemig.br/admin/editais/upload/20160212152041-Versao%20retificada%20PPM%202016.pdf").decode("utf-8"), "title" : ("Apoio a Núcleo de Inovação Tecnológica").decode("utf-8")}]
 
     return render_template('partners/be-a-partner.html', edicts=edicts)
 
 
-@mod.route('/article/<id>', methods=['GET'])
-def show(id):
-    pass
-
-@mod.route('/article/new', methods=['GET'])
+@mod.route('/edict/new', methods=['GET'])
 def new():
-    pass
+    return render_template('partners/new.html')
 
-@mod.route('/article/<id>/edit', methods=['GET'])
+
+@mod.route('/edict/<id>/edit', methods=['GET'])
 def edit(id):
     pass
 
 
-@mod.route('/article/new', methods=['POST'])
+@mod.route('/edict/new', methods=['POST'])
 def create():
     pass
 
 
-@mod.route('/article/<id>/edit', methods=['POST'])
+@mod.route('/edict/<id>/edit', methods=['POST'])
 def update(id):
     pass
 
 
-@mod.route('/article/<id>/delete', methods=['GET'])
+@mod.route('/edict/<id>/delete', methods=['GET'])
 def delete(id):
     pass
