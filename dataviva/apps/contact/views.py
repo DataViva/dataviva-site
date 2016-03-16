c0cdd237d7557b0f5411836dcd2f3933843212cd
Pedro Goecking <pedrogoecking93@gmail.com>
2016-03-16 12:15:35 -0300

    Adding method that will receive data from form.

--- dataviva/apps/contact/views.py
+++ dataviva/apps/contact/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template, g
+from flask import Blueprint, render_template, g, redirect, url_for, flash
 from dataviva.apps.general.views import get_locale
+from forms import RegistrationForm
+from models import Form
+from dataviva import db
+from datetime import datetime
 
 
 mod = Blueprint('contact', __name__,
@@ -20,4 +24,25 @@ def add_language_code(endpoint, values):
 
 @mod.route('/', methods=['GET'])
 def index():
-    return render_template('contact/index.html')
+    form = RegistrationForm()
+    return render_template('contact/index.html', form=form, action=url_for('contact.create'))
+
+
+@mod.route('/contact/new', methods=['POST'])
+def create():
+    form = RegistrationForm()
+    if form.validate() is False:
+        return render_template('contact/index.html', form=form)
+    else:
+        form = Form()
+        form.name = form.name.data
+        form.email = form.email.data
+        form.message = form.message.data
+        form.postage_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        db.session.add(form)
+        db.session.commit()
+
+        message = u'Sua mensagem foi enviada com sucesso. Em breve retornaremos.'
+        flash(message, 'success')
+        return redirect(url_for('scholar.index'))
