2525feef457e3e89709c4682ab3e8fbb18da5bb2
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-03-16 15:54:21 -0300

    Add data moke to edicts.

--- dataviva/apps/partners/views.py
+++ dataviva/apps/partners/views.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf8 -*-
 from flask import Blueprint, render_template, g
 from dataviva.apps.general.views import get_locale
 
@@ -18,10 +18,44 @@ def add_language_code(endpoint, values):
     values.setdefault('lang_code', get_locale())
 
 
+
+
 @mod.route('/')
 def index():
-    return render_template('partners/index.html', body_class='parceiros')
+    return render_template('partners/index.html')
 
 @mod.route('/be-a-partner')
 def be_a_partner():
-    return render_template('partners/be-a-partner.html')
+    edicts = [ 
+    {"link" : ("http://www.fapemig.br/admin/editais/upload/20151215143334-Horizon%202020%20FAPEMIG%20guidelines.pdf").decode("utf-8"), "title" : ("Bolsa de Incentivo ao Pesquisador Público Estadual -  BIPDT.").decode("utf-8")},
+    {"link": ("http://www.fapemig.br/admin/editais/upload/20160212152041-Versao%20retificada%20PPM%202016.pdf").decode("utf-8"), "title" : ("Apoio a Núcleo de Inovação Tecnológica").decode("utf-8")}]
+
+    return render_template('partners/be-a-partner.html', edicts=edicts)
+
+
+@mod.route('/article/<id>', methods=['GET'])
+def show(id):
+    pass
+
+@mod.route('/article/new', methods=['GET'])
+def new():
+    pass
+
+@mod.route('/article/<id>/edit', methods=['GET'])
+def edit(id):
+    pass
+
+
+@mod.route('/article/new', methods=['POST'])
+def create():
+    pass
+
+
+@mod.route('/article/<id>/edit', methods=['POST'])
+def update(id):
+    pass
+
+
+@mod.route('/article/<id>/delete', methods=['GET'])
+def delete(id):
+    pass
