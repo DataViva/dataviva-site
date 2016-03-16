05b5cccf87142ba4a571ff0b629db1313c408ad5
Pedro Goecking <pedrogoecking93@gmail.com>
2016-03-16 12:18:31 -0300

    Adding file to check entry from user.

---
+++ dataviva/apps/contact/forms.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from flask.ext.wtf import Form
+from wtforms import TextField, TextAreaField, validators, ValidationError
+
+
+class NumberOfWords(object):
+    def __init__(self, max, message=None):
+        self.max = max
+        if not message:
+            message = u"Campo deve possuir no máximo %d palavras." % (max)
+        self.message = message
+
+    def __call__(self, form, field):
+        if len(field.data.split()) > self.max:
+            raise ValidationError(self.message)
+
+
+class RegistrationForm(Form):
+    name = TextField('name', [validators.Required(u"Por favor, insira o seu nome."), validators.Length(max=100)])
+    email = TextField('email', [validators.Required(u"Por favor, insira o seu e-mail."), NumberOfWords(max=1)])
+    message = TextAreaField('message', [validators.Required(u"Por favor, insira a mensagem."), NumberOfWords(max=250)])
