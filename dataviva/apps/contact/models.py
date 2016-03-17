c4ac77672582413c92d50ee8e9ce18dd3d92bb92
Pedro Goecking <pedrogoecking93@gmail.com>
2016-03-17 14:25:17 -0300

    Adjusting type of columns, name of table and return of class.

--- dataviva/apps/contact/models.py
+++ dataviva/apps/contact/models.py
@@ -1,23 +1,24 @@
 from dataviva import db
-import smtplib
+#import smtplib
 
 
 class Form(db.Model):
-    __tablename__ = 'contact_form'
+    __tablename__ = 'contact_message'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(400))
-    email = db.Column(db.Text())
-    message = db.Column(db.String(250))
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    subject = db.Column(db.String(200))
+    message = db.Column(db.Text())
     postage_date = db.Column(db.DateTime)
 
     def date_str(self):
         return self.postage_date.strftime('%d/%m/%Y')
 
     def __repr__(self):
-        return '<Article %r>' % (self.title)
+        return '<Form %r>' % (self.name)
 
-    sender = email
-    receivers = ['contato@dataviva.info']
+    #sender = email
+    #receivers = ['contato@dataviva.info']
 
 #       try:
 #       smtpObj = smtplib.SMTP('localhost')
