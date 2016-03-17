c7b2076fb24e7a6cf704af9df333c8dc0dda6774
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-03-17 15:43:29 -0300

    Implemented editing data.

--- dataviva/apps/partners/views.py
+++ dataviva/apps/partners/views.py
@@ -21,8 +21,6 @@ def add_language_code(endpoint, values):
     values.setdefault('lang_code', get_locale())
 
 
-
-
 @mod.route('/')
 def index():
     return render_template('partners/index.html')
@@ -37,11 +35,6 @@ def be_a_partner():
 def new():
     form = RegistrationForm();
     return render_template('partners/new.html', form=form)
-
-
-@mod.route('/edict/<id>/edit', methods=['GET'])
-def edit(id):
-    pass
 
 @mod.route('/edict/new', methods=['POST'])
 def create():
@@ -61,12 +54,38 @@ def create():
         return redirect(url_for('partners.index'))
 
 
+@mod.route('/edict/<id>/edit', methods=['GET'])
+def edit(id):
+    form = RegistrationForm()
+    edict = Edict.query.filter_by(id=id).first_or_404()
+    form.title.data = edict.title
+    form.link.data = edict.link
+    return render_template('partners/edit.html', form=form, action=url_for('partners.update', id=id))
+
+
 
 @mod.route('/edict/<id>/edit', methods=['POST'])
 def update(id):
-    pass
+    form = RegistrationForm()
+    if form.validate() is False:
+        return render_template('partners/new.html', form=form)
+    else:
+        edict = Edict.query.filter_by(id=id).first_or_404()
+        edict.title = form.title.data
+        edict.link = form.link.data
+
+        db.session.commit()
+
+        message = u'Edital editado com sucesso!'
+        flash(message, 'success')
+        return redirect(url_for('partners.index'))
 
 
 @mod.route('/edict/<id>/delete', methods=['GET'])
 def delete(id):
     pass
+
+
+
+
+    
