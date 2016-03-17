91de92361ae02d796da4ef6b58ac8e63ca437614
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-03-17 17:40:56 -0300

    Add colunm active in calls tables.

--- dataviva/apps/partners/models.py
+++ dataviva/apps/partners/models.py
@@ -5,4 +5,5 @@ class Call(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(400))
     link = db.Column(db.String(250))
+    active = db.Column(db.Integer)
 
