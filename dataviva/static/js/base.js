0cded5d3bdbf590afbf6eaa71f50cce093b416d1
Diogo Lundberg <diogo@lundberg.com.br>
2016-03-27 02:43:53 -0300

    Adds facebook comments

--- dataviva/static/js/base.js
+++ dataviva/static/js/base.js
@@ -1,3 +1,11 @@
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.5&appId=222520191136295";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 $(document).ready(function () {
     new WOW().init();
     $("[data-toggle=popover]").popover({ trigger: "hover" });
