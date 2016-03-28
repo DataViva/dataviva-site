03032dd779cb5798f47ed78d0b24a64ee31ca85f
Victor Moraes <victordmor@Arsenal.local>
2016-03-28 15:08:58 -0300

    PEP-8 ruled

--- dataviva/apps/university/views.py
+++ dataviva/apps/university/views.py
@@ -9,41 +9,43 @@ mod = Blueprint('university', __name__,
                 url_prefix='/<lang_code>/university',
                 static_folder='static')
 
+
 @mod.url_value_preprocessor
 def pull_lang_code(endpoint, values):
     g.locale = values.pop('lang_code')
+
 
 @mod.url_defaults
 def add_language_code(endpoint, values):
     values.setdefault('lang_code', get_locale())
 
+
 @mod.route('/<university_id>')
 def index(university_id):
 
-    #Usage example /university/00575
+    # Usage example /university/00575
 
-    university = UniversityModel.query.filter_by(id=university_id).first_or_404()
+    university = UniversityModel.query.filter_by(
+        id=university_id).first_or_404()
 
     university_service = University(university.id)
     majors_service = UniversityMajors(university.id)
 
     header = {
-        'year' : university_service.year(),
-        'type' : university_service.university_type(),
-        'enrolled' : university_service.enrolled(),
-        'entrants' : university_service.entrants(),
-        'graduates' : university_service.graduates()
+        'year': university_service.year(),
+        'type': university_service.university_type(),
+        'enrolled': university_service.enrolled(),
+        'entrants': university_service.entrants(),
+        'graduates': university_service.graduates()
     }
 
     body = {
-        'major_with_more_enrollments' : majors_service.major_with_more_enrollments(),
-        'highest_enrollment_number_by_major' : majors_service.highest_enrolled_number(),
-        'major_with_more_entrants' : majors_service.major_with_more_entrants(),
-        'highest_entrant_number_by_major' : majors_service.highest_entrants_number(),
-        'major_with_more_graduates' : majors_service.major_with_more_graduates(),
-        'highest_graduate_number_by_major' : majors_service.highest_graduates_number()
+        'major_with_more_enrollments': majors_service.major_with_more_enrollments(),
+        'highest_enrollment_number_by_major': majors_service.highest_enrolled_number(),
+        'major_with_more_entrants': majors_service.major_with_more_entrants(),
+        'highest_entrant_number_by_major': majors_service.highest_entrants_number(),
+        'major_with_more_graduates': majors_service.major_with_more_graduates(),
+        'highest_graduate_number_by_major': majors_service.highest_graduates_number()
     }
 
     return render_template('index.html', university=university, header=header, body=body)
-
-
