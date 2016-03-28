51b5b821a67ad311a3025a01260cf49c13c733a6
Diogo Lundberg <diogo@lundberg.com.br>
2016-03-28 03:46:06 -0300

    Adds image cropper to news

--- dataviva/static/js/modules/news.js
+++ dataviva/static/js/modules/news.js
@@ -13,6 +13,76 @@ $(document).ready(function(){
         $('#text-content-editor').summernote('destroy');
     });
 
+
+
+
+//10x3
+
+    var $image = $("#thumb-image-crop > img")
+    $($image).cropper({
+        aspectRatio: 350/227,
+        preview: ".img-preview",
+        viewMode: 3,
+        done: function(data) {
+            // Output the result data for cropping image.
+        }
+    });
+
+    var $thumbInput = $("#thumb-input");
+    if (window.FileReader) {
+        $thumbInput.change(function() {
+            var fileReader = new FileReader(),
+                    files = this.files,
+                    file;
+
+            if (!files.length) {
+                return;
+            }
+
+            file = files[0];
+
+            if (/^image\/\w+$/.test(file.type)) {
+                fileReader.readAsDataURL(file);
+                fileReader.onload = function () {
+                    $thumbInput.val("");
+                    $image.cropper("reset", true).cropper("replace", this.result);
+                };
+            } else {
+                showMessage("Please choose an image file.");
+            }
+        });
+    } else {
+        $thumbInput.addClass("hide");
+    }
+
+    $("#download").click(function() {
+        $("#thumb-image").attr("src",$image.cropper("getDataURL"));
+    });
+
+    $("#zoomIn").click(function() {
+        $image.cropper("zoom", 0.1);
+    });
+
+    $("#zoomOut").click(function() {
+        $image.cropper("zoom", -0.1);
+    });
+
+    $("#rotateLeft").click(function() {
+        $image.cropper("rotate", 45);
+    });
+
+    $("#rotateRight").click(function() {
+        $image.cropper("rotate", -45);
+    });
+
+
+
+
+
+
+
+
+
     $(function() {
         $('#news-form').submit(function() {
             var aHTML = $('#text-content-editor').summernote('code');
