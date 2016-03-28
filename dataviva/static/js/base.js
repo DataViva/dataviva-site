3a980f595142cf6e8a4995f2a431483da8cc1075
Diogo Lundberg <diogo@lundberg.com.br>
2016-03-28 01:46:08 -0300

    Moves summernote config to base js

--- dataviva/static/js/base.js
+++ dataviva/static/js/base.js
@@ -16,6 +16,35 @@ $(document).ready(function () {
     }(document, 'script', 'facebook-jssdk'));
 });
 
+var lang = document.documentElement.lang
+
+if (lang == 'pt') {
+    lang = 'pt-BR';
+} else if (lang == 'en') {
+    lang = 'en-US';
+}
+
+window.summernoteConfig = {
+    lang: lang,
+    toolbar: [
+        ['style', ['style']],
+        ['font', ['bold', 'italic', 'underline', 'clear']],
+        ['fontname', ['fontname']],
+        ['fontsize', ['fontsize']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['table', ['table']],
+        ['insert', ['link', 'picture', 'video']],
+        ['view', ['fullscreen', 'codeview', 'help']]
+      ],
+    fontNames: [
+        'Arial', 'Arial Black', 'Comic Sans MS', 'Courier New',
+        'Helvetica Neue', 'Helvetica', 'Impact', 'Lucida Grande',
+        'Open Sans', 'Tahoma', 'Times New Roman', 'Verdana'
+      ],
+    placeholder: 'Escreva aqui o conteúdo desta publicação'
+}
+
 function selectorCallback(id, event) {
     url = window.location.origin + window.location.pathname + '?bra_id='+id;
     window.location = url;
--- dataviva/static/js/modules/news.js
+++ dataviva/static/js/modules/news.js
@@ -1,33 +1,4 @@
 $(document).ready(function(){
-    var lang = document.documentElement.lang
-
-    if (lang == 'pt') {
-        lang = 'pt-BR';
-    } else if (lang == 'en') {
-        lang = 'en-US';
-    }
-
-    var summernoteConfig = {
-        lang: lang,
-        toolbar: [
-            ['style', ['style']],
-            ['font', ['bold', 'italic', 'underline', 'clear']],
-            ['fontname', ['fontname']],
-            ['fontsize', ['fontsize']],
-            ['color', ['color']],
-            ['para', ['ul', 'ol', 'paragraph']],
-            ['table', ['table']],
-            ['insert', ['link', 'picture', 'video']],
-            ['view', ['fullscreen', 'codeview', 'help']]
-          ],
-        fontNames: [
-            'Arial', 'Arial Black', 'Comic Sans MS', 'Courier New',
-            'Helvetica Neue', 'Helvetica', 'Impact', 'Lucida Grande',
-            'Open Sans', 'Tahoma', 'Times New Roman', 'Verdana'
-          ],
-        placeholder: 'Escreva aqui o conteúdo desta publicação'
-    }
-
     $('#text-content-editor').append($('#text_content').val())
     $('#text-content-editor').summernote(summernoteConfig);
 
