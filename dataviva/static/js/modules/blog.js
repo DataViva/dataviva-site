89b0089495cf2b2c5e8e731ec3a728da65987ee3
Diogo Lundberg <diogo@lundberg.com.br>
2016-03-29 14:29:11 -0300

    Moves javascript from admin html to the js file

--- dataviva/static/js/modules/blog.js
+++ dataviva/static/js/modules/blog.js
@@ -14,7 +14,6 @@ $(document).ready(function(){
         $('#text-content-editor').summernote('destroy');
     });
 
-//10x3
     var inputThumbCallback = function() {
         $($('#thumb-crop')).cropper({
             aspectRatio: 350/227,
@@ -61,3 +60,126 @@ $(document).ready(function(){
         });
     });
 });
+
+
+var ScholarApproval = function () {
+    this.table = $('#approvalTable').DataTable({
+        "sAjaxSource": "/blog/all",
+        "sAjaxDataProp": "posts",
+        "order": [],
+        "aoColumnsDefs": [
+            { "data": "postselector" },
+            { "data": "title" },
+            { "data": "authors" },
+            { "data": "publicationDate" },
+            { "data": "active" },
+        ],
+        "columnDefs": [
+            {
+                "targets": 0,
+                "orderable": false,
+                "className": "column-checkbox",
+                "render": function (data, type, post, meta){
+
+                    var checkbox = '<div class="checkbox checkbox-success">' +
+                                   '    <input name="postselector" id="'+post[0]+'" type="checkbox">' +
+                                   '    <label for="'+post[0]+'"></label>'
+                                   '</div>';
+
+                    return checkbox;
+                }
+            },
+            {
+                "targets": 1,
+                "className": "column-title",
+                "render": function (data, type, post, meta){
+                    return '<a href="/blog/post/'+post[0]+'">'+post[1]+'</a>';
+                }
+            },
+            {
+                "targets": 4,
+                "orderable": false,
+                "className": "column-checkbox",
+                "render": function (data, type, post, meta){
+                    if (data){
+                       return '<input type="checkbox" class="js-switch" name="checkboxApproval" value="'+post[0]+'" checked>';
+                    }
+                    else {
+                       return '<input type="checkbox" class="js-switch" name="checkboxApproval" value="'+post[0]+'">';
+                    }
+                }
+              }],
+        "columns": [
+                { "width": "8%" },
+                null,
+                null,
+                { "width": "20%" },
+                { "width": "12%" }
+        ],
+        "paging": false,
+        "bFilter": false,
+        "info": false,
+        "initComplete": function(settings, json) {
+            $( ".js-switch" ).each(function() {
+                var switchery = new Switchery(this, {
+                    size: 'small',
+                    color: '#5A9DC4'
+                });
+            });
+        }
+    });
+
+};
+
+ScholarApproval.prototype.multipleCheckbox = function(){
+    $('#approvalTable thead').on('click','#select-all', function () {
+        var posts = {};
+        $('input[name="checkboxApproval"]').each(function() {
+            posts[this.value] = this.checked;
+        });
+        sendCheckedArticle(posts);
+    });
+};
+
+ScholarApproval.prototype.singleCheckbox = function(){
+    $('#approvalTable tbody').on('click','.body-checkbox', function () {
+        var post = {};
+        post[this.firstElementChild.value] = this.firstElementChild.checked;
+        sendCheckedArticle(post);
+    });
+};
+
+
+ScholarApproval.prototype.controlCheckbox = function(approvalTable){
+    $('#select-all').on('click', function(){
+       var rows = approvalTable.table.rows({ 'search': 'applied' }).nodes();
+       $('input[type="checkbox"]', rows).prop('checked', this.checked);
+    });
+
+    $('#approvalTable tbody').on('change', 'input[type="checkbox"]', function(){
+       if(!this.checked){
+          var el = $('#select-all').get(0);
+          if(el && el.checked && ('indeterminate' in el)){
+             el.indeterminate = true;
+            }
+        }
+    });
+};
+
+var approvalTable = new ScholarApproval();
+approvalTable.multipleCheckbox();
+approvalTable.singleCheckbox();
+approvalTable.controlCheckbox(approvalTable);
+
+
+
+function sendCheckedArticle(postsApproval){
+    var lang = $('html').attr('lang');
+    $.ajax({
+      method: "POST",
+      url: "/"+lang+"/blog/admin/activate",
+      data: postsApproval,
+      success: function (msg) {
+      }
+    })
+}
