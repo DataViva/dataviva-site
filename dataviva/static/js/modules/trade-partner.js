ea3aea5620b6846f6fd5f4ec59d8024526a4c54e
lauradebella <lauradebella@gmail.com>
2016-03-29 15:59:36 -0300

    Adds js file.

---
+++ dataviva/static/js/modules/trade-partner.js
@@ -0,0 +1,64 @@
+window.showGraph = function(trade-partner, location, tab) {
+    if ($('#graphs #graphs-' + tab).length === 0) {
+        $.ajax({
+            method: "POST",
+            url: trade-partner+"/graphs/"+tab+"?bra_id="+location,
+            success: function (graphs) {
+                $('#graphs').append(graphs);
+            }
+        });
+    }
+    $("#graphs").children().hide();
+    $('#graphs #graphs-' + tab).show();
+}
+
+$(document).ready(function () {
+    $(function() {
+        var jcarousel = $('.jcarousel');
+
+        jcarousel
+            .on('jcarousel:reload jcarousel:create', function () {
+                var carousel = $(this),
+                    width = carousel.innerWidth();
+
+                if (width >= 600) {
+                    width = 200;
+                } else if (width >= 350) {
+                    width = 170;
+                }
+
+                carousel.jcarousel('items').css('width',width);
+            })
+            .jcarousel({
+                wrap: 'circular'
+            });
+
+        $('.jcarousel-control-prev').jcarouselControl({
+            target: '-=1'
+        });
+
+        $('.jcarousel-control-next').jcarouselControl({
+            target: '+=1'
+        });
+    });
+
+    if(document.location.hash) {
+        var tab = document.location.hash.substring(1),
+            trade-partner = document.location.pathname.split('/')[3],
+            location = getParameterByName('bra_id');
+
+        $('[href=#' + tab + ']').tab('show');
+
+        showGraph(trade-partner, location, tab);
+    }
+
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        if ($(this).attr('graph') != null) {
+            var trade-partner = this.dataset.trade-partner,
+                location = this.dataset.location,
+                tab = $(this).attr('aria-controls');
+
+            showGraph(trade-partner, location, tab);
+        }
+    });
+});
