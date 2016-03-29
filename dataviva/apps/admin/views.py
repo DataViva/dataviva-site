59b114c7c22aacf04457b086f83acaa927c15386
Diogo Lundberg <diogo@lundberg.com.br>
2016-03-29 10:42:24 -0300

    Adds admin template

--- dataviva/apps/admin/views.py
+++ dataviva/apps/admin/views.py
@@ -1,27 +1,11 @@
-from sqlalchemy import func
-from flask import Blueprint, render_template, g, redirect, url_for, \
-    flash, request, jsonify, make_response
-from flask.ext.login import login_required
+from flask import Blueprint, render_template, g
 from flask.ext.babel import gettext
-from dataviva import db, app
-from datetime import datetime
 from dataviva.apps.general.views import get_locale
-# models
-from dataviva.apps.account.models import User
-from dataviva.apps.ask.models import Question, Status, Reply, Flag, Vote
-# forms
-from dataviva.apps.admin.forms import AdminQuestionUpdateForm
-from dataviva.utils.jinja_helpers import jinja_strip_html
-
-# utils
-from dataviva.utils import send_mail
-
 from functools import wraps
 
-# import urllib2, urllib
-# from config import SITE_MIRROR
-
-mod = Blueprint('admin', __name__, url_prefix='/<lang_code>/admin')
+mod = Blueprint('admin', __name__,
+                template_folder='templates',
+                url_prefix='/<lang_code>/admin')
 
 
 def get_current_user_role():
@@ -56,4 +40,4 @@ def pull_lang_code(endpoint, values):
 
 @mod.route('/')
 def index():
-    return 'admin'
+    return render_template('admin/index.html')
