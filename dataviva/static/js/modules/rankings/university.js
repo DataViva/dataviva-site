f678c63c76571f2be7a843a4f62230379649075e
Victor Moraes <victordmor@Arsenal.local>
2016-04-05 12:21:12 -0300

    Adding university JS controller

---
+++ dataviva/static/js/modules/rankings/university.js
@@ -0,0 +1,56 @@
+var universityTable = function () {
+    this.tableId = '#university-table';
+
+    this.table = $(this.tableId).DataTable({
+        "sAjaxSource": "/hedu/all/all/show/all/?order=enrolled.desc",
+        "sAjaxDataProp": "data",
+        "order": [],
+        "aoColumns": [
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            { "bVisible": false },
+            { "bVisible": false },
+            { "bVisible": false },
+            { "bVisible": false },
+            null,
+            null,
+            null,
+            null,
+            null,
+            { "bVisible": false },
+            null
+        ],
+        "deferRender": true,
+        "scrollY": 500,
+        "scrollCollapse": true,
+        "scroller": true,
+        initComplete: function () {
+            var select = $('#year-selector')
+
+            universityTable.table
+                .column( 0 )
+                .cache( 'search' )
+                .sort()
+                .unique()
+                .each( function ( d ) {
+                    select.append( $('<option value="'+d+'">'+d+'</option>') );
+                } );
+
+            select.on( 'change', function () {
+               universityTable.table
+                    .column( 0 )
+                    .search( $(this).val() )
+                    .draw();
+            });
+
+            $('#year-selector').append(select);
+        }
+    });
+};
+
+var universityTable = new universityTable();
