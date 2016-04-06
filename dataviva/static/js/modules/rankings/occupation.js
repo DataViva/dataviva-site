d369c10ffb22f71d884e9e312ad3fa2f4126b49c
FredericoVieira <fredops1@hotmail.com>
2016-04-06 14:51:30 -0300

    Fixes selected url to get json by occupations groups.

--- dataviva/static/js/modules/rankings/occupation.js
+++ dataviva/static/js/modules/rankings/occupation.js
@@ -6,24 +6,24 @@ var OccupationTable = function () {
         "sAjaxDataProp": "data",
         "order": [],
         "aoColumns": [
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            { "bVisible": false },
-            { "bVisible": false },
-            { "bVisible": false },
-            { "bVisible": false },
-            null,
-            null,
-            null,
-            null,
-            null,
-            { "bVisible": false },
-            null
+            null, //year a
+            null, //wage a
+           { "bVisible": false }, //num_emp
+            null, //num_jobs a
+            { "bVisible": false }, //num_est
+            null, //wage_avg a
+            { "bVisible": false }, //age_avg
+            { "bVisible": false }, //wage_growth
+            { "bVisible": false }, //wage_growth_5
+            { "bVisible": false }, //num_emp_growth
+            { "bVisible": false }, //num_emp_growth_5
+            null, //cbo_id
+            null, //cnae_diversity a
+            null, //cnae_diversity_eff a
+            { "bVisible": false }, //bra_diversity
+            { "bVisible": false }, //bra_diversity_eff
+            { "bVisible": false }, //hist
+            { "bVisible": false } //gini
         ],
         "deferRender": true,
         "scrollY": 500,
@@ -55,22 +55,23 @@ var OccupationTable = function () {
 
 var occupationTable = new OccupationTable();
 
-var occupationFamilies = function() {
+var occupationGroups = function() {
     occupationTable.table.ajax.url("/rais/all/all/all/show.1/?order=num_jobs.desc").load();
 };
 
-var occupationGroups = function() {
+var occupationFamilies = function() {
     occupationTable.table.ajax.url("/rais/all/all/all/show.4/?order=num_jobs.desc").load();
 };
 
-$(document).ready(function(){
 
-    $('#occupation-families').click(function() {
-        occupationFamilies();
-    });
+$(document).ready(function(){
 
     $('#occupation-groups').click(function() {
         occupationGroups();
+    });
+
+    $('#occupation-families').click(function() {
+        occupationFamilies();
     });
 
 });
