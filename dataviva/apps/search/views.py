21179533e1933375d985dc185c5330a90523447e
FredericoVieira <fredops1@hotmail.com>
2016-04-08 19:55:20 -0300

    Adds controller methods for search module.

--- dataviva/apps/search/views.py
+++ dataviva/apps/search/views.py
@@ -2,6 +2,11 @@
 from flask import Blueprint, render_template, g, redirect, url_for, flash, jsonify, request
 from dataviva.apps.general.views import get_locale
 
+from models import SearchQuestion, SearchSelector
+from dataviva import db
+from forms import RegistrationForm
+
+
 mod = Blueprint('search', __name__,
                 template_folder='templates',
                 url_prefix='/<lang_code>/search')
@@ -27,9 +32,19 @@ def index():
     pass
 
 
+@mod.route('/question/all', methods=['GET'])
+def all_questions():
+    result = SearchQuestion.query.all()
+    questions = []
+    for row in result:
+        questions += [(row.id, row.profile_id, row.description, row.selectors_str(), row.answer)]
+    return jsonify(questions=questions)
+
+
 @mod.route('/admin', methods=['GET'])
 def admin():
-    return render_template('search/admin.html')
+    questions = SearchQuestion.query.all()
+    return render_template('search/admin.html', questions=questions)
 
 
 @mod.route('/admin/question/new', methods=['GET'])
@@ -40,4 +55,27 @@ def new():
 
 @mod.route('/admin/question/new', methods=['POST'])
 def create():
-    pass
+    form = RegistrationForm()
+    if form.validate() is False:
+        return render_template('search/new.html', form=form)
+    else:
+        question = SearchQuestion()
+        profile_id = form.profile.data
+        #Id=1, profile=Entrepreneurs / Empreendedores
+        #Id=2, profile=Development Agents / Agentes de Desenvolvimento
+        #Id=3, profile=Students and Professionals / Estudantes e Profissionais
+        profile_id = 1
+        question.profile_id = profile_id
+        question.description = form.description.data
+        question.answer = form.answer.data
+
+        selector_input_list = form.selector.data.split(',')
+        for selector_input in selector_input_list:
+            question.selectors.append(SearchSelector(selector_input))
+
+        db.session.add(question)
+        db.session.commit()
+
+        message = u'Muito obrigado! Sua pesquisa foi submetida com sucesso!'
+        flash(message, 'success')
+        return redirect(url_for('search.admin'))
