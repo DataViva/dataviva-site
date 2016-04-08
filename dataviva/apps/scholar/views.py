0dd544254ede3623d0216800b2b9ea8dca1fbca8
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-04-08 17:24:44 -0300

    Add g type and css file.

--- dataviva/apps/scholar/views.py
+++ dataviva/apps/scholar/views.py
@@ -13,6 +13,11 @@ mod = Blueprint('scholar', __name__,
                 url_prefix='/<lang_code>/scholar')
 
 
+@mod.before_request
+def before_request():
+    g.page_type = mod.name
+
+
 @mod.url_value_preprocessor
 def pull_lang_code(endpoint, values):
     g.locale = values.pop('lang_code')
