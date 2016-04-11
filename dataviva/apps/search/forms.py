466f5c05a3361f0459d7be0c632dec044dec7cff
Frederico <fredops1@hotmail.com>
2016-04-10 23:18:57 -0300

    Updates form variable profile to show dinamicaly options storaged in database.

--- dataviva/apps/search/forms.py
+++ dataviva/apps/search/forms.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from flask.ext.wtf import Form
-from wtforms import TextField, validators
+from wtforms import TextField, SelectField, validators
+from models import SearchProfile
 
 
 class RegistrationForm(Form):
-    profile = TextField('profile', validators=[
-        validators.Required(u"Por favor, selecione o perfil da pesquisa."),
-        validators.Length(max=400)
-    ])
+
+    profile = SelectField('profile', coerce=int, choices=[(profile.id, profile.name) for profile in SearchProfile.query.order_by('name')], validators=[
+        validators.Required(u"Por favor, selecione o profile.")])
 
     description = TextField('description', validators=[
         validators.Required(u"Por favor, insira a pergunta."),
