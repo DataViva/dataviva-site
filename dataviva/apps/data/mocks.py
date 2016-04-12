ee5b12030beb421b90019a826d7d0af1cf5ee61a
Diogo Lundberg <diogo@lundberg.com.br>
2016-04-12 14:17:18 -0300

    Adds rais database mock

---
+++ dataviva/apps/data/mocks.py
@@ -0,0 +1,28 @@
+from collections import namedtuple
+
+
+DataBase = namedtuple('DataBase', ['dimensions'])
+Dimension = namedtuple('Dimension', ['name', 'depths'])
+Depth = namedtuple('Depth', ['id', 'value'])
+
+
+locations = Dimension('bra_plural', [
+    Depth('bra_1', 'regions'),
+    Depth('bra_3', 'states'),
+    Depth('bra_5', 'mesoregions'),
+    Depth('bra_7', 'microregions'),
+    Depth('bra_9', 'municipalities'),
+])
+
+industries = Dimension('cnae_plural', [
+    Depth('cnae_1', 'sections'),
+    Depth('cnae_3', 'divisions'),
+    Depth('cnae_6', 'classes'),
+])
+
+occupations = Dimension('cbo_plural', [
+    Depth('cbo_1', 'main_groups'),
+    Depth('cbo_4', 'families'),
+])
+
+rais_dimensions = [locations, occupations, industries]
