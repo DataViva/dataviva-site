e834a972e0cd361f193d72897b04f151b76aa16f
FredericoVieira <fredops1@hotmail.com>
2016-04-13 19:01:25 -0300

    Updates scholar form.

--- dataviva/apps/scholar/forms.py
+++ dataviva/apps/scholar/forms.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from flask.ext.wtf import Form
-from wtforms import FileField, TextField, TextAreaField, validators, ValidationError
+from wtforms import FileField, HiddenField, TextField, TextAreaField, validators, ValidationError
 
 
 class NumberOfWords(object):
+
     def __init__(self, max, message=None):
         self.max = max
         if not message:
@@ -16,13 +17,38 @@ class NumberOfWords(object):
 
 
 class RegistrationForm(Form):
-    title = TextField('title', [validators.Required(u"Por favor, insira o título do artigo."), validators.Length(max=400)])
-    theme = TextField('theme', [validators.Required(u"Por favor, insira o tema do artigo."), NumberOfWords(max=5)])
-    authors = TextField('authors', [validators.Required(u"Por favor, insira o(s) autor(es) do artigo."), validators.Length(max=100)])
-    keywords = TextField('keywords', [validators.Required(u"Por favor, insira as palavras-chave do artigo."), NumberOfWords(max=3)])
-    abstract = TextAreaField('abstract', [validators.Required(u"Por favor, insira o resumo do artigo."), NumberOfWords(max=250)])
-    article_file = FileField('article_file', [validators.Required(u"Por favor, insira o arquivo do artigo.")])
+    title = TextField('title', validators=[
+        validators.Required(u"Por favor, insira o título do artigo."),
+        validators.Length(max=400)
+    ])
 
+    theme = TextField('theme', validators=[
+      validators.Required(u"Por favor, insira o tema do artigo."),
+      NumberOfWords(max=5)
+    ])
+
+    authors = TextField('authors', validators=[
+        validators.Required(u"Por favor, insira o(s) autor(es) do artigo."),
+        validators.Length(max=100)
+    ])
+
+    keywords = TextField('keywords', validators=[
+        validators.Required(u"Por favor, insira as palavras-chave do artigo."),
+        NumberOfWords(max=3)
+    ])
+
+    abstract = TextAreaField('abstract', validators=[
+        validators.Required(u"Por favor, insira o resumo do artigo."),
+        NumberOfWords(max=250)
+    ])
+
+    article_file = FileField('article_file', validators=[
+        validators.Required(u"Por favor, insira o arquivo do artigo.")
+    ])
+
+    file_path = HiddenField('file_path', validators=[
+        validators.Required(u"Por favor, insira um arquivo para o artigo.")
+    ])
 
 '''
 REGRAS FORMULÁRIO:
