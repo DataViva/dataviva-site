462115dc28c355e794e9bca92266804cba791dd7
FredericoVieira <fredops1@hotmail.com>
2016-04-15 17:36:04 -0300

    Updates controller methods to support files upload.

--- dataviva/apps/scholar/views.py
+++ dataviva/apps/scholar/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template, g, redirect, url_for, flash, jsonify, request
+from flask import Blueprint, render_template, g, redirect, url_for, flash, jsonify, request, send_from_directory
 from dataviva.apps.general.views import get_locale
 
 from sqlalchemy import desc
@@ -7,6 +7,37 @@ from models import Article, AuthorScholar, KeyWord
 from dataviva import db
 from forms import RegistrationForm
 from datetime import datetime
+
+import os
+import simplejson
+import upload_file as uploadfile
+from werkzeug import secure_filename
+from dataviva import app
+
+app.config['UPLOAD_FOLDER'] = os.getcwd()+'/dataviva/static/data/scholar/'
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['pdf', 'doc', 'docx'])
+IGNORED_FILES = set(['.gitignore'])
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def gen_file_name(filename):
+    """
+    If file was exist already, rename it and return a new name
+    """
+
+    i = 1
+    while os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
+        name, extension = os.path.splitext(filename)
+        filename = '%s_%s%s' % (name, str(i), extension)
+        i = i + 1
+
+    return filename
 
 
 mod = Blueprint('scholar', __name__,
@@ -56,6 +87,19 @@ def edit(id):
     form.authors.data = article.authors_str()
     form.keywords.data = article.keywords_str()
     form.abstract.data = article.abstract
+
+    files = [f for f in os.listdir(app.config['UPLOAD_FOLDER']) if os.path.isfile(
+        os.path.join(app.config['UPLOAD_FOLDER'], f)) and f not in IGNORED_FILES]
+
+    file_display = []
+
+    for f in files:
+        size = os.path.getsize(os.path.join(app.config['UPLOAD_FOLDER'], f))
+        file_saved = uploadfile(name=f, size=size)
+        file_display.append(file_saved.get_file())
+
+    #return simplejson.dumps({"files": file_display})
+
     return render_template('scholar/edit.html', form=form, action=url_for('scholar.update', id=id))
 
 
@@ -71,11 +115,28 @@ def create():
         article.abstract = form.abstract.data
         form.article.data
 
-        import os
-        file = request.files['article']
-        last_article = Article.query.order_by(desc(Article.postage_date)).first_or_404()
-        filename = str(last_article.id)
-        file.save(os.path.join('/home/fvieira/Desktop', filename+'.pdf'))
+        file = request.files['file']
+
+        if file:
+            filename = secure_filename(file.filename)
+            filename = gen_file_name(filename)
+            mimetype = file.content_type
+
+            if not allowed_file(file.filename):
+                result = uploadfile(name=filename, type=mimetype, size=0, not_allowed_msg="Filetype not allowed")
+
+            else:
+                # save file to disk
+                uploaded_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(uploaded_file_path)
+
+                # get file size after saving
+                size = os.path.getsize(uploaded_file_path)
+
+                # return json for js call back
+                result = uploadfile(name=filename, type=mimetype, size=size)
+
+            #return simplejson.dumps({"files": [result.get_file()]})
 
         article.postage_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         article.approval_status = 0
@@ -112,7 +173,30 @@ def update(id):
         article.title = form.title.data
         article.theme = form.theme.data
         article.abstract = form.abstract.data
-        form.article = 'test'
+
+        file = request.files['file']
+
+        if file:
+            filename = secure_filename(file.filename)
+            filename = gen_file_name(filename)
+            mimetype = file.content_type
+
+            if not allowed_file(file.filename):
+                result = uploadfile(name=filename, type=mimetype, size=0, not_allowed_msg="Filetype not allowed")
+
+            else:
+                # save file to disk
+                uploaded_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(uploaded_file_path)
+
+                # get file size after saving
+                size = os.path.getsize(uploaded_file_path)
+
+                # return json for js call back
+                result = uploadfile(name=filename, type=mimetype, size=size)
+
+            #return simplejson.dumps({"files": [result.get_file()]})
+
         article.postage_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         article.authors = []
         article.keywords = []
@@ -183,5 +267,24 @@ def all():
     result = Article.query.all()
     articles = []
     for row in result:
-        articles += [(row.id, row.title, row.authors_str(), row.postage_date.strftime('%d/%m/%Y'), row.approval_status)]
+        articles += [(row.id, row.title, row.authors_str(),
+                      row.postage_date.strftime('%d/%m/%Y'), row.approval_status)]
     return jsonify(articles=articles)
+
+
+@mod.route("/delete/<string:filename>", methods=['DELETE'])
+def delete(filename):
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+    if os.path.exists(file_path):
+        try:
+            os.remove(file_path)
+            return simplejson.dumps({filename: 'True'})
+        except:
+            return simplejson.dumps({filename: 'False'})
+
+
+# serve static files
+@mod.route("/data/<string:filename>", methods=['GET'])
+def get_file(filename):
+    return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER']), filename=filename)
