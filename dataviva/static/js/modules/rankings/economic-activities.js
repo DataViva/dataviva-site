57fe5128a257999be564da6bff61bf425f305354
FredericoVieira <fredops1@hotmail.com>
2016-04-20 16:08:52 -0300

    Reoders rankings columns exhibition in economic-activities.

--- dataviva/static/js/modules/rankings/economic-activities.js
+++ dataviva/static/js/modules/rankings/economic-activities.js
@@ -10,11 +10,18 @@ var EconomicActivitiesTable = function () {
             {data: 10},
             {data: 11},
             null,
-            {data: 0},
             {data: 2},
+            {data: 0},
+            {data: 3},
             {data: 4},
+            {data: 8},
+            {data: 9},
+            {data: 6},
+            {data: 7},
             {data: 12},
-            {data: 13}
+            {data: 13},
+            {data: 14},
+            {data: 15}
         ],
         "columnDefs": [
             {
