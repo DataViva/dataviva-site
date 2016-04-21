ea918515a41760435a05f90b9086e157d2f4ba68
Diogo Lundberg <diogo@lundberg.com.br>
2016-04-21 13:03:55 -0300

    Adds help models

--- dataviva/apps/help/models.py
+++ dataviva/apps/help/models.py
@@ -1,23 +1,23 @@
 from dataviva import db
 from sqlalchemy import ForeignKey
-​
-​
+
+
 class HelpSubjectQuestion(db.Model):
     __tablename__ = 'help_subject_question'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(400))
     answer = db.Column(db.String(400))
     subject_id = db.Column(db.Integer, ForeignKey('help_subject.id'))
-​
+
     def __repr__(self):
         return '<SubjectQuestion %r>' % (self.description)
-​
-​
+
+
 class HelpSubject(db.Model):
     __tablename__ = 'help_subject'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     questions = db.relationship("HelpSubjectQuestion", backref='help_subject_question')
-​
+
     def __repr__(self):
         return '<Subject %r>' % (self.name)
