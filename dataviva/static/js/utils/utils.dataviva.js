befe417d5380e5bd624b20f406c91bd2c235e6d2
r13 <diogo@lundberg.com.br>
2016-04-25 15:08:09 -0300

    rankings e ajustes gerais

--- dataviva/static/js/utils/utils.dataviva.js
+++ dataviva/static/js/utils/utils.dataviva.js
@@ -224,7 +224,7 @@ dataviva.format.number = function(value, opts) {
   else if (growth) {
     return_value += "%";
     var arrow = negative ? "down" : "up";
-    return_value = "<span class='"+arrow+"'>" + return_value + "<i class='growth-arrow "+arrow+" fa fa-arrow-circle-"+arrow+"'></i>" + "</span>";
+    return_value = "<span class='text-"+arrow+"'>" + return_value + "&nbsp;<i class='growth-arrow "+arrow+" fa fa-arrow-circle-"+arrow+"'></i>" + "</span>";
   }
 
   return_value = String(return_value)
