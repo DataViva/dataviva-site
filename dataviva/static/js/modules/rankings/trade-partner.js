c9854ff352c0e038c64b580a600d943a68817af8
Diogo Lundberg <diogo@lundberg.com.br>
2016-04-26 11:47:52 -0300

    Fixes #847

--- dataviva/static/js/modules/rankings/trade-partner.js
+++ dataviva/static/js/modules/rankings/trade-partner.js
@@ -43,61 +43,71 @@ var TradePartnerTable = function () {
                 render: function (data, type, row, meta){
                     return dataviva.format.number(row[3], {"key": headers[3]});
                 },
-                className: "table-number"
+                className: "table-number",
+                type: 'num-dataviva'
             },
             {
                 render: function (data, type, row, meta){
                     return dataviva.format.number(row[2], {"key": headers[2]});
                 },
-                className: "table-number"
+                className: "table-number",
+                type: 'num-dataviva'
             },
             {
                 render: function (data, type, row, meta){
                     return dataviva.format.number(row[8], {"key": headers[8]});
                 },
-                className: "table-number"
+                className: "table-number",
+                type: 'num-dataviva'
             },
             {
                 render: function (data, type, row, meta){
                     return dataviva.format.number(row[9], {"key": headers[9]});
                 },
-                className: "table-number"
+                className: "table-number",
+                type: 'num-dataviva'
             },
             {
                 render: function (data, type, row, meta){
                     return dataviva.format.number(row[6], {"key": headers[6]});
                 },
-                className: "table-number"
+                className: "table-number",
+                type: 'num-dataviva'
             },
             {
                 render: function (data, type, row, meta){
                     return dataviva.format.number(row[7], {"key": headers[7]});
                 },
-                className: "table-number"
+                className: "table-number",
+                type: 'num-dataviva'
             },
             {
                 render: function (data, type, row, meta){
                     return dataviva.format.number(row[10], {"key": headers[10]});
                 },
-                className: "table-number"
+                className: "table-number",
+                type: 'num-dataviva'
             },
             {
                 render: function (data, type, row, meta){
                     return dataviva.format.number(row[11], {"key": headers[11]});
                 },
-                className: "table-number"
+                className: "table-number",
+                type: 'num-dataviva'
             },
             {
                 render: function (data, type, row, meta){
                     return dataviva.format.number(row[12], {"key": headers[12]});
                 },
-                className: "table-number"
+                className: "table-number",
+                type: 'num-dataviva'
             },
             {
                 render: function (data, type, row, meta){
                     return dataviva.format.number(row[13], {"key": headers[13]});
                 },
-                className: "table-number"
+                className: "table-number",
+                type: 'num-dataviva'
             }
         ],
         "deferRender": true,
