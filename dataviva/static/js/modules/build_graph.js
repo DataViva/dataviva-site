e4c0547ae1a59d7d0fa3885587c40cb14de964dc
Diogo Lundberg <diogo@lundberg.com.br>
2016-04-28 15:24:18 -0300

    Adds new selector to build a graph

--- dataviva/static/js/modules/build_graph.js
+++ dataviva/static/js/modules/build_graph.js
@@ -1,34 +1,30 @@
 var dimensionSelectCallback = function(id, event) {
-    $('#'+this).html(dataviva[this.toString()][id].name);
+    $('#'+id).html(d.name);
     $('#modal-selector').modal('hide');
 }
 
-$(document).ready(function() {
+dataviva.requireAttrs(['datasets'], function() {
 
+    for (dataset in dataviva.datasets) {
+        $('#datasets').append( $('<option value="'+dataset+'">'+dataviva.dictionary[dataset]+'</option>'));
+    }
 
-    dataviva.requireAttrs(['datasets', 'bra', 'cnae', 'cbo', 'hs', 'wld', 'course_hedu', 'course_sc', 'university'], function() {
+    $('#datasets').on('change', function() {
+        $('#datasets #dataset-empty-option').remove();
 
-        for (dataset in dataviva.datasets) {
-            $('#datasets').append( $('<option value="'+dataset+'">'+dataviva.dictionary[dataset]+'</option>'));
-        }
+        $('#dimensions').empty();
+        dataviva.datasets[this.value].dimensions.forEach(function(dimension) {
+            var div = $("<div></div>").addClass("form-group"),
+                label = $("<label></label>").attr("for", dimension.id).addClass("control-label"),
+                deactivate_button = $("<button></button>").attr("for", dimension.id).addClass("btn btn-xs btn-white pull-right")
+                                        .html(dataviva.dictionary['deactivate']),
+                selector_button = $("<button></button>").attr("id", dimension.id).addClass("btn btn-block btn-outline btn-white")
+                                        .attr("onclick", "select_attr(id);")
+                                        .html(dataviva.dictionary['select']);
 
-        $('#datasets').on('change', function() {
-            $('#datasets #dataset-empty-option').remove();
+            label.html(dataviva.dictionary[dimension.id]);
 
-            $('#dimensions').empty();
-            dataviva.datasets[this.value].dimensions.forEach(function(dimension) {
-                var div = $("<div></div>").addClass("form-group"),
-                    label = $("<label></label>").attr("for", dimension.id).addClass("control-label"),
-                    deactivate_button = $("<button></button>").attr("for", dimension.id).addClass("btn btn-xs btn-white pull-right")
-                                            .html(dataviva.dictionary['deactivate']),
-                    selector_button = $("<button></button>").attr("id", dimension.id).addClass("btn btn-block btn-outline btn-white")
-                                            .attr("onclick", "start_selector('"+dimension.name+"', dimensionSelectCallback);")
-                                            .html(dataviva.dictionary['select']);
-
-                label.html(dataviva.dictionary[dimension.id]);
-
-                $('#dimensions').append(div.append(label).append(deactivate_button).append(selector_button));
-            });
+            $('#dimensions').append(div.append(label).append(deactivate_button).append(selector_button));
         });
     });
 });
