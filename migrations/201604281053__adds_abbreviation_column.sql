74f3113c8294cfec81fd2990442a0b4f47f8cf20
Diogo Lundberg <diogo@lundberg.com.br>
2016-04-28 16:02:45 -0300

    Migration adding abbreviation to attrs_bra, closes #895

--- migrations/201604281053__adds_abbreviation_column.sql
+++ migrations/201604281053__adds_abbreviation_column.sql
@@ -1,2 +1,8 @@
-ALTER TABLE `attrs_bra` 
+ALTER TABLE `attrs_bra`
 ADD COLUMN `abbreviation` VARCHAR(2) NULL AFTER `color`;
+
+UPDATE attrs_bra
+SET abbreviation = (SELECT attrs_abbreviation.abbreviation
+                    FROM (select upper(right(left(attrs_bra.id, 3), 2)) as abbreviation, attrs_bra.id
+                          from attrs_bra where length(attrs_bra.id)>3) attrs_abbreviation
+                    WHERE attrs_bra.id = attrs_abbreviation.id);
