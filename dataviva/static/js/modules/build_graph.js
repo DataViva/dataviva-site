590693d298c120869cd71e0ac8d12f9d3613d422
FredericoVieira <lauradebella@gmail.com>
2016-04-29 15:07:52 -0300

    Creates triggers to update viewParameters on change.

--- dataviva/static/js/modules/build_graph.js
+++ dataviva/static/js/modules/build_graph.js
@@ -1,6 +1,7 @@
 var selectorGraphs = Selector()
     .callback(function(d){
         $('#'+selectorGraphs.type()).html(d.name);
+        $('#dimensions input[name='+selectorGraphs.type()+']').val(d.id).trigger('change');
         $('#modal-selector').modal('hide');
     });
 
@@ -11,7 +12,19 @@ function select_dimension_graph(id) {
 
 function deactivate_dimension_graph(id) {
   $(id).siblings("button").html('Select');
+  $(id).siblings("input").val("").trigger('change');
+
 }
+
+var viewParameters = {};
+
+var update_views = function() {
+  viewParameters = {};
+  $('#dimensions input').each(function(){
+    viewParameters[this.name] = this.value;
+  })
+}
+
 
 dataviva.requireAttrs(['datasets'], function() {
 
@@ -29,17 +42,26 @@ dataviva.requireAttrs(['datasets'], function() {
                 deactivate_button = $("<button></button>").attr("for", dimension.id).addClass("btn btn-xs btn-white pull-right")
                                         .html(dataviva.dictionary['deactivate'])
                                         .attr("onclick", "deactivate_dimension_graph(this)"),
-                selector_button = $("<button></button>").attr("id", dimension.id).addClass("btn btn-block btn-outline btn-white")
+                selector_button = $("<button></button>").attr("id", dimension.id).addClass("btn btn-block btn-outline btn-primary")
                                         .attr("onclick", "select_dimension_graph(id);")
                                         .html(dataviva.dictionary['select']);
 
+                filter = $("<input></input>").attr("type", "hidden").attr("name", dimension.id);
+
             label.html(dataviva.dictionary[dimension.id]);
 
-            $('#dimensions').append(div.append(label).append(deactivate_button).append(selector_button));
+            $('#dimensions').append(div.append(label).append(deactivate_button).append(selector_button).append(filter));
+        
+            filter.change(function() {
+              update_views();
+            });
+
         });
     });
 
-    /*$('#dimensions').on('change', function() {
-        console.log('mudou');
-    });*/
+
+
 });
+
+
+
