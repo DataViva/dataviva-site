5d15f43a4c85a0edbbfd24aa20d34adf9c7ea18b
r13 <gilmardealcantara@gmail.com>
2016-04-29 18:44:04 -0300

    Update  error parameters error.

--- scripts/data_download/gera_rais.sh
+++ scripts/data_download/gera_rais.sh
@@ -1,8 +1,17 @@
-# ./gera_rais.sh en/pt output_path 
+# ./gera_rais.sh en/pt output_path select(1,2,3,4)
+# ./scripts/data_download/gera_rais.sh en ~/files_rais/en/ &> out scripts/data_download/out_rais_years_4.txt&
+# gerando 4
 
-for year in  {2002..2013}; 
+if [[ $@ != 2 ]]; then
+    echo "Error, use:"
+    echo "./scripts/data_download/gera_rais.sh en/pt output_path 1/2/3/4 &> out scripts/data_download/out_rais_years.txt&"
+    echo -e " 1 = rais-2003-microregions-classes-families\n 2 = rais-2013-municipalities-classes\n 3 = rais-2013-municipalities-classes-main_groups\n 4 = rais-2013-municipalities-classes-families\n"
+    exit
+fi
+
+for year in  {2013..2002}; 
 	do echo $1 $2 $year;
-	# python scripts/data_download/rais.py $1 $2 $year 4
+	#python scripts/data_download/rais.py $1 $2 $year $3
 	sleep 5
 done
 	
