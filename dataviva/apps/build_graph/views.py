b315e262676b1aeec5be8fac18ec4dbe46fa6142
FredericoVieira <lauradebella@gmail.com>
2016-05-03 15:58:16 -0300

    Updates json for graphs.

--- dataviva/apps/build_graph/views.py
+++ dataviva/apps/build_graph/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+import re
 from flask import Blueprint, render_template, g, jsonify, request
 from dataviva.apps.general.views import get_locale
 from dataviva.apps.embed.models import Build
 from sqlalchemy import not_
+
 
 mod = Blueprint('build_graph', __name__,
                 template_folder='templates',
@@ -28,11 +30,13 @@ def add_language_code(endpoint, values):
 def index():
     return render_template('build_graph/index.html')
 
+
 def parse_filter(filter):
     if filter != 'all':
         return '<%s>' % filter
     else:
         return filter
+
 
 @mod.route('/views/<dataset>/<bra>/<filter1>/<filter2>')
 def views(dataset, bra, filter1, filter2):
@@ -46,18 +50,24 @@ def views(dataset, bra, filter1, filter2):
     if bra != 'all':
         build_query.filter(not_(Build.bra.like('all')))
 
-
-    build_list = []
+    views = {}
     for build in build_query.all():
         if bra:
             build.set_bra(bra)
 
         if filter1 != 'all':
             build.set_filter1(request.args.get('filter1'))
-            
+
         if filter2 != 'all':
             build.set_filter2(request.args.get('filter2'))
 
-        build_list.append(build.json())
+        title = re.sub(r'\s\(.*\)', r'', build.title())
+        if title not in views:
+            views[title] = {}
+        else:
+            views[title][build.app.type] = {
+                'url': build.url(),
+                'name': build.app.name()
+            }
 
-    return jsonify(builds=build_list)
+    return jsonify(views=views)
