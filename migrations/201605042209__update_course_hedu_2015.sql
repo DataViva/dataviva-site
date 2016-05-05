501d7aa1860db2f85197c2e813d690665beb5f3a
Diogo Lundberg <diogo@lundberg.com.br>
2016-05-04 22:26:40 -0300

    Update for hedu attrs 2015

---
+++ migrations/201605042209__update_course_hedu_2015.sql
@@ -0,0 +1,3 @@
+INSERT INTO attrs_course_hedu (id,name_en,desc_en,keywords_en,name_pt,desc_pt,keywords_pt,gender_pt,plural_pt,article_pt,color) VALUES
+('861P01','Fire prevention and control',NULL,NULL,'Proteção contra o fogo e combate a incêndios',NULL,NULL,NULL,1,0,'#a4bd99'),
+('525E07','Marine engineering',NULL,NULL,'Engenharia marítima',NULL,NULL,NULL,1,0,'#17bcef');
