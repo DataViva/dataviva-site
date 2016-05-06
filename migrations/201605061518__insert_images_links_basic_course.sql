d0b9a7eec6d2cd433c102ff3646e10be0a18cc56
FredericoVieira <lauradebella@gmail.com>
2016-05-06 15:44:19 -0300

    Updates upper case in basic course.

--- migrations/201605061518__insert_images_links_basic_course.sql
+++ migrations/201605061518__insert_images_links_basic_course.sql
@@ -1,71 +1,71 @@
 -- insert category img
 
-UPDATE attrs_course_sc set
+UPDATE attrs_course_sc SET
 img_link = "https://www.flickr.com/photos/igor_pires/2965357433/in/photolist-5w3esi-fzir1N-fz3CGx-fz3Z2g-fz2YBz-aro24U-fzhpgo-fzimLm-fzhLHW-KD11D-fzhvN5-feEXNd-ryr9zQ-nW8jN-feEWTh-feqGE6-feqJXx-feF1eU-pJisoH-feEVL5-nLERFj-feEV3W-6AuWM6-8DDBYd-am1tKS-dZ4HSA-4SarfC-7i67km-gRTGBu-8PYH6D-q3WDvL-kkyYMn-8W3zE2-dhf63h-qQD1vm-nYmaQy-oSqDMi-aLfMPR-pmgwo9-ajtDqB-s6wqt9-dE3HzH-puR8nu-gRTDaw-bBHn3z-gRTGiL-gRTHcQ-g5NZAF-bLB1pn-ohU52s",
 img_author = "Igor Pire"
 WHERE id = 1;   
 
-UPDATE attrs_course_sc set
+UPDATE attrs_course_sc SET
 img_link = "https://www.flickr.com/photos/stjnoticias/15330260636/in/photolist-pmFAoy-2vLxU-5Vo41F-7xRodc-9x3o4b-p7eFh8-8C1r4e-dx5hxG-dx5hW3-dx5fzw-5Hp6o8-fcZ1wi-porgjn-ct7frm-8Zy8yB-fdeky3-dx5hzU-5VsCGb-f9b5EZ-poHBLM-eSVfZ1-7xRoLP-p7dsCV-dwYLxZ-9qPBbd-p7drM6-2HUg5L-2HPXHk-4q7pWY-eBEHW1-53JMZR-pQ9eoj-dwYNYD-p7eFtR-4JwxZN-7hXW8H-fuWykH-p7eFwr-6BAqjx-5VsxMw-9X9FcC-5L3igA-poHBmZ-porgxD-porfRi-fcZ1DP-5VnYkF-6MEEKH-5VoJnk-dx5fBN",
 img_author = "Superior Tribunal de Justiça ST"
 WHERE id = 2;   
 
-UPDATE attrs_course_sc set
+UPDATE attrs_course_sc SET
 img_link = "https://www.flickr.com/photos/basf/8725202259/in/photolist-ei1WDH-5wqoKU-9NJAiA-9ey41N-9ey3q5-riNKP5-838qAL-9LPBnH-83Bk1o-D5Y7xr-83Bmco-8S5wF9-83ygvv-frVaYZ-83Bm1U-5Lwqub-838r9S-o989jf-nrRe3G-r5AMzL-c7LQ11-nyaDSK-egQX9f-5Lsbor-qpSPjM-9eya9w-bBj4mH-83Bb6Q-fyMuZR-6NCbf1-8FHza3-83BmXW-ZU7V-8Pd1tE-823Zt5-83Bfqj-835iCB-5Lwq79-83Bfuh-83Bdbo-4UToXS-bRMDM6-aogREn-83Bk77-hN1djZ-83yhke-838qDL-eeueTa-83y796-83BbiJ",
 img_author = "BASF - We create chemistr"
 WHERE id = 3;   
 
-UPDATE attrs_course_sc set
+UPDATE attrs_course_sc SET
 img_link = "https://www.flickr.com/photos/66762419@N02/6077749623/in/photolist-ag54dB-emaoCb-9z99Qs-cMETtj-5zKgqJ-41mm5L-oHHD93-ooPGoL-9VWZ29-b5sBhP-dm7JQ2-s1YUJV-7YEryP-dvoxxe-oHGRTe-cSqZbY-7b8h3c-9LVCAY-em4BD6-db1eTW-spqdyu-41kXUf-45q9nx-h9yETf-41mJxU-8dVppc-aGsHkH-aCkZUM-8XZjuD-sj7Si8-n7yub4-cmJX6h-r9UXe4-41mVqE-oDTg1E-3HkFHK-oYbrfd-5AEmnq-6YcyFW-qwraCK-boByyJ-41hxh4-41mQmq-8x8LF9-86f4uk-7Vyaog-a5aLBx-5Lpk3z-ouPQNb-9Ycs5x",
 img_author = "Ana Andreoll"
 WHERE id = 4;   
 
-UPDATE attrs_course_sc set
+UPDATE attrs_course_sc SET
 img_link = "https://www.flickr.com/photos/58863234@N07/5859994932/in/photolist-9VQ1ko-avriwR-r4mg1G-9r7JhW-fNt9Zv-64cLh1-74dNrk-7FbnDp-mJUVec-nsvGLA-RZqzx-74hH4u-9Fk51x-2cvJ42-e7hiCZ-r4mivb-rkN4qn-r4mhpU-qoUS3b-aCxq87-qp8bJ4-r4n9i3-aR2HUt-rkTxE2-azRGob-nvm5bH-r2z9HB-rkN1Ti-6KYa2t-wFcu51-6TLkLF-7sDXgF-3t5ZgG-9thWDv-9XtwzC-851Qhq-MrLsX-7v3nhv-Aa6LBD-rKp7qJ-xBLY3B-7v38qD-7v6R67-bEepeJ-7v77VY-rkQcK5-f6Yx18-q9ifKd-f7efhs-8rZqLp",
 img_author = "Heriberto Estolan"
 WHERE id = 5;   
 
-UPDATE attrs_course_sc set
+UPDATE attrs_course_sc SET
 img_link = "https://www.flickr.com/photos/esthervargasc/6252068570/in/photolist-6qwyKS-NRY91-exYNnb-aWnLRi-f51w4h-pvfyFn-cjfg9A-foZebs-f4Lhmt-exVNDF-9DL7SU-7S5FFH-4KEKBZ-5J1rg5-exVLWT-fKyzEd-ejwaW8-f4KRRR-exZbGu-t68kE-exVMRg-exYMhy-ejwaUR-exVZag-exYXK5-9DL84w-f4L2or-5wicvF-f4L5ir-awtuaG-foJW9T-f4L2T8-f4KWEH-8KkEBZ-E25Wr-CmuK5-foZkvN-f517dC-4ik7vs-dMbdBA-7GRgz8-bcc1Cv-dMbdHU-rty8HL-rsNLhH-f4ZYny-f4KYBr-nDmawE-6L5pWc-ieLFBg",
 img_author = "Esther Varga"
 WHERE id = 6;   
 
-UPDATE attrs_course_sc set
+UPDATE attrs_course_sc SET
 img_link = "https://www.flickr.com/photos/jluis9796/381756396/in/photolist-zJASQ-shcmW4-5q8YyA-rL48iw-9EL6hD-nKFmFe-rZZGfT-shfeHK-bDbAjQ-ohq9Ar-gRXvY7-7vWdJm-rWWZmP-raNHer-CEhAXB-davpqW-dCLQiQ-hBtkwP-rkfVmm-87jfSK-oTYzj9-sgi7FD-acaL2i-rjv2xB-pLE6UM-a9Lek1-rL3PKA-9yrkJc-c1pq17-se11Ks-sgixkt-o1n7Hi-osc99C-rjhNdq-nHTTne-9BAZYh-uEgxZt-a32Mtu-s3s5K7-rkrMmG-eTtp7r-pJskvG-bAJ8e-ocJA2Q-pCWzJh-eDAMhJ-qqQBRg-oGEExT-nJ9dgb-8mPhoW",
 img_author = "jluis979"
 WHERE id = 7;   
 
-UPDATE attrs_course_sc set
+UPDATE attrs_course_sc SET
 img_link = "https://www.flickr.com/photos/agecombahia/9692270699/in/photolist-fLtqpv-fLtzjT-r55Fme-fLpCoz-uxSsCm-g5TURE-fLtrpn-fTTc8h-p6rZ15-fLtmHV-fLt4Zr-8A99S9-fLKHy3-fLKZUd-g5TrWZ-fTSVP7-fLtrSe-fLKJ8b-fTU6xz-fLGgKZ-fLLd73-dEw2C9-fLtsm2-fLt8DH-fLKWP9-fLKYuN-fLt7on-8A617z-fLtzQe-7rbrtv-q3jgRm-fLtpCP-gacUSv-fLKGjU-fLtnCV-fLKJfm-92q7ER-fLKDGs-fLGaLs-fLKKhS-fLtvcD-fPCfSm-fTTv6F-fTTHJK-bkKXTi-fPkP62-fPC6dN-fLKG51-fTSv32-fLt7Zv",
 img_author = "Fotos GOVB"
 WHERE id = 8;  
 
-UPDATE attrs_course_sc set
+UPDATE attrs_course_sc SET
 img_link = "https://www.flickr.com/photos/jor200910001/3593564779/in/photolist-6txXBr-5xoJJz-q17KTB-3mgpQy-rzB4jP-9qsYjL-qhoatP-ebLJbo-9tArP1-4dcVQX-q11ub7-pkyXGJ-kiHbMD-9sYzaU-aSrgBF-81Aoxa-pkNuwT-hzeW6t-pkzao9-8vwooE-9qsYW1-pkzaq3-9TRScA-EcjtGH-7vb2o6-q17Kmz-dbFiCh-qfhgfs-qfgVM9-qhocLK-qho8FR-pkNqtH-pkz2po-pkNFwD-q11uZG-9qsZZG-qhuS7w-pPLWcH-6np3gP-6ticbw-ygZ8q-qhv3wQ-bLBMdX-8iYL3j-m98nRg-na9YBC-ru8bBb-q17SEc-naa3fE-qhokw4",
 img_author = "jor20091000"
 WHERE id = 9;  
 
-UPDATE attrs_course_sc set
+UPDATE attrs_course_sc SET
 img_link = "https://www.flickr.com/photos/hkclebicar/3484811718/in/photolist-6iWz7E-bw8fBv-g6jxLL-2Zutag-aJBrKg-6R8GS5-8zysRd-2akiSz-931Z1K-8WRmQ3-6UNEzw-4grdxy-6RgWGt-5juiZL-bhuYAc-oXZJ2B-mHwvrd-9RHot6-6gfBUw-93qub1-awKn7k-6iSpXV-8VqsfD-amzCYn-bvZk2U-6iSmo2-buBnFB-8aLQBd-935fPE-oXmkXB-7exKY5-g6j59k-6R8BsA-8zyghC-a6GqfR-6ya2P1-aZH4dB-fyzJUz-6R8C5J-6y5Uer-fckC9b-amBGVq-fckD1q-bW1ohh-7h5MH-6iSqox-bW1msj-93nomT-6R4BcH-dQXRnb",
 img_author = "Haroldo kennedy"
 WHERE id = 10;  
 
-UPDATE attrs_course_sc set
+UPDATE attrs_course_sc SET
 img_link = "https://www.flickr.com/photos/tomascecco/9219078712/in/photolist-f3EbUY-9a9ETh-9a9PXG-9a9EDw-ayorWP-fEpFcd-dh2rfi-72b8Pj-NqT8c-4ZZrvW-NquAu-d2YrvE-NqndU-NqtvG-NqKYg-NqNKn-4YzvDz-pZtu1p-NqqPy-kodHkb-qPqWVi-6t8Jbd-7Nndqn-m7ooxZ-6QXx6V-6t8J6f-4GsjX9-85vPSs-6NgtNY-pXr3wR-8mUmDH-6RhXtA-fgJKhN-q3vH82-ofwVaB-8mXuWb-LbQkn-nL5Mqm-bKNr5X-ojjUBU-59YDEn-ebMLAt-4Gsi7G-8mXufu-8mXtYA-4Gsj53-d6LWd-86zY8U-qA4JKZ-ebTqXA",
 img_author = "Tomas Cecchin"
 WHERE id = 11;  
 
-UPDATE attrs_course_sc set
+UPDATE attrs_course_sc SET
 img_link = "https://www.flickr.com/photos/gissklein/7881424580/in/photolist-d1smWG-5AhDrU-fB6cqQ-5AdiRg-bA8pcC-o7c4gi-nMVwTD-bA8pcJ-bA8uSh-5AhB8L-5Ader8-5Ad9nx-5AhwTE-b56puv-5AhpX3-kM4kwc-eg6ZJK-887eX4-5AdbW6-b8sxfe-bA8pcj-8Dv5Tm-buDAz8-b8su6Z-7F2gp8-b56aDP-6dCx3g-b56dz8-b56ji2-b55PCx-b56gR8-bA8pc3-b5612Z-7F2kH2-6sFZ7n-4rwoJL-b56mNR-5AdoaM-qk3YRr-6JKUzv-4rseR8-4rwYfG-4rsdxn-b55SYD-8DrYsX-o3qmYU-McyQx-6dGH59-4rsMdX-8nMHfx",
 img_author = "Gisleyne Klei"
 WHERE id = 12;  
 
-UPDATE attrs_course_sc set
+UPDATE attrs_course_sc SET
 img_link = "https://www.flickr.com/photos/governodorio/24637337892/in/photolist-f3qTQY-m2Naba-f3bEQg-f3bNU6-f3bHXr-f3bHCc-8jW9VN-8jSYAF-y52hpx-f3bGLc-gE3JyQ-f3qU8C-f3r45w-8jWajm-DgpTn7-Dx7NcN-CL1Vsd-CL8NhX-8k1Tqw-8jWags-w6cMSV-w6BKye-v95aiV-vNHmEg-w3Tzus-vNHkWT-vNA6QG-w6mocB-w5yGA3-8jW9TC-ubsH5P-w5d8Rm-wma6vU-vpX8x8-wngEii-wm9Mdm-wm9KBW-w5cHEf-wjv52Y-wm9FJ5-wjv2A5-vpNhYG-wm9BZ1-wjuXdY-w5cG8E-wjuCvY-vpMT1G-vpMQYf-wjuwy7-wnfUat",
 img_author = "Governo R"
 WHERE id = 13;  
 
-UPDATE attrs_course_sc set
+UPDATE attrs_course_sc SET
 img_link = "https://www.flickr.com/photos/101018242@N02/9627376651/in/photolist-fEJPEB-fF2oa3-7Z1Ea8-8nb9Xx-fJuect-8na9n8-fEJwKv-8nbHDk-dAHi8g-oYUgnR-fF23Wf-8qfpm9-qAF2mh-dANHo3-8ndJJG-fJujfK-8nem31-8nbaFH-fJLRbh-8ndRcb-qAT7zP-dAHi5z-8nbgWk-8najK6-8ne95w-8nabAt-fEJPRx-fF2eKQ-fJLM2m-8nbkJ4-6Vkx2s-8nexUh-fF274w-fEJueg-8namy8-8ndhGJ-8qfpH1-8nerDY-rxEqYF-dAHi18-8nb18K-8naC9R-nJsand-8qcgfp-8nahUn-dAHhJt-7Z4RuJ-8ne9C3-8qfpsS-8ndg7h",
 img_author = "Colégio Batista Shepar"
 WHERE id = XX;  
