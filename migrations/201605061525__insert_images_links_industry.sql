18e1b0cb56d9aa6c792d16f686e6e91905e45d01
gilmardealcantara <gilmardealcantara@gmail.com>
2016-05-06 15:37:18 -0300

    Add inserts images links to industry.

---
+++ migrations/201605061525__insert_images_links_industry.sql
@@ -0,0 +1,88 @@
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/presidenciard/15550215047/in/photolist-pG7VaZ-qkKfYC-k9M8tS-9Q39AN-fJfN4C-n1cPdk-pp3Rp8-dSvLtW-jCjQUb-9PZhFx-fJfP9s-7eFnYk-poPvpA-fHYedr-rxkWHK-k2imo8-72scTC-8ZJQVC-mcfjZV-a4aMiW-cDsR47-rzNpr2-dcMKXJ-66uQZ5-jNVypt-mh4vqV-fJfMKb-fHYcdt-pMj5Wp-qDC4gS-7eFocT-4Er11S-fHYe2c-qWcFFk-nnA53A-fHYdBB-fJfMmS-fBvuCV-nFRYwR-nnzXHG-fHYeu8-pKsups-dSqb86-k9J7RK-7eKivE-9PZi1t-avfd5t-7eFo7V-pp49Qa-ejDwVZ",
+set img_author = "PresidenciaRD"
+WHERE ID = "a";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/nestorgalina/5388776024/in/photolist-9dbTns-bnG2aC-6sEVf7-nFP7Nn-bZRkQ5-hb4oGy-hbHs8Y-7scxVT-73JZPz-dQqmxX-hbJD2g-6Xvt3p-8kq1i9-4MiF6s-dk3wQG-5dYiV3-dbDjwV-rGaMRr-4VU6cV-7rwdK9-ecb5n-ecaru-fMhMct-qx5VQW-56jCYb-7nTyro-4en7EU-dqtcWQ-4JRUYw-6SP8Pc-7Hgc3s-7HBCBv-6SP7fi-dz3s5S-9c1aRv-5UfrRG-7LVZV9-7JBKjH-67gRJa-6STaYU-dTjRf1-6STbhA-6STcfb-6STbcs-7HU4EN-eiwRaH-54uzsL-6e8xbj-6STbFs-6SP82n",
+set img_author = "Nestor Galina"
+WHERE ID = "b";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/greyworld/6159264209/in/photolist-aogQEv-8MCUGP-oQG4du-p1FrxH-iyitqJ-oDg6q-qpK3rC-qE1m5L-pW7pkj-gwmN7H-qJtPb2-gwmAC2-Dc4Xd-83yh86-iuP6MX-qpRzQg-83Bnqj-5Lwr9N-5BYXKo-fBN3Es-bopadf-8WmWdE-8udRCd-5LsbeX-83y6mR-rm6JbP-9NH9LR-6FjVBT-9ey3MS-nYfqpN-9ey3tY-ei1WDH-5wqoKU-9NJAiA-9ey41N-9ey3q5-riNKP5-838qAL-9LPBnH-83Bk1o-D5Y7xr-83Bmco-8S5wF9-83ygvv-frVaYZ-83Bm1U-5Lwqub-838r9S-o989jf-nrRe3G",
+set img_author = "Grey World"
+WHERE ID = "c";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/langleyo/5953560788/in/photolist-a56ycN-nHFyRo-FtdvMZ-849Vom-5JsMuP-6sPaVX-d98NLY-Ex8M5D-fYNj6x-57njL4-crpVem-EBeTcq-AQtXRF-dJLYoy-9b32GM-eUK9SM-qiVjR2-6Z6XwX-bkRgK-qvccdR-9rfwDg-qB6J36-hyaHM3-5Axiq4-cPmG2u-tyhq-jpVsHV-7L8aqL-9EXAJg-tyif-iJYKMV-bEaSK-i4UG9e-q1pBgc-nZdhe-dww6Yf-7ZLSHQ-n4MheS-dtm6Vc-pg2n3y-2tkMyd-da4ykU-5bSYyi-5dj7eg-fsJ8F1-7L8b1C-pb16rK-666VQc-527sbL-aBdGGo",
+set img_author = "langleyo"
+WHERE ID = "d";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/pacifico/20924036582/in/photolist-xSZa6q-xRwxp7-xBeLSE-xSZmH3-nqSj4f-np6YAA-fANscp-nqCSoe-dzMmLy-nr9HGD-6RRvhg-8ugDab-nozpBV-np6EXu-8udu3X-9kQtML-azGQpN-e4K2Sk-e4K5Rr-e4QEEu-e4JLjn-3NctJ9-nozMxE-nqSd6c-8ugBJb-8ugAhq-8vQLAw-8udt5Z-qRsFPx-8udxfM-6t1YxG-nqDjdk-6M6UbQ-aDCzJU-e4JxaB-6M6U4W-6t1Yb7-6pXwSQ-e4Qjab-e4JvRX-qnVzTB-e4QBqG-e4QAPo-dZFDCw-6sVdHx-e4QDS7-e4K55r-6kC62v-cju4pq-aZhnHT",
+set img_author = "Etienne Girardet"
+WHERE ID = "e";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/dirceugarcia/3424537552/in/photolist-6dBDGU-e2EjJx-6dxvQt-6zQjqT-raCCVQ-6zRA1i-bWN7HB-e2EjLk-ceUfzA-c483pC-c483QL-mNRMUM-485X9x-ceasMj-6zRyQD-fr2bjv-cf9kQo-ceasvN-bWN6kR-fftZ9A-o66ui8-ck71wY-ceatVY-bWN6gT-ci7EDd-kCCBEF-dTBHus-6zVJVN-cjf2qu-ck6ZgY-bWN6vc-bxdhUm-kB1kQK-cmtpiw-cf9nfJ-e635ui-fmMrEW-ceUbnq-ceUdr9-c484bC-e2EnPF-6zQh7v-ceatK9-iBYmjd-e68Hw5-9YBKPJ-6zRKc6-e68GWq-pa8Gvm-dYnjSn",
+set img_author = "Dirceu Garcia"
+WHERE ID = "f";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/61721802@N05/5702976032/",
+set img_author = "j f "
+WHERE ID = "g";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/amiguel01/15937029797/in/photolist-qhirHP-8oPd9K-ddpzba-mjCmNi-6cx5DD-eFMyuC-hjXyVt-dZBcMh-6u68ov-sGQJfo-dM9bWX-mjD83Z-qoUsJk-6wKqKB-6CgD1Y-6qSPMs-qfysc3-ogJ4gN-dcRCv5-79LcLG-pHdBsz-rcvnpk-6DonaR-7P6ADe-k9VX2x-9BCyha-pGYUVV-7mZjDS-dhm7B2-6r9Rrh-oYRpYt-64B924-oNb2ym-97gZwQ-7vKZR9-nZSEBd-oEyWzY-eVMbNr-6uak47-dgHUge-qgthjB-eZBN2s-ehJnFU-k47BRv-cRVfqm-ddpzPW-7mVv3i-85tVRp-e1YZ6f-9huNsL",
+set img_author = "Pedro Soares"
+WHERE ID = "h";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/deniwlp84/8904046052/in/photolist-a7fvne-7As1CG-a7fBW2-eyQ6qj-a7if55-eyPJem-eyQ75o-eyPyFo-a7hnMG-4XdiYd-a7hBKo-a7jkYw-eyQ7Eh-eyM4Np-eyM3SF-a7f7aV-eyPMd7-eyPYXA-eyMaCM-a7br3D-a7bBtz-65avfq-8Vi5oA-kJSejr-kyzwq3-a7dMXp-kJSe8e-ddwdyH-a7ekqT-a7e9qh-a7eeuh-kAAEki-rr1GEQ-fGag55-qujHER-efW7Mg-rbvaZF-a6ZTbo-a6YiUx-a6Xwk8-a7efoU-4he1t5-a7f4eE-9YiP18-eyMcc4-iYKtHa-kJSe7T-eJcief-a7bYNB-bJRPur",
+set img_author = "Deni Williams"
+WHERE ID = "i";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/smemon/6972691660/",
+set img_author = "Sean MacEntee"
+WHERE ID = "j";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/zetotal/3840452595/in/photolist-6RnjKV-9htwiK-hZXwb6-fLvs1e-c9ZgvN-fLN1Fq-cQEsSh-dpxLrM-99NVui-fLvspH-fBE11V-cPAA4A-cQEtfS-8Hw7ss-mYqCDT-bQefBp-62gfuY-abzwFs-5y9zvJ-9hwCSQ-7L1iWh-bQeawi-b19BBz-8Lc3pX-4D34uw-4kpr7d-b5JN1p-4qSLjw-zgWwL-7zsN2P-c8A5KC-hZXKuS-9dsDnd-e2JW5P-nGqCfv-NS3TF-96nnjm-4UnETC-haUbiz-cPAA9f-5K8D2P-ibhd69-hEfXfG-ibgTTZ-ibgRrp-9c37sq-p9D29R-8wH5zZ-8UEZZ4-99S4RY",
+set img_author = "Rodrigo Amorim"
+WHERE ID = "k";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/raissaiara/6005184161/in/photolist-a9E92X-dCYb6w-cUNsa1-2poS9M-cdbVNd-6rF4sr-pLDgS3-bVPCVv-56Q4nY-agBgzr-5RsZZQ-7Vgboe-fM77Lc-5gM5gD-qjqjMT-nvzaop-C5RzuR-9jkuye-Eqcoii-bsLaTQ-4HeRa5-6bRsKj-nhUu7E-62vPrG-5oTPWn-8r88fp-9sTMhS-BUDMp4-32VXBL-bHPtNM-qFneZ1-6rF4gR-nT5anx-5jALL5-dsfPo9-7VhGSL-62Lcr8-4aXhga-bHcRTa-C7qwZ2-Bw2vG3-ENr7mw-aMdc1z-eVjVmS-arBWY2-aCLaEC-5iJ9md-pSQNA6-3BXpyU-E1iFSE",
+set img_author = "Raíssa Iara"
+WHERE ID = "l";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/rednuht/479370088/",
+set img_author = "Rafael Matsunaga"
+WHERE ID = "m";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/copsadmirer/4714514068/in/photolist-8bB7Mj-xZ7Zk-76GCqm-LWkc-Duvpb-DuvFw-FswSD-KLEB-5aPMB-9pw3iU-aS43Wc-3tnFY2-LurF2-3yU2ri-Duw8K-91vqWo-aoZAZ9-DuvUc-2mwfPW-aas7NH-5rVpZa-237DRT-7ZTvSL-pT77nk-9ucFSz-qGfuAL-dbi3cp-pkJM1E-aoWPuZ-8eY4vW-5KKGVe-pUmNxG-bzDknR-9NdY7E-7KBfAU-4PsK3j-DuvDh-Duw2y-dZUJyw-5Sab7c-2QJj5c-6ASvHj-bwHaaw-DuvJr-czxh9-efBK5q-qPTbEr-c2tb3S-9aKtkK-atnWG4",
+set img_author = "copsadmirer@yahoo.es"
+WHERE ID = "n";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/zoomzoom/3129422274/",
+set img_author = "Gabriel White"
+WHERE ID = "o";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/cybrarian77/6284181389/in/photolist-azj5bZ-3dexLL-4sPiSE-sbPj7-8iZij1-6QSEvx-8zHNZp-4UKfGB-9Ss88A-8waACn-6tSMxh-6QSFZX-6NEghd-6QWJb7-6QWKtC-5UjH7u-6QSEmX-7nY8xW-NRiU7-8hPctL-5svxbg-bR1g1Z-fA8g3f-4P1QiN-6tSK25-2pMK5-EjAsV-6tNDbi-aA44QU-7yUH1t-po2AnK-fJbwY3-6QSfdz-daDyRK-7JhmBs-6QWKpC-6khztF-pbQjSz-nt9qd6-fhSAp8-wkQB3J-8iTccZ-97R1eb-rcmjHn-aKHACt-juWET4-9tGKT1-4AmxQ-r6wBm-8aEB1K",
+set img_author = "cybrarian77"
+WHERE ID = "p";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/adrenailha/2533302491/in/photolist-efz7A-5BYHvs-dPYecs-4RW2nb-5BYFch-4RW2p9-4RRR82-5BUmZ2-4RRQza-5BUnDT-4RW2aQ-4RWknN-5BUpJx-4RW2AY-4RRRoK-55xwiH-5BUnBp-5BYGM9-9ujTSW-4RW22E-4RW2GC-4RW1Rb-5BYH3U-4RRQuP-4RW2r5-4RWkfu-4RW267-4RRR3x-5BYFrQ-5BUnTZ-axLQF3-5BUo7e-5BUpsz-4RSadT-4RRRmc-5BYGku-5BYEJs-5BUp38-4RRX5X-5BYExC-5BYESo-5BUpbZ-5BYFXJ-5BYHmu-5BYFJo-4RRRAx-4RRRye-9ujTT9-5BYGbh-zsiYYy",
+set img_author = "Adrenailha Florianópolis"
+WHERE ID = "q";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/prefeituradeolinda/8495544450/in/photolist-dWHToj-oWBiwt-7DWYHF-r5AwNv-qq4t7X-r5yx2F-rmSKiA-8mmCeQ-qq3NzN-rmTYgY-8mmFU7-r5tN2k-mam94B-rmGb1H-7FRa7b-bkWg9E-9pab6w-8miSSR-rmWUCE-7SKUq6-7SmwSS-rmZn9e-r5n3iv-7E1AFh-oWB5tK-fCGbL3-kN2dM8-kHkjtR-kXTk2D-fCpZta-vfHWqj-oCDf9c-d5EWQS-6Vv3pL-d5LeLj-8VjExP-r5nKQY-kEr1DQ-majfkY-4oDTVi-rmUWmJ-4xPKr7-r5rThU-dVrD1b-qq5u5B-8mmAWu-r5kKwL-r5yPdD-qqbTzv-9pa8tf",
+set img_author = "Prefeitura de Olinda"
+WHERE ID = "r";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/jeremybrooks/4164721895/in/photolist-7m2hHV-aajSs-6axXyQ-5oopxt-Q2ut-jsXpJ-j4HdK-jsXzN-j4PXG-j4Pnc-j4Ku9-j4xWN-66Wko7-j4Gjg-j4GDj-MqCWQ-j4yFX-j4MDA-j4Pyw-jsYmZ-j4EeS-j4Rb8-j4Ev8-j4L77-hHZ5eu-RawF-j4EKr-j4F1f-j4NLH-j4Dth-j4CW2-j4GZg-j4KTF-j4zDg-j4DZk-j4KGw-j4FeX-4QBATu-j4BWF-j4BiB-j4Nsk-jsYzU-j4G4Y-j4Fuj-j4Cf5-j4PLE-jsZwC-j4RQP-j4fWv-Rax1",
+set img_author = "Jeremy Brooks"
+WHERE ID = "s";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/126594556@N06/16149516810/in/photolist-qB5uH1-aixUHV-a55yCA-DDpfeQ-AvKrg-pK5CMs-dPgXTn-dPgSdX-dAa1Ah-n181rp-cgYpU-dPgXC2-8S7Byq-a52J7T-cgYpZ-Awc2L-4hJHy6-94PC1H-9yAsrg-caSWnC-cicwG-4oVvft-9eEw33-aixUrM-8S7u9N-rjfhax-6niiyg-4oZC3u-r4G487-qMkVqe-2cWXNR-4oZmmQ-aiAH6q-cyPNN1-4oVQHx-JSHDP-4hJQ8F-pQZ3RF-cgYpV-j6bJwA-6jtd6p-oSqDMi-dKbj6w-9Hb1U1-9pF4nJ-4hJSiD-pNAMLo-4oZRF1-7Yztiw-iYUzGi",
+set img_author = "Ana Clarissa Moura"
+WHERE ID = "t";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/iamjudyz/10174626096/in/photolist-gv6BRS-6pRcBe-a6mZTc-qsc1zK-apoEN5-p91TJs-anzbuX-8jEup7-oEMAxX-m8qdEn-FV3ZJd-FpGAfs-s5A6q7-6sGPXm-4NNb6z-fWaMz9-eWxZR8-dM6qyu-FpGZrC-bj47qa-5RuGqW-cf7fQC-cercBE-kzGTQM-4zU6te-qgRKa1-qprwEy-rpM2rd-6pVkCb-e59oWo-6pVmiL-4Q77UP-6pVkxq-7EwT9w-qYYu1R-ndMYfU-9j22Qg-7Ht52c-8LGmWr-bD76FR-foSP1S-nADwng-p4XSK5-bmbHvt-663CdG-gkVtP1-mjEJ76-a2P6wu-9j23Lt-6oj5An",
+set img_author = "Judy Zhang"
+WHERE ID = "u";
+
+UPDATE attrs_cnae set img_link = "https://www.flickr.com/photos/76050642@N02/11239843786/",
+set img_author = "Rodrigo W. Blum"
+WHERE ID = "x";
+
