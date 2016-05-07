6fc2c68f86aadf3e2bcef0ded970fd07217eefa9
Diogo Lundberg <diogo@lundberg.com.br>
2016-05-07 10:28:45 -0300

    Adds build a graph controller better structure

--- dataviva/static/js/modules/build_graph.js
+++ dataviva/static/js/modules/build_graph.js
@@ -10,117 +10,134 @@ function select_dimension_graph(id) {
   $('#modal-selector').modal('show');
 }
 
-function deactivate_dimension_graph(id) {
-  $(id).siblings("button").html('Select');
-  $(id).siblings("input").val("all").trigger('change');
-}
+var buildGraph = (function () {
 
-
-$('.dropdown-select').on( 'click', '.dropdown-menu li a', function() { 
-   var target = $(this).html();
-
-   //Adds active class to selected item
-   $(this).parents('.dropdown-menu').find('li').removeClass('active');
-   $(this).parent('li').addClass('active');
-
-   //Displays selected text on dropdown-toggle button
-   $(this).parents('.dropdown-select').find('.dropdown-toggle').html(target);
-});
-
-
-dataviva.requireAttrs(['datasets'], function() {
-
-    for (dataset in dataviva.datasets) {
-        $('#datasets').append( $('<option value="'+dataset+'">'+dataviva.dictionary[dataset]+'</option>'));
+    return {
+        dataset: dataset,
+        views: views,
+        init: init
     }
 
-    $('#datasets').on('change', function() {
-        $('#datasets #dataset-empty-option').remove();
-        var dataset = this.value;
+    var views, dataset;
 
+    function buildDimensions(dataset) {
         $('#dimensions').empty();
         dataviva.datasets[dataset].dimensions.forEach(function(dimension, index) {
-                var div = $("<div></div>").addClass("form-group"),
-                label = $("<label></label>").attr("for", dimension.id).addClass("control-label"),
-                deactivate_button = $("<button></button>").attr("for", dimension.id).addClass("btn btn-xs btn-link pull-right")
-                                        .html(dataviva.dictionary['deactivate'])
-                                        .attr("onclick", "deactivate_dimension_graph(this)"),
-                selector_button = $("<button></button>").attr("id", dimension.id).addClass("btn btn-block btn-outline btn-primary")
-                                        .attr("onclick", "select_dimension_graph(id);")
-                                        .html(dataviva.dictionary['select']);
+            var div = $('<div></div>').addClass('form-group'),
+            label = $('<label></label>').attr('for', dimension.id).addClass('control-label'),
+            deactivate_button = $('<button></button>').attr('for', dimension.id).addClass('btn btn-xs btn-link pull-right')
+                                    .html(dataviva.dictionary['deactivate'])
+                                    .attr('onclick', 'deactivate_dimension_graph(this)'),
+            selector_button = $('<button></button>').attr('id', dimension.id).addClass('btn btn-block btn-outline btn-primary')
+                                    .html(dataviva.dictionary['select'])
+                                    .attr('onclick', 'select_dimension_graph(id);');
 
-                filter = $("<input></input>").attr("type", "hidden").attr("name", dimension.id).attr("id", 'filter'+index).val('all');
+            filter = $('<input></input>').attr('type', 'hidden').attr('name', dimension.id).attr('id', 'filter'+index).val('all');
 
-                label.html(dataviva.dictionary[dimension.id]);
-            
-                filter.change(function() {
-                      $.ajax({
-                            method: "GET",
-                            url: "/" + lang + "/build_graph/views/" + dataset +"/" +
-                                $('#dimensions #filter0').val() + "/" +
-                                ($('#dimensions #filter1').val() == 'all' ? 'all' : $('#dimensions #filter1')[0].name) + "/" + 
-                                ($('#dimensions #filter2').val() == 'all' ? 'all' : $('#dimensions #filter2')[0].name),
-                            data: {
-                                    filter1: $('#dimensions #filter1').val(),
-                                    filter2: $('#dimensions #filter2').val() 
-                                },
-                            success: function (result) {
-                                //('#titles').empty()
+            label.html(dataviva.dictionary[dimension.id]);
 
-                                window.buildGraphViews = result.views;
+            if (dimension.name == 'School') {
+                div.append(filter);
+            } else {
+                div.append(filter).append(label).append(deactivate_button).append(selector_button);
+            }
 
-                                if(result.views){
-                                    
-                                    $('#views').empty()
-
-                                    var div = $("<div></div>").addClass("form-group");
-                                        select = $("<select></select>").addClass("form-control"),
-                                        label = $("<label></label>").attr("for", "titles").addClass("control-label"),
-                                        
-                                    label.html(dataviva.dictionary["views"]);
-
-                                    for(view in result.views){
-                                        option = $('<option value="'+view+'">'+view+'</option>')
-                                        select.append(option);
-                                    }
-
-                                    select.on('change', function() {
-                                        $('#graphs').empty()
-
-                                        for (graph in buildGraphViews[this.value]) {
-                                            var name = buildGraphViews[this.value][graph].name,
-                                                url = buildGraphViews[this.value][graph].url
-                                            graphButton = $('<a></a>').addClass('btn btn-primary')
-                                                            .attr('href', "/" + dataviva.language + '/embed/' + url)
-                                                            .attr('target', "graphs-frame-build-graphs")
-                                                            .html(name);
-                                            $('#graphs').append(graphButton);
-                                        }
-                                    });
-
-                                    div.append(label).append(select);
-
-                                    $('#views').append(div);
-                            }
-                        }
-                    });
-
-                });
-
-                if (dimension.name == 'School') { 
-                    div.append(filter)
-                } else {
-                    div.append(filter).append(label).append(deactivate_button).append(selector_button)
-                }
-
-                $('#dimensions').append(div);
+            $('#dimensions').append(div);
         });
+    }
+
+    function buildViews(views) {
+        $('#views').empty()
+
+        var div = $('<div></div>').addClass('form-group');
+            select = $('<select></select>').addClass('form-control'),
+            label = $('<label></label>').attr('for', 'titles').addClass('control-label'),
+
+        label.html(dataviva.dictionary['views']);
+
+        for(view in views){
+            option = $('<option value="'+view+'">'+view+'</option>')
+            select.append(option);
+        }
+
+        div.append(label).append(select);
+        $('#views').append(div);
+    }
+
+    function buildGraphs(graphs) {
+        $('#graphs').empty()
+
+        var div = $('<div></div>').attr('class', 'dropdown dropdown-select');
+            graphButton = $('<button data-toggle="dropdown" aria-expanded="true"></button>')
+                            .attr('id', 'graph-button')
+                            .attr('class', 'btn btn-outline btn-block dropdown-toggle'),
+            dropDownMenu = $('<ul role="menu" aria-labelledby="graph-button"></ul>')
+                            .attr('class', 'dropdown-menu'),
+            label = $('<label></label>').attr('for', 'graph-button').addClass('control-label');
+
+        label.html(dataviva.dictionary['graphs']);
+
+        for(graph in graphs){
+            option = $('<option value="'+graph+'">'+graph+'</option>')
+            select.append(option);
+        }
+
+        div.on( 'click', '.dropdown-menu li a', function() {
+           var target = $(this).html();
+
+           //Adds active class to selected item
+           $(this).parents('.dropdown-menu').find('li').removeClass('active');
+           $(this).parent('li').addClass('active');
+
+           //Displays selected text on dropdown-toggle button
+           $(this).parents('.dropdown-select').find('.dropdown-toggle').html(target);
+        });
+
+        div.append(label).append(select);
+        $('#graphs').append(div);
+    }
+
+    function updateViews() {
+        $.ajax({
+            method: "GET",
+            url: "/" + lang + "/build_graph/views/" + dataset +"/" +
+                $('#dimensions #filter0').val() + "/" +
+                ($('#dimensions #filter1').val() == 'all' ? 'all' : $('#dimensions #filter1')[0].name) + "/" +
+                ($('#dimensions #filter2').val() == 'all' ? 'all' : $('#dimensions #filter2')[0].name),
+            data: {
+                    filter1: $('#dimensions #filter1').val(),
+                    filter2: $('#dimensions #filter2').val()
+                },
+            success: function (result) {
+                console.log(result.views);
+            }
+        });
+    }
+
+    function changeDataSet() {
+        buildGraph.dataset = this.value;
+        buildDimensions(this.value);
+        updateViews();
+    }
+
+    function init() {
+        for (dataset in dataviva.datasets) {
+            $('#datasets').append( $('<option value="'+dataset+'">'+dataviva.dictionary[dataset]+'</option>'));
+        }
+
+        $('#datasets').on('change', changeDataSet);
+    }
+})();
+
+function deactivate_dimension_graph(id) {
+  $(id).siblings('button').html('Select');
+  $(id).siblings('input').val('all').trigger('change');
+}
+
+$(document).ready(function () {
+    dataviva.requireAttrs(['datasets'], function() {
+        buildGraph.init();
     });
 });
-
-
-
-
-
 
 
