17f33b2d68c2d562c053a5762c9f86cb6019ebb1
Diogo Lundberg <diogo@lundberg.com.br>
2016-05-08 15:19:59 -0300

    Fixed facebook sharing button

--- dataviva/static/js/base.js
+++ dataviva/static/js/base.js
@@ -365,15 +365,6 @@ $(document).ready(function () {
         });
     });
 
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.5&appId=222520191136295";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
-
     $("[name='language-selector']").val(document.documentElement.lang);
 
     $("[name='language-selector']").change(function() {
@@ -444,3 +435,4 @@ $(document).ready(function () {
         return false
     });
 });
+
