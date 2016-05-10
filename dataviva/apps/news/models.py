814c611ccecbf9a477941ce986ef533627ddc22b
lauradebella <lauradebella@gmail.com>
2016-05-10 16:09:15 -0300

    Updates news model.

--- dataviva/apps/news/models.py
+++ dataviva/apps/news/models.py
@@ -6,7 +6,7 @@ class Publication(db.Model):
     __tablename__ = 'news_publication'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(400))
-    subject = db.Column(db.String(100))
+    author = db.Column(db.String(100))
     text_call = db.Column(db.String(500))
     text_content = db.Column(db.Text(4194304))
     thumb = db.Column(db.Text(4194304))
@@ -14,11 +14,10 @@ class Publication(db.Model):
     publish_date = db.Column(db.DateTime)
     active = db.Column(db.Boolean)
     show_home = db.Column(db.Boolean)
-    authors = db.relationship('AuthorNews', backref='news_publication', lazy='eager')
+    subject = db.relationship('PublicationSubject', backref='news_publication', lazy='eager')
 
-    def authors_str(self):
-        author_names = [author.name for author in self.authors]
-        return ', '.join(author_names)
+    def subject_str(self):
+        return self.subject[0].name
 
     def date(self):
         return self.publish_date.strftime('%d/%m/%Y')
@@ -27,7 +26,7 @@ class Publication(db.Model):
         return '<Publication %r>' % (self.title)
 
 
-class AuthorNews(db.Model):
+class PublicationSubject(db.Model):
     __tablename__ = 'news_author'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
@@ -37,4 +36,4 @@ class AuthorNews(db.Model):
         self.name = name
 
     def __repr__(self):
-        return '<AuthorNews %r>' % (self.name)
+        return '<PublicationSubject %r>' % (self.name)
