658210d7be32a294acb459d6b804494e58774b29
FredericoVieira <fredops1@hotmail.com>
2016-05-23 15:37:34 -0300

    Adds generic help js file.

---
+++ dataviva/static/js/modules/help.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    dataviva.requireAttrs(['bra'], function() {});
+    dataviva.requireAttrs(['cnae'], function() {});
+    dataviva.requireAttrs(['cbo'], function() {});
+    dataviva.requireAttrs(['hs'], function() {});
+    dataviva.requireAttrs(['wld'], function() {});
+    dataviva.requireAttrs(['course_hedu'], function() {});
+    dataviva.requireAttrs(['university'], function() {});
+    dataviva.requireAttrs(['course_sc'], function() {});
+});
