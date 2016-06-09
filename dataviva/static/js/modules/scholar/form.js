fbd0008f393eb233dc219b39bef13439610226b5
Frederico Vieira <fredops1@hotmail.com>
2016-06-09 16:26:17 -0400

    Updates scholar js to handle with new and edit articles file.

--- dataviva/static/js/modules/scholar/form.js
+++ dataviva/static/js/modules/scholar/form.js
@@ -20,7 +20,7 @@ function uploadFiles(url, files) {
     xhr.upload.onprogress = function(e) {
         if (e.lengthComputable) {
             progressBar.value = (e.loaded / e.total) * 100;
-            progressBar.textContent = progressBar.value; // Fallback for unsupported browsers.
+            progressBar.textContent = progressBar.value; //Fallback for unsupported browsers.
         }
     };
     xhr.send(formData); //multipart/form-data
@@ -36,25 +36,42 @@ function validateFile(){
     }
 }
 
-$('#progress').hide();
-$('#delete').hide();
-$('#article-url').text("");
+if (window.location.pathname.split('/').pop() === 'new'){
+    $('#uploaded-file').hide();
+    $('#progress').hide();
+}
+
+if (window.location.pathname.split('/').pop() === 'edit'){
+    var path = window.location.pathname.split('/');
+    var articleId = path[path.length-2];
+    $('#input-file').hide();
+    $('#progress').hide();
+    $('#uploaded-file').empty();
+    $('#uploaded-file').append('<button href="#" class="btn btn-danger" id="delete-edit">' + 
+                                        '<i class="fa fa-trash-o m-r-sm"></i> Delete File ' +
+                                '</button>' +
+                                '<a id="article-url" href="https://dataviva-dev.s3.amazonaws.com/scholar/'+articleId+'/files/article" target="_blank"> Artigo <i class="fa fa-file-pdf-o m-r-sm"></i></a>'
+                                )
+}
 
 $(document).ready(function() {
     $('#file').get(0).addEventListener('change', function(e) {
         if ($('#file').val().split('.').pop().toLowerCase() !== 'pdf'){
+            $('#file').val('');
             showMessage('Tipo de arquivo não suportado.', 'danger', 8000);
             return false;
         }
         else if ($('#file')[0].files[0].size/1024/1024 > 50){
+                $('#file').val('');
                 showMessage('Arquivo deve possuir no máximo 50MB.', 'danger', 8000);
                 return false;
         }
         else {
             $('#progress').show();
             uploadFiles('/'+window.lang+'/scholar/admin/article/upload', this.files);
-            $('#delete').show();
-            $('#inputfile').hide();
+            $('#article-url').attr('href', '/scholar/admin/file/'+csrf_token[0].value.substring(0,10)+'/'+csrf_token[0].value.substring(12,52));
+            $('#input-file').hide();
+            $('#uploaded-file').show();
             $('#progress').hide();
         }
     }, false);
@@ -66,15 +83,21 @@ $(document).ready(function() {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
                     showMessage(xhr.responseText, 'success', 8000);
-                    $('#delete').prop('disabled', true);
-                    $('#delete').hide();
-                    $('#inputfile').show();
+                    $('#file').val('');
+                    $('#uploaded-file').hide();
+                    $('#input-file').show();
                 } else {
                     showMessage(xhr.statusText, 'danger', 8000);
-                    $('#delete').prop('disabled', false);
                 }
             }
         };
         xhr.send(csrf_token[0].value);
+    });
+
+    $('#delete-edit').on('click', function (e) {
+        $('#file').val('');
+        showMessage('File Removed!', 'success', 8000);
+        $('#uploaded-file').hide();
+        $('#input-file').show();
     });
 });
