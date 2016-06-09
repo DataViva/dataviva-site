93d518c94fc02a781a80fb2981341129967e10de
gilmardealcantara <gilmardealcantara@gmail.com>
2016-06-09 14:54:43 -0300

    Remove comment in help.js

--- dataviva/static/js/modules/help.js
+++ dataviva/static/js/modules/help.js
@@ -1,14 +1,3 @@
-/*$(document).ready(function () {
-    dataviva.requireAttrs(['bra'], function() {});
-    dataviva.requireAttrs(['cnae'], function() {});
-    dataviva.requireAttrs(['cbo'], function() {});
-    dataviva.requireAttrs(['hs'], function() {});
-    dataviva.requireAttrs(['wld'], function() {});
-    dataviva.requireAttrs(['course_hedu'], function() {});
-    dataviva.requireAttrs(['university'], function() {});
-    dataviva.requireAttrs(['course_sc'], function() {});
-});
-*/
 $('.sidebar a').on('click', function(){
     $('.sidebar a').attr('class','');
     $(this).toggleClass('active');
