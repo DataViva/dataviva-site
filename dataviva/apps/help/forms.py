8317c5f5c521003e6ba49f09ed9546047c464463
Frederico Vieira <fredops1@hotmail.com>
2016-06-15 15:53:26 -0400

    Updates help forms fields validators.

--- dataviva/apps/help/forms.py
+++ dataviva/apps/help/forms.py
@@ -7,25 +7,23 @@ from models import HelpSubject
 class RegistrationForm(Form):
 
     #TODO - Get PT-Lang categories names
-    category = SelectField('category', choices=[(category.id, category.name_en) for category in HelpSubject.query.order_by('name_en')], validators=[
+    subject = SelectField('subject', choices=[(str(subject.id), subject.name_en) for subject in HelpSubject.query.order_by('name_en')], validators=[
         validators.Required(u"Por favor, selecione a categoria.")])
 
     description_en = TextField('description_en', validators=[
         validators.Required(u"Por favor, insira a pergunta em inglês."),
-        validators.Length(max=400)
+        validators.Length(max=200)
     ])
 
     description_pt = TextField('description_pt', validators=[
         validators.Required(u"Por favor, insira a pergunta em português."),
-        validators.Length(max=400)
+        validators.Length(max=200)
     ])
 
     answer_en = HiddenField('answer_en', validators=[
-        validators.Required(u"Por favor, insira a resposta em inglês."),
-        validators.Length(max=400)
+        validators.Required(u"Por favor, insira a resposta em inglês.")
     ])
 
     answer_pt = HiddenField('answer_pt', validators=[
-        validators.Required(u"Por favor, insira a resposta em português."),
-        validators.Length(max=400)
+        validators.Required(u"Por favor, insira a resposta em português.")
     ])
