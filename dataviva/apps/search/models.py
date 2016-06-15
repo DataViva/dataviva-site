197062b992b09af0a309effdb0e2b297e7ad1b6f
gilmardealcantara <gilmardealcantara@gmail.com>
2016-06-15 18:22:58 -0300

    Updadate models(add columns selectors in SearchQuestion table and remove SearchQuestionSelector table and selectors_sorted)

--- dataviva/apps/search/models.py
+++ dataviva/apps/search/models.py
@@ -4,14 +4,6 @@ from sqlalchemy import ForeignKey
 from dataviva.utils.title_case import title_case
 
 
-class SearchQuestionSelector(db.Model):
-    __tablename__ = 'search_question_selector'
-    question_id = db.Column(db.Integer, ForeignKey('search_question.id'), primary_key=True)
-    selector_id = db.Column(db.Integer, ForeignKey('search_selector.id'), primary_key=True)
-    order = db.Column(db.Integer)
-    selector = db.relationship("SearchSelector")
-
-
 class SearchProfile(db.Model):
     __tablename__ = 'search_profile'
     id = db.Column(db.String(50), primary_key=True)
@@ -47,17 +39,12 @@ class SearchQuestion(db.Model):
     description_pt = db.Column(db.String(400))
     description_en = db.Column(db.String(400))
     answer = db.Column(db.String(400))
-    selectors = db.relationship("SearchQuestionSelector")
     profile_id = db.Column(db.Integer, ForeignKey('search_profile.id'))
+    selectors = db.Column(db.String(400))
 
     def description(self):
         lang = getattr(g, "locale", "en")
         return getattr(self, "description_" + lang)
-
-    def selectors_sorted(self):
-        selectors = self.selectors
-        selectors_sorted = sorted(selectors, key=lambda selectors: selectors.order)
-        return [x.selector_id for x in selectors_sorted]
 
     def __repr__(self):
         return '<SearchQuestion %r>' % (self.description())
