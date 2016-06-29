6f2d0072bca97946c9282a5231d47da7f6bb5f53
gilmardealcantara <gilmardealcantara@gmail.com>
2016-06-29 17:40:16 -0300

    update migrations for post_sectors.

--- migrations/201606221645_create_prost_selector.sql
+++ migrations/201606221645_create_prost_selector.sql
@@ -1,7 +1,10 @@
 create table blog_post_subject(
-    id_post int not null,
-    id_subject int not null
+    post_id int UNSIGNED NOT NULL, 
+    subject_id int UNSIGNED NOT NULL,
+    CONSTRAINT pk_ps PRIMARY KEY(post_id, subject_id),
+    FOREIGN KEY (post_id) REFERENCES blog_post(id),
+    FOREIGN KEY (subject_id) REFERENCES blog_subject(id)
 );
 
-insert blog_post_subject (id_post, id_subject)
+insert blog_post_subject (post_id, subject_id)
     select id, subject_id from blog_post;
