3f7dd2badf1d8843e750fccca2bc994293cca1aa
Gilmaralcantara <gilmardealcantara@gmail.com>
2016-07-27 18:49:06 -0300

    Change dataviva.s3.amazonaws.com to https://dataviva.s3.amazonaws.com

--- config.py
+++ config.py
@@ -83,7 +83,7 @@ AWS_SECRET_KEY = get_env_variable('DATAVIVA_OAUTH_AWS_SECRET')
 
 ''' S3 Buckets '''
 S3_BUCKET = get_env_variable('S3_BUCKET', 'dataviva-dev')
-S3_HOST = get_env_variable('S3_HOST', 'dataviva.s3.amazonaws.com')
+S3_HOST = get_env_variable('S3_HOST', 'https://dataviva.s3.amazonaws.com')
 
 '''
     Mail credentials to send automatic emails to users
