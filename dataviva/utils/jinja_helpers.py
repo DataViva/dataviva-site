856e8bc1f33bf21d2261a18a004b4f6f536b85f6
rafaelsmedina <r.salesmf@gmail.com>
2016-08-09 16:00:46 -0400

    work with negative number

--- dataviva/utils/jinja_helpers.py
+++ dataviva/utils/jinja_helpers.py
@@ -66,8 +66,13 @@ def jinja_split(s, char):
     return s.split(char)
 
 def max_digits(number, digits, counter=None):
+    negative = False
+    separator = ',' if g.locale == 'pt' and counter == None else '.'
+    if number < 0:
+        negative = True
+        number = abs(number)
+
     old_number = number
-    separator = ',' if g.locale == 'pt' and counter == None else '.';
 
     if type(number) == float:
         number = Decimal(number)
@@ -75,6 +80,7 @@ def max_digits(number, digits, counter=None):
     if type(number) == Decimal:
         if number > 1000:
              number = int(number)
+
     if number >= 1000:
         str_n = [1]
         for i in range(len(str(number)), 0, -3):
@@ -95,20 +101,25 @@ def max_digits(number, digits, counter=None):
             number = round(number, 2)
         number_str = str(number)
         number_str = number_str.replace('.', separator);
-        return number_str[0:digits+1]
-    
-    number_str = str(float(number))
-    number_str = number_str.replace('.', separator);
-    if old_number < 10 or old_number >= 1000:
-        number_list = list(number_str)
-        comma, digit  =  number_list.index(separator), number_list.index(separator)-1 
-        number_list[digit], number_list[comma] = number_list[comma], number_list[digit]
-        number_str = ''.join(number_list)
-        
-    if len(number_str) > 3 and number_str[digits] == separator:
-        return number_str[0:digits]
-    else:  
-        return number_str[0:digits+1]
+        result =  number_str[0:digits+1]
+    else:
+        number_str = str(float(number))
+        number_str = number_str.replace('.', separator);
+        if old_number < 10 or old_number >= 1000:
+            number_list = list(number_str)
+            comma, digit  =  number_list.index(separator), number_list.index(separator)-1 
+            number_list[digit], number_list[comma] = number_list[comma], number_list[digit]
+            number_str = ''.join(number_list)
+
+        if len(number_str) > 3 and number_str[digits] == separator:
+            result =  number_str[0:digits]
+        else: 
+            result =  number_str[0:digits+1]
+
+    if negative:
+        return "-" + result
+    else:
+        return result
 
 def ordinal(number, gender='m'):
     if g.locale == 'en':
