568a1aa07079427315c2f73dcbc9d9a1ed6b0711
FredericoVieira <fredops1@hotmail.com>
2016-09-01 15:41:52 -0300

    Adds new contries in attrs_wld table.

---
+++ migrations/201609011407__insert_countries_attrs_wld.sql
@@ -0,0 +1,11 @@
+INSERT INTO dataviva.attrs_wld (id, id_2char, id_3char, id_num, id_mdic, name_en, name_pt, gender_pt, plural_pt, article_pt, color)
+VALUES ('nabes', 'bq', 'bes', 535, 99, 'Bonaire', 'Bonaire', 'm', 0, 0,'#0b1097')
+
+INSERT INTO dataviva.attrs_wld (id, id_2char, id_3char, id_num, id_mdic, name_en, name_pt, gender_pt, plural_pt, article_pt, color)
+VALUES ('nacuw', 'cw', 'cuw', 531 ,200, 'Curação', 'Curação', 'm', 0, 0,'#0b1097')
+
+INSERT INTO dataviva.attrs_wld (id, id_2char, id_3char, id_num, id_mdic, name_en, name_pt, gender_pt, plural_pt, article_pt, color)
+VALUES ('nablm', 'bl', 'blm', 652, 693, 'Saint Barthelemy', 'São Bartolomeu', 'm', 0, 0,'#0b1097')
+
+INSERT INTO dataviva.attrs_wld (id, id_2char, id_3char, id_num, id_mdic, name_en, name_pt, gender_pt, plural_pt, article_pt, color)
+VALUES ('namaf', 'mf', 'maf', 663, 699, 'Saint Maarten', 'Saint Maarten', 'm', 0, 0,'#0b1097')
