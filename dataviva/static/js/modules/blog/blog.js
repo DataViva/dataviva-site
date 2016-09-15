c694df14142889decb0f77a1320e2d3f1bbf9f3e
luisccf <luisccf@me.com>
2016-09-15 17:10:51 -0300

    Adds fade in and out effects when post language is changed

--- dataviva/static/js/modules/blog/blog.js
+++ dataviva/static/js/modules/blog/blog.js
@@ -1,19 +1,19 @@
 $(function() {
-	dataviva.language == 'en' && $("[name='content-en']").length ? $("[name='content-en']").show() : $("[name='content-pt']").show();
+	if (dataviva.language == 'en' && $("[name='content-en']").length)
+		$("[name='content-en']").fadeToggle()
+	else
+		$("[name='content-pt']").fadeToggle();
 
 	$('#change-language').on('click', function() {
-		if ($("[name='content-en']").length) {
-			if ($("[name='content-pt']").is(':visible')) {
-				$("[name='content-pt']").hide();
-				$("[name='content-en']").show();
-				$('#change-language').html('Ler em português');
-			} else {
-				$("[name='content-en']").hide();
-				$("[name='content-pt']").show();
-				$('#change-language').html('Read in English');
-			}
+		var fade_duration = 800;
+		if ($("[name='content-pt']").is(':visible')) {
+			$("[name='content-pt']").hide();
+			$("[name='content-en']").fadeToggle(fade_duration);
+			$('#change-language').html('Ler em português');
+		} else {
+			$("[name='content-en']").hide();
+			$("[name='content-pt']").fadeToggle(fade_duration);
+			$('#change-language').html('Read in English');
 		}
 	});
-
 });
-
