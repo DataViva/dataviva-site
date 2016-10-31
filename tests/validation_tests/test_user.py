0d93af7960020058b56c4a6f4e009bd6d8ea8013
gilmardealcantara <gilmardealcantara@gmail.com>
2016-10-31 15:32:38 -0200

    Add user test in config.

--- tests/validation_tests/test_user.py
+++ tests/validation_tests/test_user.py
@@ -1,6 +1,6 @@
 from flask import g, url_for
 from test_base import BaseTestCase
-
+from dataviva import test_user_email, test_user_password
 
 class UserTests(BaseTestCase):
 
@@ -8,8 +8,10 @@ class UserTests(BaseTestCase):
         g.locale = 'en'
 
     def test_users_can_login_and_logout(self):
-		data=dict(email='gilmardealcantara@gmail.com', password='123456')
+		data=dict(email=test_user_email, password=test_user_password)
 	  	response = self.client.post('/en/session/login', data=data)
 	  	assert '302 FOUND' == response.status
 	  	responde = self.client.get('/logout')
 	  	assert '302 FOUND' == response.status
+
+	  	
