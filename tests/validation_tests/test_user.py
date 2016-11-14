a77e1cdc9ca42d1e325c1fe515c319698403940d
Frederico <fredops1@hotmail.com>
2016-11-14 16:33:14 -0200

    Updates file identation.

--- tests/validation_tests/test_user.py
+++ tests/validation_tests/test_user.py
@@ -8,8 +8,8 @@ class UserTests(BaseTestCase):
         g.locale = 'en'
 
     def test_users_can_login_and_logout(self):
-		data=dict(email=test_user_email, password=test_user_password)
-	  	response = self.client.post('/en/session/login', data=data)
-	  	assert '302 FOUND' == response.status
-	  	response = self.client.get('/logout')
-	  	assert '302 FOUND' == response.status
+        data=dict(email=test_user_email, password=test_user_password)
+        response = self.client.post('/en/session/login', data=data)
+        assert '302 FOUND' == response.status
+        response = self.client.get('/logout')
+        assert '302 FOUND' == response.status
