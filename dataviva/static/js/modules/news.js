ae7407398adf8a276318338fe896ddca4010efc0
luisccf <luisccf@me.com>
2017-01-09 17:47:19 -0200

    Removes search parameter from url if no input is provided

--- dataviva/static/js/modules/news.js
+++ dataviva/static/js/modules/news.js
@@ -1,6 +1,9 @@
 $(function() {
     $('#search-form').submit(function() {
-        window.location.href = '/' + dataviva.language + '/news/?search=' + $(this).find('input').val().replace(' ', '+');
+        if ($(this).find('input').val())
+            window.location.href = '/' + dataviva.language + '/news/?search=' + $(this).find('input').val().replace(' ', '+');
+        else
+            window.location.href = '/' + dataviva.language + '/news';
         return false;
     });
 });
