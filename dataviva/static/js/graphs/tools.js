c9d8181a6a5e2cc5f86cdf1d7044457aa3e83b69
rafaelsmedina <r.salesmf@gmail.com>
2017-02-10 18:12:34 -0200

    Adds svg download

--- dataviva/static/js/graphs/tools.js
+++ dataviva/static/js/graphs/tools.js
@@ -1,4 +1,65 @@
 var toolsBuilder = function(viz, data, title, ui) {
+
+    function expireCookie( cName ) {
+        document.cookie =
+        encodeURIComponent( cName ) +
+        "=deleted; expires=" +
+        new Date( 0 ).toUTCString();
+      }
+
+    var initializeDownload = function(){
+        var svg = d3.select("#tree_map > svg")
+        // Add necessary name space junk and get raw node
+
+        svg.attr("version", 1.1)
+            .attr("xmlns", "http://www.w3.org/2000/svg")
+            .node()
+        svg.insert('defs',":first-child")
+        var svg_xml = (new XMLSerializer).serializeToString(svg.node());            
+        var style = "\n";
+        var requiredSheets = ['phylogram_d3.css', 'open_sans.css'];
+
+        for (var i=0; i<document.styleSheets.length; i++) {
+            var sheet = document.styleSheets[i];
+            if (sheet.href) {
+                var sheetName = sheet.href.split('/').pop();
+                if (requiredSheets.indexOf(sheetName) != -1) {
+                    var rules = sheet.rules;
+                    if (rules) {
+                        for (var j=0; j<rules.length; j++) {
+                            style += (rules[j].cssText + '\n');
+                        }
+                    }
+                }
+            } 
+        }
+
+        img = new Image();
+
+        d3.select("svg defs")
+            .append('style')
+            .attr('type','text/css')
+            .html(style);
+
+        img.src = 'data:image/svg+xml;base64,'+window.btoa(unescape(encodeURIComponent(svg_xml)));
+
+        var canvas = document.createElement("canvas");
+
+        var w = svg.node().getBBox().width,
+            h = svg.node().getBBox().height;
+
+        canvas.width = w;
+        canvas.height = h;
+        var context = canvas.getContext("2d")
+        context.fillStyle = "white";
+        context.fillRect(0,0,w,h);
+        context.drawImage(img,0,0,w,h);
+
+        title = title.removeAccents().split(" ").join("_");
+
+        return [canvas, w, h, title, svg_xml];
+    }
+
     d3.selectAll(".alt_tooltip")
         .on(d3plus.client.pointer.over, function() {
             dataviva.ui.tooltip("alt_tooltip", this)
@@ -54,124 +115,50 @@ var toolsBuilder = function(viz, data, title, ui) {
     d3.select('#download-csv').on('click', function() {
         viz.csv(true);
         dataviva.popover.hide();
+        expireCookie("downloadToken");
     });
 
     d3.select('#download-pdf').on('click', function() {
-     var svg = d3.select("#tree_map > svg")
-        // Add necessary name space junk and get raw node
 
-        svg.attr("version", 1.1)
-          .attr("xmlns", "http://www.w3.org/2000/svg")
-          .node()
-        svg.insert('defs',":first-child")
-        var svg_xml = (new XMLSerializer).serializeToString(svg.node());            
-        var style = "\n";
-        var requiredSheets = ['phylogram_d3.css', 'open_sans.css'];
+        var initialize = initializeDownload();
+        canvas = initialize[0];
+        w = initialize[1]*0.264583333;
+        h = initialize[2]*0.264583333;
+        title = initialize[3];
 
-        for (var i=0; i<document.styleSheets.length; i++) {
-          var sheet = document.styleSheets[i];
-          if (sheet.href) {
-            var sheetName = sheet.href.split('/').pop();
-            if (requiredSheets.indexOf(sheetName) != -1) {
-              var rules = sheet.rules;
-              if (rules) {
-                for (var j=0; j<rules.length; j++) {
-                  style += (rules[j].cssText + '\n');
-                }
-              }
-            }
-          }
-        }
-
-        img = new Image(),
-
-        d3.select("svg defs")
-            .append('style')
-            .attr('type','text/css')
-            .html(style);
-
-        img.src = 'data:image/svg+xml;base64,'+window.btoa(unescape(encodeURIComponent(svg_xml)));
-
-        var canvas = document.createElement("canvas");
-        document.body.appendChild(canvas);
-
-        var w = svg.node().getBBox().width,
-            h = svg.node().getBBox().height;
-
-        canvas.width = w;
-        canvas.height = h;
-        var context = canvas.getContext("2d")
-        context.fillStyle = "white";
-        context.fillRect(0,0,w,h);
-        context.drawImage(img,0,0,w,h);
-
-        w = w*0.264583333;
-        h = h*0.264583333;
         pdf = new jsPDF('l', 'mm', [w, h]);
-        pdf.addImage(canvas, 'JPEG', 0,0,w,h)
+        pdf.addImage(canvas, 'JPEG', 0,0,w,h);
         pdf.save(title);
+        
         dataviva.popover.hide();
+        expireCookie("downloadToken");
     });
 
     d3.select('#download-png').on('click', function() {
-        var svg = d3.select("#tree_map > svg")
-        // Add necessary name space junk and get raw node
-
-        svg.attr("version", 1.1)
-          .attr("xmlns", "http://www.w3.org/2000/svg")
-          .node()
-        svg.insert('defs',":first-child")
-        var svg_xml = (new XMLSerializer).serializeToString(svg.node());            
-        var style = "\n";
-        var requiredSheets = ['phylogram_d3.css', 'open_sans.css'];
-
-        for (var i=0; i<document.styleSheets.length; i++) {
-          var sheet = document.styleSheets[i];
-          if (sheet.href) {
-            var sheetName = sheet.href.split('/').pop();
-            if (requiredSheets.indexOf(sheetName) != -1) {
-              var rules = sheet.rules;
-              if (rules) {
-                for (var j=0; j<rules.length; j++) {
-                  style += (rules[j].cssText + '\n');
-                }
-              }
-            }
-          }
-        }
-
-        img = new Image(),
-
-        d3.select("svg defs")
-            .append('style')
-            .attr('type','text/css')
-            .html(style);
-
-        img.src = 'data:image/svg+xml;base64,'+window.btoa(unescape(encodeURIComponent(svg_xml)));
-
-        var canvas = document.createElement("canvas");
-        document.body.appendChild(canvas);
-
-        var w = svg.node().getBBox().width,
-            h = svg.node().getBBox().height;
-
-        canvas.width = w;
-        canvas.height = h;
-        var context = canvas.getContext("2d")
-        context.fillStyle = "white";
-        context.fillRect(0,0,w,h);
-        context.drawImage(img,0,0,w,h);
+        var initialize = initializeDownload();
+        canvas = initialize[0];
+        title = initialize[3];
         
         img_download = canvas.toDataURL("image/png");
         var download = document.createElement('a');
         download.href = img_download;
         download.download = title + ".png";
         download.click();
+
         dataviva.popover.hide();
+        expireCookie("downloadToken");
     });
 
     d3.select('#download-svg').on('click', function() {
+        var initialize = initializeDownload();
+        title = initialize[3];
+        svg = initialize[4];
+        
+        var svgBlob = new Blob([svg], {type:"image/svg+xml;charset=utf-8"});
+        saveAs(svgBlob, title + ".svg");
+
         dataviva.popover.hide();
+        expireCookie("downloadToken");
     });
 
     d3.select('#controls-toggle-btn').on('click', function() {
