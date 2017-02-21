c85e01063f829b487cde22584c37abe442e401ba
luisccf <luisccf@me.com>
2017-02-21 14:48:47 -0300

    Moves services to utils folder

--- dataviva/apps/tree_map/views.py
+++ dataviva/apps/tree_map/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template, g, request, abort
+from flask import Blueprint, render_template, g, request
 from dataviva.apps.general.views import get_locale
 from dataviva.translations.dictionary import dictionary
+from dataviva.apps.embed.models import Build
+from dataviva.apps.title.views import get_title
+from dataviva.utils.graphs_services import *
 import urllib
 import json
 
@@ -26,91 +29,33 @@ def before_request():
     g.page_type = mod.name
 
 
-def location_service(id_ibge):
-    locations = {
-        1: "region",    #todo
-        2: "state",
-        4: "mesoregion",
-        5: "microregion",
-        7: "municipality"
-    }
-
-    return (locations[len(id_ibge)], id_ibge)
-
-
-def product_service(product):
-    if len(product) == 2:
-        return ('product_section', product[:2])
-    elif len(product) == 4:
-        return ('product_chapter', product[2:4])
-    else:
-        return ('product', product[2:])
-
-
-def wld_service(wld):
-    if wld.isdigit():
-        wld = '%03d' % int(wld)
-
-    wlds = {
-        2: "continent",
-        3: "country"
-    }
-
-    return (wlds[len(wld)], wld)
-
-
-def occupation_service(occupation):
-    occupations = {
-        1: "occupation_group",
-        4: "occupation_family"
-    }
-
-    return (occupations[len(occupation)], occupation)
-
-
-def industry_service(industry):
-    if len(industry) == 1:
-        return ('industry_section', industry)
-    elif len(industry) == 3:
-        return ('industry_division', industry[1:])
-    else:
-        return ('industry_class', industry[1:])
-
-
 @mod.route('/<dataset>/<squares>/<size>')
 def index(dataset, squares, size):
     filters = []
+    title_attrs = {}
+
+    services = {'product': product_service, 'id_ibge': location_service, 'wld':
+                wld_service, 'occupation': occupation_service, 'industry': industry_service}
 
     for key, value in request.args.items():
-        if key not in ['depths', 'sizes', 'group'] and value:
-            if key == 'product':
-                filters.append(product_service(value))
-            elif key == 'id_ibge':
-                filters.append(location_service(value))
-            elif key == 'wld':
-                filters.append(wld_service(value))
-            elif key == 'occupation':
-                filters.append(occupation_service(value))
-            elif key == 'industry':
-                filters.append(industry_service(value))
-            else:
-                filters.append((key, value))
+        if key == 'type':
+            title_attrs['type'] = value
+
+        if key not in ['depths', 'sizes', 'group'] and value and key in services:
+            filters.append(services[key](value))
+            title_attrs['location' if key == 'id_ibge' else key] = value
+        else:
+            filters.append((key, value))
 
     filters = urllib.urlencode(filters)
 
-    group = request.args.get('group') or ''
-
-    params = {}
-    for param in ['depths', 'sizes']:
-        value = request.args.get(param)
-        params[param] = value if value and len(value.split()) > 1 else ''
+    title, subtitle = get_title(dataset, squares, 'tree_map', title_attrs)
 
     return render_template('tree_map/index.html',
                            dataset=dataset,
                            squares=squares,
                            size=size,
-                           group=group,
-                           depths=params['depths'],
-                           sizes=params['sizes'],
                            filters=filters,
+                           title=title or 'Title',
+                           subtitle=subtitle or '',
                            dictionary=json.dumps(dictionary()))
---
+++ dataviva/utils/graphs_services.py
@@ -0,0 +1,32 @@
+def location_service(id_ibge):
+    locations = {1: 'region', 2: 'state', 4: 'mesoregion',
+                 5: 'microregion', 7: 'municipality'}
+    return (locations[len(id_ibge)], id_ibge)
+
+
+def wld_service(wld):
+    if wld.isdigit():
+        wld = '%03d' % int(wld)
+    wlds = {2: 'continent', 3: 'country'}
+    return (wlds[len(wld)], wld)
+
+
+def product_service(product):
+    if len(product) == 2:
+        return ('product_section', product[:2])
+    if len(product) == 4:
+        return ('product_chapter', product[2:4])
+    return ('product', product[2:])
+
+
+def occupation_service(occupation):
+    occupations = {1: 'occupation_group', 4: 'occupation_family'}
+    return (occupations[len(occupation)], occupation)
+
+
+def industry_service(industry):
+    if len(industry) == 1:
+        return ('industry_section', industry)
+    if len(industry) == 3:
+        return ('industry_division', industry[1:])
+    return ('industry_class', industry[1:])
