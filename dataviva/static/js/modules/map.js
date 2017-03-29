e8ec63bd80cee14d8e3a590364d1e067701d5ea4
jdmmiranda307 <jvdmmiranda@gmail.com>
2017-03-29 17:00:03 -0300

    Add datasus maps

--- dataviva/static/js/modules/map.js
+++ dataviva/static/js/modules/map.js
@@ -1,11 +1,13 @@
 var map = document.getElementById('map'),
     dataset = map.getAttribute('dataset'),
     value = map.getAttribute('value'),
-    lang = 'pt';
-    apiFilters = map.getAttribute('filters');
+    apiFilters = map.getAttribute('filters'),
+    validOccupations = {},
+    currentFilters = {};
 
 var args = getUrlArgs(),
-    filters = args['filters'] ? args['filters'].split('+') : FILTERS[dataset]['state'] || [];
+    filters = args.hasOwnProperty('filters') ? args['filters'].split('+') : [],
+    values = args.hasOwnProperty('values') ? args['values'].split('+') : SIZES[dataset]['state'] || [value];
 
 var buildData = function(apiResponse,statesMetadata, otherMetadata) {
 
@@ -17,6 +19,7 @@ var buildData = function(apiResponse,statesMetadata, otherMetadata) {
     var data = [];
     var headers = apiResponse.headers;
 
+
     apiResponse.data.forEach(function(item) {
         try {
             var dataItem = {};
@@ -25,18 +28,17 @@ var buildData = function(apiResponse,statesMetadata, otherMetadata) {
                 dataItem[header] = getAttrByName(item, header);
             });
 
+            if (dataItem.hasOwnProperty('occupation_family'))
+                validOccupations[dataItem['occupation_family']] = 1;
+
             dataItem['id'] = statesMetadata[dataItem['state']]['abbr_' + lang];
             dataItem['stateName'] = statesMetadata[dataItem['state']]['name_' + lang];
-            filters.forEach(function(filter){
-                try{
-                for (d in otherMetadata)
-                   dataItem[d] = otherMetadata[d][dataItem[d]]['name_' + lang];
 
-                }catch(e){
-                } 
-            });
+            for (d in otherMetadata)
+                dataItem[d] = otherMetadata[d][dataItem[d]]['name_' + lang];
+
             data.push(dataItem);
-            
+    
         } catch(e) {
 
         };
@@ -47,37 +49,121 @@ var buildData = function(apiResponse,statesMetadata, otherMetadata) {
 var loadViz = function(data){
     var uiBuilder = function() {
         var ui = [];
+
+        var filteredData = function() {
+            return data.filter(function(item) {
+                var valid = true,
+                    keys = Object.keys(currentFilters);
+
+                for (var i = 0; i < keys.length; i++) {
+                    if (currentFilters[keys[i]] == -1)
+                        continue;
+                    if (item[keys[i]] != currentFilters[keys[i]]) {
+                        valid = false;
+                        break;
+                    }
+                }
+
+                return valid;
+            });
+        };
+
+        // Adds size selector
+        if (values.length > 1) {
+            var options = [];
+            values.forEach(function(item) {
+                options.push({[dictionary[item]]: item});
+            });
+
+            ui.push({
+                'method': function(value) {
+                    var d = filteredData();
+                    d.forEach(function(item) {
+                        if (item[value] == 0)
+                            item[value] = null;
+                    });
+                    viz.data(d);
+                    viz.color(value);
+                    viz.draw();
+                },
+                'type': options.length > 3 ? 'drop' : '',
+                'label': dictionary['value'],
+                'value' : options
+            });
+        }
+
         var statements = function(filter, value){
+            if(dictionary['all'] == value)
+                return data
 
             return data.filter(function(item) {
                 return  item[filter] == value});
         };
-        // Adds filters selector
-        filters.forEach(function(filter) {
-            var options = [];
-            var key = -1;
-            var values = {};
-            var label = dictionary['all'];
-            values[label] = label
-            options.push(values);
-            for (id in otherMetadata[filter]) {
-                            key = otherMetadata[filter][id].id;
-                            values = {};
-                            label = otherMetadata[filter][id]['name_' + lang];
-                            values[label] = label;
-                           options.push(values)
-                       }
 
-            ui.push({
-                'method': function(value) {
-                    viz.data(statements(filter, value));
-                    viz.draw();
-                
-                },
-                'type': 'drop',
-                'label': dictionary[filter],
-                'value': options,
-            });
+        // Adds filters selector
+        filters.forEach(function(filter, j) {
+            currentFilters[filter] = -1;
+            var options = [];
+            if (!j) {
+                for (id in otherMetadata[filter]) {
+                    if (filter == 'occupation_family' && !validOccupations.hasOwnProperty(id))
+                        continue;
+                    var label = otherMetadata[filter][id]['name_' + lang],
+                        option = {};
+                    option[label] = label;
+                    options.push(option);
+                }
+
+                options.sort(function(a, b) {
+                    if (Object.keys(a)[0] < Object.keys(b)[0])
+                        return -1;
+                    if (Object.keys(a)[0] > Object.keys(b)[0])
+                        return 1;
+                    return 0;
+                });
+
+                options.unshift({[dictionary['all']]: -1});
+                ui.push({
+                    'method': function(value) {
+                        currentFilters[filter] = value;
+                        viz.data(filteredData());
+                        viz.draw();
+                    },
+                    'label': dictionary[filter],
+                    'type': 'drop',
+                    'value': options
+                });
+            } else {
+                for (id in otherMetadata[filter]) {
+                    if (filter == 'occupation_family' && !validOccupations.hasOwnProperty(id))
+                        continue;
+                    options.push({'id': otherMetadata[filter][id]['name_' + lang], 'label': otherMetadata[filter][id]['name_' + lang]})
+                }
+
+                options.sort(function(a, b) {
+                    if (a['label'] < b['label'])
+                        return -1;
+                    if (a['label'] > b['label'])
+                        return 1;
+                    return 0;
+                });
+
+                options.unshift({'id': -1, 'label': dictionary['all']});
+                d3plus.form()
+                    .container(d3.select('#controls'))
+                    .data(options)
+                    .id('id')
+                    .text('label')
+                    .title(dictionary[filter])
+                    .type('drop')
+                    .font({'size': 11})
+                    .focus(-1, function(value) {
+                        currentFilters[filter] = value;
+                        viz.data(filteredData());
+                        viz.draw();
+                    })
+                    .draw();
+            }
         });
 
         return ui;
@@ -91,11 +177,26 @@ var loadViz = function(data){
         .format(formatHelper())
         .tooltip({'sub': 'stateName'})
         .ui(uiBuilder())
+        .footer(dictionary['data_provided_by'] + ' ' + dataset.toUpperCase())
+        .messages({'branding': true, 'style': 'large'})
         .id('id')
         .time('year')
         .color({'heatmap': ["#282F6B", "#B22200"],
                 'value': value})
-        .draw()
+        .draw();
+
+    var addForms = function() {
+        if ($('#d3plus_drawer').length == 0){
+            setTimeout(addForms, 1000);
+            return;
+        }
+
+        $('#d3plus_drawer').append($('#controls').children());
+        $('#controls').remove();
+    };
+
+    addForms();
+
 }
 
 
@@ -123,10 +224,10 @@ var getUrls = function() {
     return urls;
 };
 
+var loading = dataviva.ui.loading('.loading').text(dictionary['Building Visualization']);
 
 $(document).ready(function() {
          ajaxQueue(
-    //     ['http://api.staging.dataviva.info/'+dataset +'/year/state?'+filters, 'http://api.staging.dataviva.info/metadata/state'], 
         getUrls(),
         function(responses) {
             var data = responses[0];
