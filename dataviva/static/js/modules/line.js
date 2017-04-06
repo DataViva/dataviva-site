41343e37e79ce8ef1eac76e020dd9c99b9ba8ee7
Frederico Vieira <fredops1@hotmail.com>
2017-04-06 17:32:46 -0300

    Adds some functionalities to line graph.

--- dataviva/static/js/modules/line.js
+++ dataviva/static/js/modules/line.js
@@ -1,20 +1,28 @@
 var lineGraph = document.getElementById('lineGraph'),
     dataset = lineGraph.getAttribute('dataset'),
     line = lineGraph.getAttribute('line'),
-    yValue = lineGraph.getAttribute('yvalue'),
+    yValue = lineGraph.getAttribute('y_value'),
     baseTitle = lineGraph.getAttribute('graph-title'),
     baseSubtitle = lineGraph.getAttribute('graph-subtitle'),
     args = getUrlArgs(),
     yearRange = args.hasOwnProperty('year') ? [0, +args['year']] : [0, 0],
     depths = args.hasOwnProperty('depths') ? args['depths'].split('+') : DEPTHS[dataset][line] || [line],
     group = depths[0],
+    yValues = args.hasOwnProperty('yValues') ? args['yValues'].split('+') : SIZES[dataset][yValue] || [yValue],
     filters = args.hasOwnProperty('filters') ? args['filters'].split('+') : [],
     basicValues = BASIC_VALUES[dataset] || [],
     calcBasicValues = CALC_BASIC_VALUES[dataset] || {},
     currentFilters = {},
-    //currentTitleAttrs = {'size': size, 'shapes': squares}
+    currentTitleAttrs = {'yValue': yValue, 'Line': line}
     metadata = {},
     lastYear = 0;
+
+function string2date(dateString) {
+    dateString = dateString.split('-');
+    var month = (dateString.length == 2 && dateString[1] !== '0') ? dateString[1] : 1;
+    var year = dateString[0];
+    return new Date(month + '/01/' + year)
+}
 
 var buildData = function(apiResponse){
 
@@ -42,8 +50,11 @@ var buildData = function(apiResponse){
             if(group && HAS_ICONS.indexOf(group) >= 0)
                 dataItem['icon'] = '/static/img/icons/' + group + '/' + group + '_' + dataItem[group] + '.png';
 
+            for (key in calcBasicValues)
+                dataItem[key] = calcBasicValues[key](dataItem);
+
             depths.forEach(function(depth) {
-                if (depth != 'type'){
+                if (depth != 'type'){ //Refatorar
                     if (depth != line)
                         dataItem[depth] = metadata[line][dataItem[line]][depth]['name_' + lang];
                     else 
@@ -51,11 +62,9 @@ var buildData = function(apiResponse){
                 }
             });
 
-            
-
             //if(type == 'balance') dataItem['icon'] = '/static/img/icons/' + type + '/' + dataItem['type'] + '_val.png';
 
-            //dataItem['date'] = string2date(dataItem['year'] + '-' + dataItem['month'])
+            dataItem['date'] = string2date(dataItem['year'] + '-' + dataItem['month'])
             data.push(dataItem);
 
             //if (dataItem.hasOwnProperty('year') && dataItem['year'] > lastYear)
@@ -69,15 +78,60 @@ var buildData = function(apiResponse){
 }
 
 var loadViz = function(data) {
+    var uiBuilder = function() {
+        var config = {
+                'id': 'id',
+                'text': 'label',
+                'font': {'size': 11},
+                'container': d3.select('#controls'),
+                'search': false
+            };
+    
+        if (dataset == 'secex') {
+            d3plus.form()
+                .config(config)
+                .data([{'id': 1, 'label': dictionary['year']}, {'id': 0, 'label': dictionary['month']}])
+                .title(dictionary['time_resolution'])
+                .type('toggle')
+                .focus(dictionary['year'] ? 1 : 0, function(value) {
+                    debugger;
+                    viz.x({
+                        'value': 'month'
+                    });
+                    viz.draw();
+                //      if (value) {
+                //         loadViz(data);
+                //     } else {
+                //         var loadingData = dataviva.ui.loading('#tree_map').text(dictionary['Downloading Additional Years'] + '...');
+                //         window.location.href = window.location.href.replace(/&year=[0-9]{4}/, '').replace(/\?year=[0-9]{4}/, '?');
+                //     }
+                })
+                .draw();
+        }
+    };
+
+    var hasIdLabel = function() {
+        return DICT.hasOwnProperty(dataset) && DICT[dataset].hasOwnProperty('item_id') && DICT[dataset]['item_id'].hasOwnProperty(line);
+    }
+
+    var tooltipBuilder = function() {
+        return {
+            'short': {
+                '': hasIdLabel() ? DICT[dataset]['item_id'][line] : 'id',
+                [dictionary['basic_values']]: [yValue]
+            },
+            'long': {
+                '': hasIdLabel() ? DICT[dataset]['item_id'][line] : 'id',
+                [dictionary['basic_values']]: basicValues.concat(Object.keys(calcBasicValues))
+            }
+        }
+    };
 
     var viz = d3plus.viz()
         .container('#lineGraph')
         .data({'value': data, 'padding': 0})
         .type('line')
         .shape({'interpolate': 'monotone'})
-        .id({
-            'value': depths
-        })
         .x({
             'value': 'year',
             'label': {'font': {'size': 20}},
@@ -93,13 +147,36 @@ var loadViz = function(data) {
         .labels({'align': 'left', 'valign': 'top'})
         .background('transparent')
         .time({'value': 'year'})
+        .icon({'value': 'icon', 'style': 'knockout'})
+        .legend({'order': {'sort': 'desc', 'value': 'size'}})
+        .footer(dictionary['data_provided_by'] + ' ' + (dictionary[dataset] || dataset).toUpperCase())
+        .messages({'branding': true, 'style': 'large'})
+        .tooltip(tooltipBuilder())
+        .format(formatHelper())
+        .ui(uiBuilder());
+
+        if (group){
+            viz.id([group, line]);
+            viz.color(group);
+        } else {
+            viz.id(line).color(line);
+        }
+
+        if (COLORS.hasOwnProperty(group)) {
+            viz.attrs(COLORS[group]);
+            viz.color('color');
+        } else {
+            viz.color({'scale':'category20', 'value': args['color'] || depths[0]});
+        }
+
+        $('#lineGraph').css('height', (window.innerHeight - $('#controls').height() - 40) + 'px');
     
     viz.draw();
 
 };
 
 var getUrls = function() {
-    var dimensions = [dataset, 'year', line],
+    var dimensions = [dataset, (dataset == 'secex' ? 'month/year' : 'year'), line],
     metadataAttrs = [];
     
     depths.concat(filters).forEach(function(attr) {
@@ -111,7 +188,6 @@ var getUrls = function() {
 
     var urls = [API_DOMAIN + '/' + dimensions.join('/') + '?' + lineGraph.getAttribute('filters')]
 
-    //if (dataset == 'secex') urls[0] = urls[0].replace('/year', '/year/month');
     if (line != 'type') urls.push(API_DOMAIN + '/metadata/' + line);
 
     return urls;
@@ -127,9 +203,6 @@ $(document).ready(function() {
             metadata[line] = responses[1];
 
             data = buildData(data);
-
-            // debugger
-
             loadViz(data);
 
             loading.hide();
@@ -142,4 +215,12 @@ $(document).ready(function() {
 // http://localhost:5000/en/product/021201/trade-partner?menu=exports-destination-line&url=line%2Fsecex%2Fall%2F021201%2Fall%2Fwld%2F%3Fy%3Dexport_val%26color%3Dcolor
 
 // Url:
-// http://localhost:5000/en/line/secex/country/value?product=021201
+// http://localhost:5000/en/line/secex/country/value?product=021201&type=export
+
+//ToDo:
+// 1. Montar gráfico Anual (Padrão) e Mensal (Seletores UI).
+// 2. Interpolar dados faltantes.
+// 3. Calcular Balança Comercial.
+// 4. Verificar tooltips:
+//      - Valores;
+//      - Ícones para países;
