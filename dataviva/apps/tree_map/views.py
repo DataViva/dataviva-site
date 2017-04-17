ac58c865fef2c626156b9e266e1da3c7a0202c0b
luisccf <luisccf@me.com>
2017-04-17 16:24:10 -0300

    Gets title metadata from api

--- dataviva/apps/tree_map/views.py
+++ dataviva/apps/tree_map/views.py
@@ -45,7 +45,7 @@ def index(dataset, squares, size):
           continue
         if value and key in services:
             filters.append(services[key](value))
-            title_attrs[services[key](value)[0]] = value
+            title_attrs[services[key](value)[0]] = services[key](value)[1]
         else:
             filters.append((key, value))
 
