25122a8d51da7f637886f68527f23b902eb6c753
luisccf <luisccf@me.com>
2017-05-18 14:16:56 -0300

    Fixes missing controls in tree map

--- dataviva/static/js/modules/tree_map.js
+++ dataviva/static/js/modules/tree_map.js
@@ -374,8 +374,9 @@ var loadViz = function(data) {
     $('#tree_map').css('height', (window.innerHeight - $('#controls').height() - 40) + 'px');
     viz.draw();
     
-    if (document.getElementById('controls').style.display == 'none')
+    if ($('#controls').css('display') == 'none') {
         $('#controls').fadeToggle();
+    }
     
     toolsBuilder('tree_map', viz, data, titleHelper(yearRange).value);
 };
