451afc88f490e941358f8a2d911fa1d445dd804e
daniel1409 <danieltafuri1998@gmail.com>
2017-07-10 14:08:01 -0300

    Add hedu course and shift icons

--- dataviva/static/js/graphs/helper.js
+++ dataviva/static/js/graphs/helper.js
@@ -256,7 +256,7 @@ var CALC_BASIC_VALUES = {
     'cnes_professional': {}
 };
 
-var HAS_ICONS = ['continent', 'industry_section', 'product_section', 'occupation_group'];
+var HAS_ICONS = ['continent', 'industry_section', 'product_section', 'occupation_group', 'hedu_course_field', 'shift'];
 var NEEDS_CASTING = ['wage', 'average_wage', 'enrolled', 'entrants', 'graduates'];
 
 var titleBuilder = function(title, subtitle, attrs, dataset, filters, yearRange) {
