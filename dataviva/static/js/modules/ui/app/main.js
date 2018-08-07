b96cf9cbfa81f758a40294a1e7750f0311769e58
leandrocollares <leco@uvic.ca>
2018-08-07 11:58:38 -0300

    Add files pertaining to localization support

--- dataviva/static/js/modules/ui/app/main.js
+++ dataviva/static/js/modules/ui/app/main.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
+import i18n from "/lib/i18n";
 import * as Components from "./components/**/*.vue";
 
-const selector = new Vue({ render: h => h("DataBaseSelector") });
+const selector = new Vue({ render: h => h("DataBaseSelector"), i18n });
 Object.values(Components).map(a => a.name && Vue.component(a.name, a));
 
 if (document.getElementById("database-selector") != null) {
