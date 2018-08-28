8e76772e262da0a9b5a257ddf7d1c05b67d884f1
leandrocollares <leco@uvic.ca>
2018-08-28 16:53:52 -0300

    Add CSS animation to dataviva logo

--- dataviva/static/js/modules/ui/app/components/elements/loader.vue
+++ dataviva/static/js/modules/ui/app/components/elements/loader.vue
@@ -18,5 +18,15 @@ img {
   left: calc(50% - 50px);
   height: 100px;
   width: 100px;
+  animation: fade-in-out 1s steps(12) infinite;
+}
+
+@keyframes fade-in-out {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
