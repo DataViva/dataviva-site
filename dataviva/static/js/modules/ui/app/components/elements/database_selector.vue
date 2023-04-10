22179c06b2546ad50f15d5d645b67dee57eee25f
Samuel Bernardes <sbernardes467@gmail.com>
2023-04-10 10:23:24 -0300

    change buttuns 4x4

--- dataviva/static/js/modules/ui/app/components/elements/database_selector.vue
+++ dataviva/static/js/modules/ui/app/components/elements/database_selector.vue
@@ -1,34 +1,21 @@
 <template>
-  <div
-    class="flex flex-row flex-wrap justify-center pv3">
-    <a
-      v-for="database in databases"
-      :key="database.name"
-      :data-content="$t(database.tooltipText)"
-      class="no-underline burgundy t-bg-transparent hover-bg-burgundy mh2 h4
-             w5 mh2 mb4 pt1 hover-white"
-      data-toggle="popover"
-      data-placement="top"
-      @click="selectDataBase(database.code);">
-      <div class="tc">
-        <div
-          class="f1 mt3 mb2">
-          <i
-            :class="database.icon.db" />
+  <div class="row">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3" v-for="database in databases" :key="database.name">
+      <a :data-content="$t(database.tooltipText)" class="no-underline burgundy t-bg-transparent hover-bg-burgundy h4 w100 mb4 pt1 hover-white" data-toggle="popover" data-placement="top" @click="selectDataBase(database.code);">
+        <div class="tc">
+          <div class="f1 mt3 mb2">
+            <i :class="database.icon.db" />
+          </div>
+          <div>
+            <p class="f3 capitalize">
+              {{ $t(database.name) }}
+            </p>
+          </div>
         </div>
-        <div>
-          <p
-            class="f3 capitalize">
-            {{ $t(database.name) }}
-          </p>
-        </div>
-      </div>
-    </a>
+      </a>
+    </div>
     <transition name="fade">
-      <selector
-        v-if="show"
-        :database="database"
-        @close="closeModal()"/>
+      <selector v-if="show" :database="database" @close="closeModal()"/>
     </transition>
   </div>
 </template>
