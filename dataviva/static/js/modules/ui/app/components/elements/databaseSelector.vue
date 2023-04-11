1f331cca5231ffcf5a85c3af3c16870be90508dc
Samuel Bernardes <sbernardes467@gmail.com>
2023-04-11 09:40:41 -0300

    no message

--- dataviva/static/js/modules/ui/app/components/elements/databaseSelector.vue
+++ dataviva/static/js/modules/ui/app/components/elements/databaseSelector.vue
@@ -1,26 +1,24 @@
 <template>
-	<div class="row">
-		<div v-for="database in databases" class="buttons-base">
-			<a :key="database.name" :data-content="$t(database.tooltipText)"
-				class="no-underline burgundy t-bg-transparent hover-bg-burgundy mh2 h4 w5 mh2 mb4 pt1 hover-white"
-				data-toggle="popover" data-placement="top" @click="selectDataBase(database.code);">
-				<div class="tc">
-					<div class="f1 mt3 mb2">
-						<i :class="database.icon.db" />
-					</div>
-					<div>
-						<p class="f3 capitalize">teste</p>
-					</div>
+	<div class="flex flex-row flex-wrap justify-center pv3">
+		<a v-for="database in databases" :key="database.name" :data-content="$t(database.tooltipText)" class="no-underline burgundy t-bg-transparent hover-bg-burgundy mh2 h4
+					   w5 mh2 mb4 pt1 hover-white" data-toggle="popover" data-placement="top" @click="selectDataBase(database.code);">
+			<div class="tc">
+				<div class="f1 mt3 mb2">
+					<i :class="database.icon.db" />
 				</div>
-			</a>
-		</div>
+				<div>
+					<p class="f3 capitalize">
+						{{ "teste" }}
+					</p>
+				</div>
+			</div>
+		</a>
 		<transition name="fade">
 			<selector v-if="show" :database="database" @close="closeModal()" />
 		</transition>
 	</div>
 </template>
-
-
+  
 <script>
 import "../../assets/css/*.css";
 
@@ -53,3 +51,4 @@ export default {
 	},
 };
 </script>
+  
