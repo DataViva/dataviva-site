6625aaa4609eb52d6756774db192ea9d0612eeb2
Rubens da Cunha Castro <89034366+RCastro13@users.noreply.github.com>
2023-05-18 09:17:23 -0300

    Feature/new color (#1771)
    * update site colors, logo and style changes
    
    * update about page responsivity
    
    * new colors for graphs page
    
    * about responsiveness fix
    
    * downloads sidebar color
    
    * fixed help page colors and functionality
    
    * small colors changes
    
    ---------
    
    Co-authored-by: João Vitor <101755765+Akayy0@users.noreply.github.com>

--- dataviva/static/js/modules/help/classifications.js
+++ dataviva/static/js/modules/help/classifications.js
@@ -1099,56 +1099,6 @@ window.showDatasus = function() {
     window.Datasus = new DatasusTable();
 };
 
-window.showPort = function() {
-    // @todo: move translate to babel
-    dataviva.dictionary['port'] = lang == 'en' ? 'Port' : 'Porto';
-
-    var loadingPort = dataviva.ui.loading('.classifications-ports .classifications-ports-wrapper');
-    loadingPort.text(dataviva.dictionary['loading'] + "...");
-
-    var PortTable = function () {
-        this.tableId = '#ports-table';
-
-        this.table = $(this.tableId).DataTable({
-            "dom": '<"classifications-ports-control">Bfrtip',
-             "buttons": [
-                {
-                    extend: 'csvHtml5',
-                    text: '<i class="fa fa-floppy-o fa-lg"></i>',
-                    filename: 'dataviva-help-ports'
-                }
-            ],
-            "ajax": {
-                "url": "/attrs/port/?lang=" + lang,
-                "dataSrc": "data",
-                "cache": true,
-            },
-            "order": [],
-            "columns": [
-                {
-                    data: "id"
-                },
-                {
-                    data: "name"
-                },
-            ],
-            "deferRender": true,
-            "language": dataviva.datatables.language,
-            "scrollY": 500,
-            "scrollX": true,
-            "scrollCollapse": true,
-            "scroller": true,
-            initComplete: function () {
-                $('#ports-table_filter input').removeClass('input-sm');
-                $('#ports-table_filter').addClass('pull-right');
-                $('.classifications-ports .classifications-ports-wrapper .classifications-ports-content').show();
-                loadingPort.hide();
-            }
-        });
-    };
-    window.Port = new PortTable();
-};
-
 $('.help-classifications-locations').on('click', function(){
     dataviva.requireAttrs(['bra'], function() {
         if(!window.locations){ 
@@ -1228,11 +1178,5 @@ $('.help-classifications-establishment-informations').on('click', function(){
 $('.help-classifications-datasus').on('click', function(){
     if(!window.Datasus){
         showDatasus();
-    }
-});
-
-$('.help-classifications-ports').on('click', function(){
-    if(!window.Port){
-        showPort();
     }
 });
