b79273153995f9fbbb9dc0218915011b829cfc8b
Kayque Meira Siqueira <61908237+Kayross54x@users.noreply.github.com>
2023-06-14 09:03:56 -0300

    Adding the correct html and css to staging (#1784)
    * Adding the correct html and css to staging
    
    * solve translate bugs and education missiong data
    
    ---------
    
    Co-authored-by: Samuel Bernardes <sbernardes467@gmail.com>

--- dataviva/apps/industry/views.py
+++ dataviva/apps/industry/views.py
@@ -28,7 +28,7 @@ def add_language_code(endpoint, values):
 
 
 def getRaisLatestYear():
-    latestRaisYear = "2021"
+    latestRaisYear = "2020"
 
     response = requests.get(g.api_url + "years/rais")
 
@@ -138,6 +138,9 @@ def index(cnae_id, tab):
             body['municipality_with_more_num_jobs_value'] = industry_municipality_service.highest_number_of_jobs()
             body['municipality_with_more_num_jobs_name'] = industry_municipality_service.municipality_with_more_num_jobs()
             body['municipality_with_more_num_jobs_state'] = industry_municipality_service.municipality_with_more_jobs_state()
+            body['municipality_with_more_wage_avg_name'] = industry_municipality_service.municipality_with_more_wage_average()
+            body['municipality_with_more_wage_avg_value'] = industry_municipality_service.biggest_wage_average()
+            body['municipality_with_more_wage_avg_state'] = industry_municipality_service.municipality_with_biggest_wage_average_state()
 
 
     else:
@@ -145,8 +148,13 @@ def index(cnae_id, tab):
 
         body['municipality_with_more_num_jobs_value'] = industry_municipality_service.highest_number_of_jobs()
         body['municipality_with_more_num_jobs_name'] = industry_municipality_service.municipality_with_more_num_jobs()
+        body['municipality_with_more_wage_avg_name'] = industry_municipality_service.municipality_with_more_wage_average()
+        body['municipality_with_more_wage_avg_value'] = industry_municipality_service.biggest_wage_average()
         body['municipality_with_more_num_jobs_state'] = industry_municipality_service.municipality_with_more_jobs_state()
+        body['municipality_with_more_wage_avg_state'] = industry_municipality_service.municipality_with_biggest_wage_average_state()
 
+    body['occ_with_more_wage_avg_name'] = industry_occupation_service.occupation_with_biggest_wage_average()
+    body['occ_with_more_wage_avg_value'] = industry_occupation_service.biggest_wage_average()
     body['occ_with_more_number_jobs_name'] = industry_occupation_service.occupation_with_more_jobs()
     body['occ_with_more_number_jobs_value'] = industry_occupation_service.highest_number_of_jobs()
 
