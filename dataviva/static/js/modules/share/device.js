b81b2f6ce19d558edde1cfea69dd99bdd42fdb43
kayque <kayque.original@hotmail.com>
2023-07-13 11:50:30 -0300

    Fixing share button

--- dataviva/static/js/modules/share/device.js
+++ dataviva/static/js/modules/share/device.js
@@ -1,20 +1,31 @@
-$(document).ready(() => {
 
-    const url = location.pathname;
+const url = location.href;
 
-    const title = $('#title').text();
+const title = $('#title').text();
 
-    $('#share-button').on('click', () => {
-        if (navigator.share) {
-            navigator.share({
-                title: `${title}`,
-                text: 'Confira esse notícia de Dataviva!',
-                url: url,
-            })
-                .catch((error) => console.log('Error sharing', error));
-        } else {
-            console.log('Share not supported on this browser, do it the old way.');
-        }
-    });
-})
+const button = document.getElementById('share-button');
+button.value = url;
+
+button.onclick = () => {
+    if (navigator.share) {
+        navigator.share({
+            title: `${title}`,
+            text: 'Confira esse notícia de Dataviva!',
+            url: url,
+        })
+            .catch((error) => console.log('Error sharing', error));
+    } else {
+        var tempInput = document.createElement("input");
+        tempInput.value = button.value;
+        document.body.appendChild(tempInput);
+        
+        tempInput.select();
+        tempInput.setSelectionRange(0, 99999); /* Para dispositivos móveis */
+        document.execCommand("copy");
+        
+        document.body.removeChild(tempInput);
+        alert('Link copiado para a área de transferência!');
+        console.log('Navigator object not supported on this browser, do it the old way.');
+    }
+}
 
