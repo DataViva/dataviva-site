c3532336c2fc7b31a901b1cc5da0b237d00d8b3c
kayque <kayque.original@hotmail.com>
2023-07-31 10:47:49 -0300

    Fixing request error

--- dataviva/static/js/modules/location.js
+++ dataviva/static/js/modules/location.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
     let latestSecexYear = "2022";
     let latestRaisYear = "2021";
 
-    Promise.all([fetch("https://api.dataviva.info/years/secex", fetchObject), fetch("https://api.dataviva.info/years/rais", fetchObject)]).then((values) => {
+    Promise.all([fetch("https://api.dataviva.info/years/secex"), fetch("https://api.dataviva.info/years/rais")]).then((values) => {
         values.forEach((value, index) => {
             value.json().then((data) => {
                 let localData = data.years.at(-1);
