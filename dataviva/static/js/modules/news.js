924acdd8c7a5a905bf720d7f0dc9610f9e66a7ff
Rubens da Cunha Castro <rubens.cunhacastro@gmail.com>
2023-10-03 09:48:16 -0300

    updating news page (layout and filter)

--- dataviva/static/js/modules/news.js
+++ dataviva/static/js/modules/news.js
@@ -1,9 +1,15 @@
 $(function() {
     $('#search-form').submit(function() {
+        let arrayItens = []
+        let itens = document.querySelectorAll(".checkBoxes [type=checkbox]:checked")
+        for(let item of itens) {
+            arrayItens.push(Number(item.id));
+        }
+
         if ($(this).find('input').val())
-            window.location.href = '/' + dataviva.language + '/news/?search=' + $(this).find('input').val().replace(' ', '+');
+            window.location.href = '/' + dataviva.language + `/news/?${arrayItens.length != 0 ? `subject=${arrayItens}&` : ""}search=` + $(this).find('input').val().replace(' ', '+');
         else
-            window.location.href = '/' + dataviva.language + '/news';
+            window.location.href = '/' + dataviva.language + `/news${arrayItens.length != 0 ? `/?subject=${arrayItens}` : ""}`;
         return false;
     });
 });
