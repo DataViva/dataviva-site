b5b389ebf9c97f52dc119e63924740486f287538
samuel-bernardes <sbernardes467@gmail.com>
2024-04-02 08:45:31 -0300

    Update secex database years in static files

--- dataviva/apps/embed/views.py
+++ dataviva/apps/embed/views.py
@@ -198,9 +198,9 @@ def embed(app_name="tree_map", dataset="rais", bra_id="4mg",
         year_range_dict = __year_range__.copy()
 
         if current_build.app.type in ['network', 'rings']:
-            year_range_dict["secex"] = ["2000-1", "2020-12"]
+            year_range_dict["secex"] = ["2000-1", "2023-12"]
         elif current_build.app.type in ['scatter']:
-            year_range_dict["secex"] = ["2000-1", "2019-12"]                      
+            year_range_dict["secex"] = ["2000-1", "2023-12"]                      
 
         year_range = json.dumps(year_range_dict)
         ret = make_response(render_template("embed/embed.html",
